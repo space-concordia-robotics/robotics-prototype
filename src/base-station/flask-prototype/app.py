6528f2c86d607ef412ee24685681dc1404f30263
nbeeri <nbeeri@hotmail.com>
2018-07-27 00:04:16 -0400

    [#42] Added a home page the has samples of ideas we have that demonstrate different attempts of creating the GUI

--- src/base-station/flask-prototype/app.py
+++ src/base-station/flask-prototype/app.py
@@ -9,7 +9,7 @@ app = flask.Flask(__name__)
 # Once we launch this, this will route us to the "../" page or index page and automatically render the Rover GUI
 @app.route("/")
 def index():
-    return flask.render_template("Rover_GUI.html")
+    return flask.render_template("home.html")
 
 @app.route("/BaseContainers")
 def base_containers():
@@ -18,5 +18,9 @@ def base_containers():
 @app.route("/BaseContainers2")
 def base_containers2():
 	return flask.render_template("BaseContainers2.html")
+
+@app.route("/BaseContainers3")
+def base_containers3():
+	return flask.render_template("BaseContainers3.html")
 
 app.run(debug=True) # add param `host= '0.0.0.0'` if you want to run on your machine's IP address
