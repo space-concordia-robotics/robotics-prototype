d2c25aceb059e6066528fb6ca9296d6fe63effca
PeterGhimself <petergranitski@gmail.com>
2018-08-05 20:44:49 -0400

    [#42] rename 'BaseContainers3' to 'AsimovOperation'

--- robot/basestation/app.py
+++ robot/basestation/app.py
@@ -10,7 +10,7 @@ app = flask.Flask(__name__)
 # Once we launch this, this will route us to the "../" page or index page and automatically render the Rover GUI
 @app.route("/")
 def index():
-    return flask.render_template("BaseContainers3.html")
+    return flask.render_template("AsimovOperation.html")
 
 
 app.run(debug=True)  # add param `host= '0.0.0.0'` if you want to run on your machine's IP address
