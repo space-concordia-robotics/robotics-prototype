152dbdd761c92ebb7fab1ad1b8cd9ceec8403973
PeterGhimself <petergranitski@gmail.com>
2018-08-10 23:19:59 -0400

    [#54] ported ClientSender/ServerListener from py2 to py3

--- robot/archives/communication/tests/UDP/ServerListener.py
+++ robot/archives/communication/tests/UDP/ServerListener.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # will listen on any incoming messages on the same network
 # and display them as they get received
 from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
@@ -8,19 +9,19 @@ PORT_NUMBER = 5000
 if len(sys.argv) == 2:
 	PORT_NUMBER = int(sys.argv[1])
 elif len(sys.argv) >= 3:
-	print "too many arguments, one optional argument is the port number, otherwise default to 5000"
-	print "example usage: python ServerListener.py <port>"
+	print("too many arguments, one optional argument is the port number, otherwise default to 5000")
+	print("example usage: python ServerListener.py <port>")
 
 SIZE = 1024
 
-hostName = gethostbyname( '0.0.0.0' )
+hostName = gethostbyname('0.0.0.0')
 
-mySocket = socket( AF_INET, SOCK_DGRAM )
-mySocket.bind( (hostName, PORT_NUMBER) )
+mySocket = socket(AF_INET, SOCK_DGRAM)
+mySocket.bind((hostName, PORT_NUMBER))
 
-print ("Test server listening on port %s \n" % (PORT_NUMBER))
+print("Test server listening on port {} \n".format(PORT_NUMBER))
 
 while True:
         (data,addr) = mySocket.recvfrom(SIZE)
-        print data
+        print(data.decode())
 sys.exit()
