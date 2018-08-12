84106c37dc5b6b65404998f020ae343b8c36d9df
PeterGhimself <petergranitski@gmail.com>
2018-08-12 13:15:57 -0400

    [#54] move UDP ClientSender/ServerListener scripts from archives/ to robot/

---
+++ robot/basestation/ClientSender.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# given the ip address of the destination (and optionally port number, otherwise default to 5000)
+# send some kind of message over to the receiver (running ServerReceiver.py)
+
+import sys
+from socket import socket, AF_INET, SOCK_DGRAM
+from time import sleep
+
+PORT_NUMBER = 5000
+SIZE        = 1024
+
+if len(sys.argv) < 2: # first one is the name of the file
+    print("please pass a destination ip address/port of the listener to send this message to, port is optional and will default to 5000")
+    print("example usage: python ClientSender.py 138.203.97.167 5000")
+    sys.exit(1)
+
+if len(sys.argv) == 3:
+    PORT_NUMBER = int(sys.argv[2])
+elif len(sys.argv) > 3:
+    print("too many args")
+    sys.exit(1)
+
+SERVER_IP = sys.argv[1]
+
+print("Test client sending packets to IP {}, via port {}\n".format(SERVER_IP, PORT_NUMBER))
+
+mySocket = socket( AF_INET, SOCK_DGRAM )
+inner_space   = ""
+left_padding  = "         "
+inner_incr    = False
+movement      = 1
+
+i = 0
+while True:
+    if i == 0:
+        inner_incr = True
+    elif i == 10:
+        inner_incr = False
+
+    if inner_incr:
+        i += movement
+        inner_space += " "*movement
+    else:
+        i -= movement
+        inner_space = inner_space[:len(inner_space) - movement]
+
+    new_line = left_padding + inner_space + "|+|" + left_padding + "|+|"
+    print(new_line)
+    sleep(0.2)
+    mySocket.sendto(str.encode(new_line), (SERVER_IP,PORT_NUMBER))
+
---
+++ robot/rover/ServerListener.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# will listen on any incoming messages on the same network
+# and display them as they get received
+from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
+import sys
+
+PORT_NUMBER = 5000
+
+if len(sys.argv) == 2:
+	PORT_NUMBER = int(sys.argv[1])
+elif len(sys.argv) >= 3:
+	print("too many arguments, one optional argument is the port number, otherwise default to 5000")
+	print("example usage: python ServerListener.py <port>")
+
+SIZE = 1024
+
+hostName = gethostbyname('0.0.0.0')
+
+mySocket = socket(AF_INET, SOCK_DGRAM)
+mySocket.bind((hostName, PORT_NUMBER))
+
+print("Test server listening on port {} \n".format(PORT_NUMBER))
+
+while True:
+        (data,addr) = mySocket.recvfrom(SIZE)
+        print(data.decode())
+sys.exit()
