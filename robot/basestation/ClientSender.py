f662d600d07b0fd9880775b62831702acb002909
PeterGhimself <petergranitski@gmail.com>
2018-08-12 22:15:02 -0500

    [#54] prep Listener/Sender for receiving/transmitting move forward, back, and terminate connection commands

--- robot/basestation/ClientSender.py
+++ robot/basestation/ClientSender.py
@@ -5,10 +5,12 @@ from socket import socket, AF_INET, SOCK_DGRAM
 from time import sleep
 
 PORT_NUMBER = 5000
-SIZE        = 1024
+SIZE = 1024
 
-if len(sys.argv) < 2: # first one is the name of the file
-    print("please pass a destination ip address/port of the listener to send this message to, port is optional and will default to 5000")
+if len(sys.argv) < 2:  # first one is the name of the file
+    print(
+        "please pass a destination ip address/port of the listener to send this message to, port is optional and will default to 5000"
+    )
     print("example usage: python ClientSender.py 127.0.0.1 5000")
     sys.exit(1)
 
@@ -20,24 +22,24 @@ elif len(sys.argv) > 3:
 
 SERVER_IP = sys.argv[1]
 
-print("Test client sending command packets ('w' or 's') to IP {}, via port {}\nPress q to terminate connection.".format(SERVER_IP, PORT_NUMBER))
+print(
+    "Base station client sending command packets ('w' or 's') to IP {}, via port {}\nPress q to terminate connection.".
+    format(SERVER_IP, PORT_NUMBER))
 
-mySocket = socket( AF_INET, SOCK_DGRAM )
+mySocket = socket(AF_INET, SOCK_DGRAM)
 
 while True:
-	try:
-		key = click.getchar()
+    try:
+        key = click.getchar()
 
-		if key == 'w':
-			print("Sending key: " + key)
-			mySocket.sendto(str.encode(key), (SERVER_IP,PORT_NUMBER))
-		elif key == 's':
-			print("Sending key: " + key)
-			mySocket.sendto(str.encode(key), (SERVER_IP,PORT_NUMBER))
-		elif key == 'q':
-			print("\nTerminating connection.")
-			break
-		else:
-			pass
-	except:
-		break
+        if key == 'w':
+            print("Sending key: " + key)
+            mySocket.sendto(str.encode(key), (SERVER_IP, PORT_NUMBER))
+        elif key == 's':
+            print("Sending key: " + key)
+            mySocket.sendto(str.encode(key), (SERVER_IP, PORT_NUMBER))
+        elif key == 'q':
+            print("\nTerminating connection.")
+            break
+    except:
+        break
--- robot/rover/ServerListener.py
+++ robot/rover/ServerListener.py
@@ -5,23 +5,33 @@ from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
 import sys
 
 PORT_NUMBER = 5000
-
-if len(sys.argv) == 2:
-	PORT_NUMBER = int(sys.argv[1])
-elif len(sys.argv) >= 3:
-	print("too many arguments, one optional argument is the port number, otherwise default to 5000")
-	print("example usage: python ServerListener.py <port>")
-
 SIZE = 1024
+if len(sys.argv) == 2:
+    PORT_NUMBER = int(sys.argv[1])
+elif len(sys.argv) >= 3:
+    print(
+        "too many arguments, one optional argument is the port number, otherwise default to 5000"
+    )
+    print("example usage: python ServerListener.py <port>")
 
 hostName = gethostbyname('0.0.0.0')
 
 mySocket = socket(AF_INET, SOCK_DGRAM)
 mySocket.bind((hostName, PORT_NUMBER))
 
-print("Test server listening on port {} \n".format(PORT_NUMBER))
+print("Rover server listening on port {} \n".format(PORT_NUMBER))
 
 while True:
-        (data,addr) = mySocket.recvfrom(SIZE)
-        print(data.decode())
-sys.exit()
+	try:
+	    (data, addr) = mySocket.recvfrom(SIZE)
+	    command = data.decode()
+
+	    if command == 'w':
+	        print("Forward")
+	    elif command == 's':
+	        print("Back")
+	    elif command == 'q':
+	        print("\nTerminating connection.")
+	        break
+	except:
+		break
