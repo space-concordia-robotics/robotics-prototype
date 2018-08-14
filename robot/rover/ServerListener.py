cf372f3583247d1afcf2cf81b90e7ec32980cc0e
PeterGhimself <petergranitski@gmail.com>
2018-08-14 00:56:47 -0500

    [#54] replace serial output sketch for platformio with BlinkingLED sketch; successful test upload

--- robot/rover/ServerListener.py
+++ robot/rover/ServerListener.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
+
 # will listen on any incoming messages on the same network
-# and display them as they get received
+# and display them as they get received,
+# as well as send corresponding commands over serial to a usb connected arduino
+
 from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
-import sys
+import sys, traceback
+import serial
+import serial.tools.list_ports
 
 PORT_NUMBER = 5000
 SIZE = 1024
+
 if len(sys.argv) == 2:
     PORT_NUMBER = int(sys.argv[1])
 elif len(sys.argv) >= 3:
@@ -15,23 +21,34 @@ elif len(sys.argv) >= 3:
     print("example usage: python ServerListener.py <port>")
 
 hostName = gethostbyname('0.0.0.0')
-
 mySocket = socket(AF_INET, SOCK_DGRAM)
 mySocket.bind((hostName, PORT_NUMBER))
+
+# set up connection to arduino
+ports = list(serial.tools.list_ports.comports())
+firstPortName = ports[0].name
+print("Connecting to port: " + firstPortName)
+ser = serial.Serial('/dev/' + firstPortName, 9600)
 
 print("Rover server listening on port {} \n".format(PORT_NUMBER))
 
 while True:
-	try:
-	    (data, addr) = mySocket.recvfrom(SIZE)
-	    command = data.decode()
+    try:
+        (data, addr) = mySocket.recvfrom(SIZE)
+        command = data.decode()
 
-	    if command == 'w':
-	        print("Forward")
-	    elif command == 's':
-	        print("Back")
-	    elif command == 'q':
-	        print("\nTerminating connection.")
-	        break
-	except:
-		break
+        if command == 'w':
+            print("Forward")
+            ser.write(str.encode(command))
+        elif command == 's':
+            print("Back")
+            ser.write(str.encode(command))
+        elif command == 'q':
+            print("\nTerminating connection.")
+            break
+    except Exception:
+        print("Exception in user code:")
+        print("-"*60)
+        traceback.print_exc(file=sys.stdout)
+        print("-"*60)
+        break
--- robot/util/platformio/src/BlinkingLED.ino
+++ robot/util/platformio/src/BlinkingLED.ino
@@ -1,13 +1,37 @@
+int incomingByte;
+int LONG_PAUSE = 200;
+int SHORT_PAUSE = 100;
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+    // open serial port, set data rate to 9600
+    Serial.begin(9600);
+    // initialize digital pin LED_BUILTIN as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+    // do things only when you receive data
+
+    if (Serial.available() > 0) {
+        // read the incoming byte
+        incomingByte = Serial.read();
+
+        if (incomingByte == 'w') {
+            for (int i = 0; i < 3; i ++) {
+                digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+                delay(SHORT_PAUSE);                // wait for a bit
+                digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+                delay(SHORT_PAUSE);                // wait for a bit
+            }
+        }
+        else if (incomingByte == 's') {
+            for (int i = 0; i < 5; i++) {
+                digitalWrite(LED_BUILTIN, HIGH);
+                delay(LONG_PAUSE);
+                digitalWrite(LED_BUILTIN, LOW);
+                delay(LONG_PAUSE);
+            }
+        }
+    }
 }
