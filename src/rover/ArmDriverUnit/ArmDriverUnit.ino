5de0ece4002761046e5d9a4e54e1ffbaef4fb2a1
Josh Glazer <vashmata@gmail.com>
2018-08-26 19:38:18 -0400

    [#24] Update ArmDriver.ino

--- src/rover/ArmDriverUnit/ArmDriverUnit.ino
+++ src/rover/ArmDriverUnit/ArmDriverUnit.ino
@@ -1,38 +1,52 @@
 /*
   TODO:
-  -replace strtok with strtok_r when implementing interrupts
-
-  -for motor control, budge time is in seconds but this isn't very granular.
-   milliseconds is ideal but requires different parsing method for time length.
-  
   -implement simple manual control:
-   -import stepper library and maybe DC library for simple manual control
-   -use libraries to write functions (that aren't optimized) for manual control
-   -rewrite parsing function to parts what tatum will be sending (x,y)
+   -(done) parsing procedure can process what tatum sends
+   -(done) servo and dc motor control work and behave the same way
+   
+   -put parsing code into an actual function (move variables out of this file)
+   -instead of seting whichMotor to 0, perform some kind of check so that the motor doesn't spin forever
+   -figure out how to send motor info from parsing function to budgeMotor etc (move motor object creation into setup() )
+   
+   -import stepper library for simple manual control
+   -replace strtok with strtok_r when implementing interrupts
+   -connect and test stepper and stepper driver with Teensy code
+   
+   -determine the actual clockwise and counter-clockwise directions of motors based on their wiring in the arm itself
+   -comment out serial.print debugging messages? maybe not
+   
+   -decide whether to use abtin's software interrupt or simpler method for simultaneous motor control
+    -implement appropriate software interrupts that can take input from PID or manual control functions?
+    -decide frequency of motor control loop: figure out estimated time for loop
+    -otherwise figure out a simpler way of doing it
+   -implement heartbeat after deciding on best timer for it (see below)
+   
+   -implement speed control or time control, decide what value to fix the one not being controlled
+   -figure out how to solve the servo jitter
+   -figure out how to step up to 5v for the sabertooth
+     
+-fix issue where i need to create the motor objects inside the motordriver header
+   -(done): the issue is a circular dependency solved by placing the encoder count variables elsewhere
   
-  -fix issue where i need to create the motor objects inside the motordriver header
-   -fixed: the issue is a circular dependency solved by placing the encoder count variables elsewhere
-  
-  -implement appropriate timer ISRs that can take input from PID or manual control functions:
+  -external interrupts for encoders and limit switches
+   -determine whether it's worth it to use the built in quadrature decoders
    -rewrite all the register bit variables to use teensy registers for encoder/limit switch interrupts
    -rewrite the motor control with timers for teensy, ensure control for all motors
-   
-  -implement PID for automatic control
-	-update parsing function or just go directly to ROSserial
-	
   -incorporate limit switches for homing position on all motors
   -implement homing function on boot
-
+   
+  -implement PID for automatic control
+	-update parsing function to take in angles or just go directly to ROSserial, probably the former
+	
   -timers
    -systick is normally a heartbeat type thing
    -lptmr runs even on low power mode, maybe this should be heartbeat instead?
    -pit is used for intervaltimer objects, there are 4 and they work like interrupts
-    -decide frequency of motor control loop: figure out estimated time for loop
    -pwm: teensy has 6 16bit pwm timers and apparently 22 total pwm options:
-    -
-   -teensy pwm page mentions ftm and tpm timers which aren't mentioned in the page with other timesr
-   -ftm+tpm has quadrature decoder?
-   -tpm is 2-8 channel timer with pwm, 16bit counter, 2 channels for pwm
+    -currently using whatever is connected to the pwm pins for timing pwm
+    -teensy pwm page mentions ftm and tpm timers which aren't mentioned in the page with other timesr
+    -ftm+tpm has quadrature decoder?
+    -tpm is 2-8 channel timer with pwm, 16bit counter, 2 channels for pwm
    
   -implement error checking?
   -implement power management? sleep mode?
@@ -41,14 +55,16 @@
 #include "PinSetup.h"
 #include "ArmMotor.h"
 
-#define BAUD_RATE 115200    //serial baud rate
-#define BUFFER_SIZE 100     //size of the buffer for the serial commands
+// definitions and variables for parsing function: to put in self-contained header file
+#define BAUD_RATE 115200 // serial baud rate
+#define BUFFER_SIZE 100  // size of the buffer for the serial commands
+char serialBuffer[BUFFER_SIZE]; // serial buffer used for early- and mid-stage tesing without ROSserial
+int whichMotor;
+int whichDir;
+int whichSpeed; int tempSpeedVar;
+// int whichTime; int tempTimeVar;
 
-char serialBugger[BUFFER_SIZE];
-
-int whichMotor; int whichDir; int whichSpeed; int tempSpeedVar; // int whichTime; int tempTimeVar;
-
-
+// these objects were created outside of the setup function because the update() proto-code isn't the right place yet
 ArmMotor motor1(MOTOR1); // base stepper (rotation)
 ArmMotor motor2(MOTOR2); // shoulder dc (flexion)
 ArmMotor motor3(MOTOR3); // elbow stepper (flexion)
@@ -59,17 +75,17 @@ ArmMotor motor6(MOTOR6); // end effector servo (pinching)
 void setup() {
   pinSetup();
   Serial.begin(BAUD_RATE);
+  // place armmotor object creations here once code is cleaned up
 }
 
 void loop() {
   
   //digitalWrite(LED_PIN, led_status);
 
-   if (Serial.available()) { // make sure the whole message was received
-    Serial.readBytesUntil(10, serialBuffer, 100); //read until NL
-    Serial.print("GOT: ");
-    Serial.println(serialBuffer);
-	
+  if (Serial.available()) { // if a message was sent to the Teensy
+    Serial.readBytesUntil(10, serialBuffer, BUFFER_SIZE); // read through it until NL
+    Serial.print("GOT: "); Serial.println(serialBuffer); // send back what was received
+
     //parseCommand(serialBuffer);           //parse the string
 	
 	/* 
@@ -80,53 +96,59 @@ void loop() {
 	
 	//strtok_r() splits message by a delimiter string
 	char* msgElem = strtok(serialBuffer, " "); // look for first element (first tag)
-	if (String(msgElem) == "motor"){
+	if (String(msgElem) == "motor"){ // msgElem is a char array so it's safer to convert to string first
 		msgElem = strtok(NULL, " "); // go to next msg element (motor number)
 		switch(*msgElem){ // determines which motor being used
 			case '1':
 				whichMotor = MOTOR1;
+				Serial.print("motor 1");
 				break;
 			case '2':
 				whichMotor = MOTOR2;
+				Serial.print("motor 2");
 				break;
 			case '3':
 				whichMotor = MOTOR3;
+				Serial.print("motor 3");
 				break;
 			case '4':
 				whichMotor = MOTOR4;
+				Serial.print("motor 4");
 				break;
 			case '5':
 				whichMotor = MOTOR5;
+				Serial.print("motor 5");
 				break;
 			case '6':
 				whichMotor = MOTOR6;
+				Serial.print("motor 6");
 				break;
 		}
 		Serial.print("motor"); Serial.println(whichMotor);
 		msgElem = strtok(NULL, " "); //find the next message element (direction tag)
-		if (String(msgElem) == "direction"){
+		if (String(msgElem) == "direction"){ // msgElem is a char array so it's safer to convert to string first
 			msgElem = strtok(NULL, " "); // go to next msg element (direction)
 			switch(*msgElem){ // determines motor direction
-				case '0':
+				case '0': // arbitrarily (for now) decided 0 is clockwise
 					whichDir = CLOCKWISE;
 					Serial.println("clockwise");
 					break;
-				case '1':
+				case '1': // arbitrarily (for now) decided 1 is counter-clockwise
 					whichDir = COUNTER_CLOCKWISE;
 					Serial.println("counter-clockwise");
 					break;
 			}
 			msgElem = strtok(NULL, " "); // find the next message element (time tag)
-			if (String(msgElem) == "speed"){
+			if (String(msgElem) == "speed"){ // msgElem is a char array so it's safer to convert to string first
 			    msgElem = strok(NULL, " "); // find the next message element (integer representing speed level)
 				tempSpeedVar = *msgElem - '0'; // example '3' - '0' = 3
-				if (tempSpeedVar <= 0){ // make sure the subtraction made sense, if it's above 9 it doesn't
-					whichSpeed = tempSpeedVar;
+				if (tempSpeedVar <= 9){ // make sure the subtraction made sense. If it's above 9, it doesn't
+					whichSpeed = tempSpeedVar; // set the actual speed
 					Serialprint("speed level: "); Serial.println(whichSpeed);
 				}
 			}
 			/*msgElem = strtok(NULL, " "); //find the next message element (time tag)
-			if (*msgElem == 'time'){
+			if (*msgElem == 'time'){ // msgElem is a char array so it's safer to convert to string first
 				msgElem = strtok(NULL, " "); //find the next message element (time in seconds)
 				// because of how this works it only takes 0-3 sec, must rework later
 				tempTimeVar = *msgElem - '0'; // example: '3' - '0' = 3
@@ -142,34 +164,25 @@ void loop() {
 	//proto code for ArmMotor::update() function?
 	switch(whichMotor){ // tells the appropriate motor to move with ArmMotor.budge()
 		case MOTOR1:
-			motor1.sinceStart = 0;
 			motor1.budge(whichDir);
 			break;
 		case MOTOR2:
-			motor2.sinceStart = 0;
-			while (motor2.sinceStart < BUDGE_TIME) {
-				motor2.budge(whichDir);
-			}
+			motor2.budge(whichDir);
 			break;
 		case MOTOR3:
-			motor3.sinceStart = 0;
 			motor3.budge(whichDir);
 			break;
 		case MOTOR4:
-			motor4.sinceStart = 0;
 			motor4.budge(whichDir);
 			break;
 		case MOTOR5:
 			motor5.budge(whichDir);
 			break;
 		case MOTOR6:
-			motor6.sinceStart = 0;
-			while (motor6.sinceStart < BUDGE_TIME) {
-				motor6.budge(whichDir);
-			}
+			motor6.budge(whichDir);
 			break;
 	}
-whichMotor = 0;
+whichMotor = 0; // this was a quick fix so that at the next loop it will wait for a new message
 
 }
 
