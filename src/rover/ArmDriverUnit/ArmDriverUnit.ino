087c41cce49466ec1ad449a65128f84d92f3f61a
Josh Glazer <vashmata@gmail.com>
2018-08-26 22:50:29 -0400

    [#24] Parsing functionality inside class ParseSerial. Budge function controls speed,time,direction and controls all 3 motor types. Servo jitter probably resolved. Input pins are initialized in pinSetup.

--- src/rover/ArmDriverUnit/ArmDriverUnit.ino
+++ src/rover/ArmDriverUnit/ArmDriverUnit.ino
@@ -1,44 +1,44 @@
 /*
   TODO:
-  -implement simple manual control:
+  -make sure there is  max/min angle limitation, either with encoders or with default speed/time and counting buttonclicks
+  -suggesgtion 1 pinmode input, 2 is micros pwm instead of analogwrite (timer is bad?)
+  -(always) implement error checking, remove parts later on if it's too slow (tatum doubts)
+  -(always) clean up code
+  -(later) implement power management? sleep mode?
+  -(later) implement PID for automatic control
+    -update parsing function to take in angles or just go directly to ROSserial, probably the former
+  -(done-ish) fix issue where i need to create the motor objects inside the motordriver header
+  -(next) solve many problems by creating motor type subclasses
+  
+  -(priority) implement simple manual control:
    -(done) parsing procedure can process what tatum sends
    -(done) servo and dc motor control work and behave the same way
-   
-   -put parsing code into an actual function (move variables out of this file)
-   -instead of seting whichMotor to 0, perform some kind of check so that the motor doesn't spin forever
+   -(done) direction, speed and time are all options and they have default values
+   -(done) leave serial.print debugging messages unless it really takes too much time (doubt)
+   -(done) parsing code is in an object, may revert later
+   -(probably done, need to test)figure out how to solve the servo jitter. setting pins to INPUT probably fixes it
+   -(done, need abtin's circuit, but test with 3.3 for now) figure out how to step up to 5v for the sabertooth
+   -(now) connect and test stepper and stepper driver with Teensy code
+   -instead of setting whichMotor to 0, perform some kind of check so that the motor doesn't spin forever
    -figure out how to send motor info from parsing function to budgeMotor etc (move motor object creation into setup() )
-   
-   -import stepper library for simple manual control
-   -replace strtok with strtok_r when implementing interrupts
-   -connect and test stepper and stepper driver with Teensy code
-   
    -determine the actual clockwise and counter-clockwise directions of motors based on their wiring in the arm itself
-   -comment out serial.print debugging messages? maybe not
    
-   -decide whether to use abtin's software interrupt or simpler method for simultaneous motor control
+   -(next) replace strtok with strtok_r when implementing interrupts
+   -(next step) decide whether to use abtin's software interrupt or simpler method for simultaneous motor control
     -implement appropriate software interrupts that can take input from PID or manual control functions?
     -decide frequency of motor control loop: figure out estimated time for loop
     -otherwise figure out a simpler way of doing it
    -implement heartbeat after deciding on best timer for it (see below)
-   
-   -implement speed control or time control, decide what value to fix the one not being controlled
-   -figure out how to solve the servo jitter
-   -figure out how to step up to 5v for the sabertooth
-     
--fix issue where i need to create the motor objects inside the motordriver header
-   -(done): the issue is a circular dependency solved by placing the encoder count variables elsewhere
-  
-  -external interrupts for encoders and limit switches
+    
+  -(next next step) external interrupts for encoders and limit switches
    -determine whether it's worth it to use the built in quadrature decoders
    -rewrite all the register bit variables to use teensy registers for encoder/limit switch interrupts
    -rewrite the motor control with timers for teensy, ensure control for all motors
-  -incorporate limit switches for homing position on all motors
-  -implement homing function on boot
+   -incorporate limit switches for homing position on all motors
+   -implement homing function on boot
+   -(next) import stepper library for simple manual control or write fancy code
    
-  -implement PID for automatic control
-	-update parsing function to take in angles or just go directly to ROSserial, probably the former
-	
-  -timers
+  -(next step) timers
    -systick is normally a heartbeat type thing
    -lptmr runs even on low power mode, maybe this should be heartbeat instead?
    -pit is used for intervaltimer objects, there are 4 and they work like interrupts
@@ -47,34 +47,25 @@
     -teensy pwm page mentions ftm and tpm timers which aren't mentioned in the page with other timesr
     -ftm+tpm has quadrature decoder?
     -tpm is 2-8 channel timer with pwm, 16bit counter, 2 channels for pwm
-   
-  -implement error checking?
-  -implement power management? sleep mode?
  */
 
 #include "PinSetup.h"
 #include "ArmMotor.h"
+#include "SerialParser.h"
 
-// definitions and variables for parsing function: to put in self-contained header file
-#define BAUD_RATE 115200 // serial baud rate
-#define BUFFER_SIZE 100  // size of the buffer for the serial commands
-char serialBuffer[BUFFER_SIZE]; // serial buffer used for early- and mid-stage tesing without ROSserial
-int whichMotor;
-int whichDir;
-int whichSpeed; int tempSpeedVar;
-// int whichTime; int tempTimeVar;
+SerialParser serialParser;
 
-// these objects were created outside of the setup function because the update() proto-code isn't the right place yet
+// these objects were created outside of the setup function because the update() proto-code isn't in the right place yet
 ArmMotor motor1(MOTOR1); // base stepper (rotation)
 ArmMotor motor2(MOTOR2); // shoulder dc (flexion)
 ArmMotor motor3(MOTOR3); // elbow stepper (flexion)
 ArmMotor motor4(MOTOR4); // wrist stepper (flexion)
 ArmMotor motor5(MOTOR5); // wrist servo (rotation)
 ArmMotor motor6(MOTOR6); // end effector servo (pinching)
-
+  
 void setup() {
   pinSetup();
-  Serial.begin(BAUD_RATE);
+  
   // place armmotor object creations here once code is cleaned up
 }
 
@@ -82,107 +73,36 @@ void loop() {
   
   //digitalWrite(LED_PIN, led_status);
 
-  if (Serial.available()) { // if a message was sent to the Teensy
-    Serial.readBytesUntil(10, serialBuffer, BUFFER_SIZE); // read through it until NL
-    Serial.print("GOT: "); Serial.println(serialBuffer); // send back what was received
+	serialParser.parseSerial();
 
-    //parseCommand(serialBuffer);           //parse the string
+  int whichMotor = serialParser.whichMotor;
+	int whichDir = serialParser.whichDir;
+	int whichSpeed = serialParser.whichSpeed;
+	unsigned int whichTime = serialParser.whichTime;
 	
-	/* 
-	 * instead of using the parse function i dumped the code here for now.
-	 * This code figures out which motor and which direction, code further
-	 * down actually tells the motors to move
-	 */
-	
-	//strtok_r() splits message by a delimiter string
-	char* msgElem = strtok(serialBuffer, " "); // look for first element (first tag)
-	if (String(msgElem) == "motor"){ // msgElem is a char array so it's safer to convert to string first
-		msgElem = strtok(NULL, " "); // go to next msg element (motor number)
-		switch(*msgElem){ // determines which motor being used
-			case '1':
-				whichMotor = MOTOR1;
-				Serial.print("motor 1");
-				break;
-			case '2':
-				whichMotor = MOTOR2;
-				Serial.print("motor 2");
-				break;
-			case '3':
-				whichMotor = MOTOR3;
-				Serial.print("motor 3");
-				break;
-			case '4':
-				whichMotor = MOTOR4;
-				Serial.print("motor 4");
-				break;
-			case '5':
-				whichMotor = MOTOR5;
-				Serial.print("motor 5");
-				break;
-			case '6':
-				whichMotor = MOTOR6;
-				Serial.print("motor 6");
-				break;
-		}
-		Serial.print("motor"); Serial.println(whichMotor);
-		msgElem = strtok(NULL, " "); //find the next message element (direction tag)
-		if (String(msgElem) == "direction"){ // msgElem is a char array so it's safer to convert to string first
-			msgElem = strtok(NULL, " "); // go to next msg element (direction)
-			switch(*msgElem){ // determines motor direction
-				case '0': // arbitrarily (for now) decided 0 is clockwise
-					whichDir = CLOCKWISE;
-					Serial.println("clockwise");
-					break;
-				case '1': // arbitrarily (for now) decided 1 is counter-clockwise
-					whichDir = COUNTER_CLOCKWISE;
-					Serial.println("counter-clockwise");
-					break;
-			}
-			msgElem = strtok(NULL, " "); // find the next message element (time tag)
-			if (String(msgElem) == "speed"){ // msgElem is a char array so it's safer to convert to string first
-			    msgElem = strok(NULL, " "); // find the next message element (integer representing speed level)
-				tempSpeedVar = *msgElem - '0'; // example '3' - '0' = 3
-				if (tempSpeedVar <= 9){ // make sure the subtraction made sense. If it's above 9, it doesn't
-					whichSpeed = tempSpeedVar; // set the actual speed
-					Serialprint("speed level: "); Serial.println(whichSpeed);
-				}
-			}
-			/*msgElem = strtok(NULL, " "); //find the next message element (time tag)
-			if (*msgElem == 'time'){ // msgElem is a char array so it's safer to convert to string first
-				msgElem = strtok(NULL, " "); //find the next message element (time in seconds)
-				// because of how this works it only takes 0-3 sec, must rework later
-				tempTimeVar = *msgElem - '0'; // example: '3' - '0' = 3
-				if (tempTimeVar <= MAX_BUDGE_TIME){ // don't allow budge movements to last a long time
-					whichTime = tempTimeVar;
-					Serial.print(whichTime); Serial.println("s");
-				}
-			}*/
-		}
-	}
-	memset(serialBuffer, 0, BUFFER_SIZE); //empty the buffer
-  }
 	//proto code for ArmMotor::update() function?
 	switch(whichMotor){ // tells the appropriate motor to move with ArmMotor.budge()
 		case MOTOR1:
-			motor1.budge(whichDir);
+			motor1.budge(whichDir,whichSpeed,whichTime);
 			break;
 		case MOTOR2:
-			motor2.budge(whichDir);
+			motor2.budge(whichDir,whichSpeed,whichTime);
 			break;
 		case MOTOR3:
-			motor3.budge(whichDir);
+			motor3.budge(whichDir,whichSpeed,whichTime);
 			break;
 		case MOTOR4:
-			motor4.budge(whichDir);
+			motor4.budge(whichDir,whichSpeed,whichTime);
 			break;
 		case MOTOR5:
-			motor5.budge(whichDir);
+			motor5.budge(whichDir,whichSpeed,whichTime);
 			break;
 		case MOTOR6:
-			motor6.budge(whichDir);
+			motor6.budge(whichDir,whichSpeed,whichTime);
 			break;
 	}
-whichMotor = 0; // this was a quick fix so that at the next loop it will wait for a new message
-
+ whichMotor=0; // this was a quick fix so that at the next loop it will wait for a new message
+  
 }
+
 
--- src/rover/ArmDriverUnit/ArmMotor.h
+++ src/rover/ArmDriverUnit/ArmMotor.h
@@ -4,6 +4,9 @@
 #include "PinSetup.h"
 #include "AbtinEncoder.h"
 
+//#include <AccelStepper.h>
+//#include <MultiStepper.h>
+
 /*
    m1 base stepper (rotation)
    m2 shoulder dc (flexion)
@@ -11,8 +14,8 @@
    m4 wrist stepper (flexion
    m5 wrist servo (rotation)
    m6 end effector servo (pinching)
- */
- 
+*/
+
 // 3 pwm pins
 // 4-6 limit switch interrupt pins
 // 8-14 encoder interrupt pins
@@ -32,13 +35,37 @@
 enum motor_code {MOTOR1 = 1, MOTOR2, MOTOR3, MOTOR4, MOTOR5, MOTOR6};
 enum motor_type {DC_MOTOR, STEPPER_MOTOR, SERVO_MOTOR};
 enum motor_direction {CLOCKWISE, COUNTER_CLOCKWISE};
+enum motor_speed {SPEED0, SPEED1, SPEED2, SPEED3};
 
-#define BUDGE_TIME 1000
-//#define MAX_BUDGE_TIME 3 // don't allow budges to last very long
+#define MAX_SPEED 3
+#define DEFAULT_SPEED 0
+
+#define PWM_STOP 127
+// speed 0(+/-32)
+#define PWM_CW0 160
+#define PWM_CCW0 96
+// speed 1 (+/-64)
+#define PWM_CW1 192
+#define PWM_CCW1 64
+// speed 2 (+/-96)
+#define PWM_CW2 224
+#define PWM_CCW2 32
+// speed 3 (+/-127)
+#define PWM_CW3 255
+#define PWM_CCW3 1
+
+#define STEP_INTERVAL0 10
+#define STEP_INTERVAL1 25
+#define STEP_INTERVAL2 50
+#define STEP_INTERVAL3 100
+
+#define MAX_BUDGE_TIME 3000
+#define MIN_BUDGE_TIME 100
+#define DEFAULT_BUDGE_TIME 500
 
 /*
   unsigned int c0 = 1600;  // was 2000 * sqrt( 2 * angle / accel )
-  
+
   struct stepperInfo {
   void (*dirFunc)(int);
   void (*stepFunc)();
@@ -90,34 +117,34 @@ enum motor_direction {CLOCKWISE, COUNTER_CLOCKWISE};
 class ArmMotor {
   public:
     //volatile int encoderCount;
-    elapsedMillis sinceStart = 0;
+    elapsedMillis sinceStart = 0; // for time of motor budging
+    elapsedMillis sinceStep = 0; // for time between stepper steps
 
     ArmMotor(int code);
 
     void setMotorSpeed();
     float getCurrentAngle();
 
-    void budge(int rotationDir, int rotationSpeed);
+    void budge(int budgeDir, int budgeSpeed, unsigned int budgeTime);
     void setDesiredAngle(float angle);
     void update();
   private:
-    int motorCode;
-    int motorType;
-	int pwmPin;
+    int motorCode, motorType; // code determines motor 1-6, type determines dc vs stepper vs servo
+    int pwmPin, enablePin, dirPin, stepPin; // first is for dc/servo, next 3 are for steppers
     float currentAngle;
     float desiredAngle;
     bool isMovementDone;
 
-}; //motor1(MOTOR1),motor2(MOTOR2),motor3(MOTOR3),motor4(MOTOR4),motor5(MOTOR5),motor6(MOTOR6);;
+};
 
-//Motor::Motor(int code): motorCode(code){}
-
-//void Motor::init(){
 ArmMotor::ArmMotor(int code): motorCode(code) {
   // this code only runs at boot so order of switch statement not important
   switch (motorCode) {
     case MOTOR1:
       motorType = STEPPER_MOTOR;
+      enablePin = M1_ENABLE_PIN;
+      dirPin = M1_DIR_PIN;
+      stepPin = M1_STEP_PIN;
       attachInterrupt(M1_ENCODER_A, m1_encoder_interrupt, CHANGE);
       attachInterrupt(M1_ENCODER_B, m1_encoder_interrupt, CHANGE);
       break;
@@ -129,21 +156,27 @@ ArmMotor::ArmMotor(int code): motorCode(code) {
       break;
     case MOTOR3:
       motorType = STEPPER_MOTOR;
+      enablePin = M3_ENABLE_PIN;
+      dirPin = M3_DIR_PIN;
+      stepPin = M3_STEP_PIN;
       attachInterrupt(M3_ENCODER_A, m3_encoder_interrupt, CHANGE);
       attachInterrupt(M3_ENCODER_B, m3_encoder_interrupt, CHANGE);
       break;
     case MOTOR4:
       motorType = STEPPER_MOTOR;
+      enablePin = M4_ENABLE_PIN;
+      dirPin = M4_DIR_PIN;
+      stepPin = M4_STEP_PIN;
       attachInterrupt(M4_ENCODER_A, m4_encoder_interrupt, CHANGE);
       attachInterrupt(M4_ENCODER_B, m4_encoder_interrupt, CHANGE);
       break;
     case MOTOR5:
       motorType = SERVO_MOTOR;
-	  pwmPin = M5_PWM_PIN;
+      pwmPin = M5_PWM_PIN;
       break;
     case MOTOR6:
       motorType = SERVO_MOTOR;
-	  pwmPin = M6_PWM_PIN;
+      pwmPin = M6_PWM_PIN;
       break;
   }
 }
@@ -155,42 +188,106 @@ ArmMotor::ArmMotor(int code): motorCode(code) {
    tell the ISR how to move. but for now I can just make simple code that does the thing, I suppose.
 */
 
-void ArmMotor::budge(int rotationDir, rotationSpeed){
+void ArmMotor::budge(int budgeDir = CLOCKWISE, int budgeSpeed = DEFAULT_SPEED, unsigned int budgeTime = DEFAULT_BUDGE_TIME) {
   // arranged in order of which motor is predicted to be controlled the most
-  switch (motorType) {
-    case STEPPER_MOTOR:
-      break;
-    case DC_MOTOR:
-      Serial.println("starting");
-      sinceStart = 0;
-      if (rotationDir == CLOCKWISE) {
-        analogWrite(pwmPin, 120);
-        while (sinceStart < BUDGE_TIME) ;
-        analogWrite(pwmPin, 0);
-      }
-      if (rotationDir == COUNTER_CLOCKWISE) {
-        analogWrite(pwmPin, 60);
-        while (sinceStart < BUDGE_TIME) ;
-        analogWrite(pwmPin, 0);
-      }
-      Serial.println("stopping");
-      break;
-      break;
-    case SERVO_MOTOR:
-      Serial.println("starting");
-      sinceStart = 0;
-      if (rotationDir == CLOCKWISE) {
-        analogWrite(pwmPin, 120);
-        while (sinceStart < BUDGE_TIME) ;
-        analogWrite(pwmPin, 90);
-      }
-      if(rotationDir==COUNTER_CLOCKWISE){
-		analogWrite(pwmPin, 60);
-		while (sinceStart < BUDGE_TIME) ;
-		analogWrite(pwmPin, 90);
-      }
-	  Serial.println("stopping");
-      break;
+  int cwSpeed, ccwSpeed, stepInterval;
+  if (budgeDir <= 1 && budgeSpeed <= MAX_SPEED && budgeTime <= MAX_BUDGE_TIME && budgeTime >= MIN_BUDGE_TIME) {
+    switch (motorType) {
+      case STEPPER_MOTOR:
+        digitalWriteFast(enablePin, HIGH);
+        switch (budgeDir) {
+          case CLOCKWISE:
+            digitalWriteFast(dirPin, HIGH);
+            break;
+          case COUNTER_CLOCKWISE:
+            digitalWriteFast(dirPin, LOW);
+            break;
+        }
+        switch (budgeSpeed) {
+          case 0:
+            stepInterval = STEP_INTERVAL0;
+            break;
+          case 1:
+            stepInterval = STEP_INTERVAL1;
+            break;
+          case 2:
+            stepInterval = STEP_INTERVAL2;
+            break;
+          case 3:
+            stepInterval = STEP_INTERVAL3;
+            break;
+        }
+        sinceStart = 0;
+        while (sinceStart < budgeTime) {
+          // implement speed control
+          digitalWriteFast(stepPin, HIGH);
+          digitalWriteFast(stepPin, LOW);
+          sinceStep = 0;
+		  while (sinceStep < stepInterval) ;
+        }
+        digitalWriteFast(enablePin, LOW);
+        break;
+      case DC_MOTOR: // code slightly different from pwm now because of pinmodes in servo
+        switch (budgeSpeed) {
+          case 0:
+            cwSpeed = PWM_CW0; ccwSpeed = PWM_CCW0;
+            break;
+          case 1:
+            cwSpeed = PWM_CW1; ccwSpeed = PWM_CCW1;
+            break;
+          case 2:
+            cwSpeed = PWM_CW2; ccwSpeed = PWM_CCW2;
+            break;
+          case 3:
+            cwSpeed = PWM_CW3; ccwSpeed = PWM_CCW3;
+            break;
+        }
+        Serial.println("starting");
+        sinceStart = 0;
+        if (budgeDir == CLOCKWISE) {
+          analogWrite(pwmPin, cwSpeed);
+          while (sinceStart < budgeTime) ;
+          analogWrite(pwmPin, PWM_STOP);
+        }
+        if (budgeDir == COUNTER_CLOCKWISE) {
+          analogWrite(pwmPin, ccwSpeed);
+          while (sinceStart < budgeTime) ;
+          analogWrite(pwmPin, PWM_STOP);
+        }
+        Serial.println("stopping");
+        break;
+      case SERVO_MOTOR:
+        switch (budgeSpeed) {
+          case 0:
+            cwSpeed = PWM_CW0; ccwSpeed = PWM_CCW0;
+            break;
+          case 1:
+            cwSpeed = PWM_CW1; ccwSpeed = PWM_CCW1;
+            break;
+          case 2:
+            cwSpeed = PWM_CW2; ccwSpeed = PWM_CCW2;
+            break;
+          case 3:
+            cwSpeed = PWM_CW3; ccwSpeed = PWM_CCW3;
+            break;
+        }
+        Serial.println("starting");
+		pinMode(pwmPin, OUTPUT);
+        sinceStart = 0;
+        if (budgeDir == CLOCKWISE) {
+          analogWrite(pwmPin, cwSpeed);
+          while (sinceStart < budgeTime) ;
+          analogWrite(pwmPin, PWM_STOP);
+        }
+        if (budgeDir == COUNTER_CLOCKWISE) {
+          analogWrite(pwmPin, ccwSpeed);
+          while (sinceStart < budgeTime) ;
+          analogWrite(pwmPin, PWM_STOP);
+        }
+		pinMode(pwmPin, INPUT);
+        Serial.println("stopping");
+        break;
+    }
   }
 }
 
