3a83d6dd573f9de7e73dce13ad139bd003e0cb28
Tatum Alenko <tatum.alenko@gmail.com>
2018-08-28 12:02:20 -0400

    [#24] Convert attributes to properties.

--- robot/basestation/Microcontroller.py
+++ robot/basestation/Microcontroller.py
@@ -1,42 +1,48 @@
-import serial
-
-
 class Microcontroller:
     """Microcontroller or SoaC class acting as a `serial` wrapper.
 
-    This will act as the mother class to represent the Arduino (Microcontroller) 
+    This will act as the mother class to represent the Arduino (Microcontroller)
     that will contain other class component objects thanks to the wonders of
     OO composition design patterns.
 
     """
 
-    def __init__(self, name, port, motors):
+    def __init__(self, name, serial_port, motors):
         self.name = name
-        self.set_port(port)
-        self.set_motors(motors)
-        print(port.get_timeout())
-        self.serial = serial.Serial(
-            port.get_path(), port.get_baudrate(), timeout=port.get_timeout())
-
-    def set_port(self, port):
-        self.port = port
-
-    def get_port(self):
-        return self.port
-
-    def set_motors(self, motors):
+        self.serial_port = serial_port
         self.motors = motors
 
-    def get_motors(self):
-        return self.motors
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def serial_port(self):
+        return self.__serial_port
+
+    @serial_port.setter
+    def setter(self, serial_port):
+        self.__serial_port = serial_port
+
+    @property
+    def motors(self):
+        return self.__motors
+
+    @motors.setter
+    def motors(self, motors):
+        self.__motors = motors
 
     def write(self, name, data):
         for motor in self.motors:
             if motor.name in name:
                 print(data)
-                motor.write(self.serial, data)
+                motor.write(self.serial_port, data)
 
     def read(self, name):
         for motor in self.motors:
             if motor.name in name:
-                return motor.read(self.serial)
+                return motor.read(self.serial_port)
