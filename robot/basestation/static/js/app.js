01397331620981a59927fa139a21e506e41e263b
Tatum Alenko <tatum.alenko@gmail.com>
2018-09-03 23:16:14 -0400

    [#24] Change `click` events to `mousedown` events. Add `mouseup` event to all `mousedown` bound events in order to call `clearInterval()` of `mousedown` handler and prevent infinite callback of `mousedown` event.

--- robot/basestation/static/js/app.js
+++ robot/basestation/static/js/app.js
@@ -7,155 +7,164 @@ $(function() {
             this.eventHandlers.forEach(eventHandler => this.bindEvent(eventHandler));
         },
         bindEvent: function(e) {
-            e.$el.bind(e.event, function() {
+            const binder = function() {
                 $.getJSON(e.route, e.handler);
+                intervalId = setInterval(function() {$.getJSON(e.route, e.handler);}, 100);
                 return false;
+            };
+            e.$el.bind(e.event, binder);
+            e.$el.bind("mouseup", function() {
+                clearInterval(intervalId);
             });
+            
             console.log('Bound ' + e.event + ' handler for', e.$el);
+
         },
         eventHandlers: [
             {
-                $el: $('a#click_btn_pitch_up'),
-                event: "click",
-                route: "/click_btn_pitch_up",
+                $el: $('a#btn_pitch_up'),
+                event: "mousedown",
+                route: "/mousedown_btn_pitch_up",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_pitch_down'),
-                event: "click",
-                route: "/click_btn_pitch_down",
+                $el: $('a#btn_pitch_down'),
+                event: "mousedown",
+                route: "/mousedown_btn_pitch_down",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_roll_left'),
-                event: "click",
-                route: "/click_btn_roll_left",
+                $el: $('a#btn_roll_left'),
+                event: "mousedown",
+                route: "/mousedown_btn_roll_left",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_roll_right'),
-                event: "click",
-                route: "/click_btn_roll_right",
+                $el: $('a#btn_roll_right'),
+                event: "mousedown",
+                route: "/mousedown_btn_roll_right",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_claw_open'),
-                event: "click",
-                route: "/click_btn_claw_open",
+                $el: $('a#btn_claw_open'),
+                event: "mousedown",
+                route: "/mousedown_btn_claw_open",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_claw_close'),
-                event: "click",
-                route: "/click_btn_claw_close",
+                $el: $('a#btn_claw_close'),
+                event: "mousedown",
+                route: "/mousedown_btn_claw_close",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_arm_up'),
-                event: "click",
-                route: "/click_btn_arm_up",
+                $el: $('a#btn_arm_up'),
+                event: "mousedown",
+                route: "/mousedown_btn_arm_up",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_arm_down'),
-                event: "click",
-                route: "/click_btn_arm_down",
+                $el: $('a#btn_arm_down'),
+                event: "mousedown",
+                route: "/mousedown_btn_arm_down",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_arm_left'),
-                event: "click",
-                route: "/click_btn_arm_left",
+                $el: $('a#btn_arm_left'),
+                event: "mousedown",
+                route: "/mousedown_btn_arm_left",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_arm_right'),
-                event: "click",
-                route: "/click_btn_arm_right",
+                $el: $('a#btn_arm_right'),
+                event: "mousedown",
+                route: "/mousedown_btn_arm_right",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_arm_back'),
-                event: "click",
-                route: "/click_btn_arm_back",
+                $el: $('a#btn_arm_back'),
+                event: "mousedown",
+                route: "/mousedown_btn_arm_back",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_arm_forward'),
-                event: "click",
-                route: "/click_btn_arm_forward",
+                $el: $('a#btn_arm_forward'),
+                event: "mousedown",
+                route: "/mousedown_btn_arm_forward",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_motor1_ccw'),
-                event: "click",
-                route: "/click_btn_motor1_ccw",
+                $el: $('a#btn_motor1_ccw'),
+                event: "mousedown",
+                route: "/mousedown_btn_motor1_ccw",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_motor1_cw'),
-                event: "click",
-                route: "/click_btn_motor1_cw",
+                $el: $('a#btn_motor1_cw'),
+                event: "mousedown",
+                route: "/mousedown_btn_motor1_cw",
+                handler: function(data) { 
+                    setInterval(this, 1000);
+                }
+            },
+            {
+                $el: $('a#btn_motor2_ccw'),
+                event: "mousedown",
+                route: "/mousedown_btn_motor2_ccw",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_motor2_ccw'),
-                event: "click",
-                route: "/click_btn_motor2_ccw",
+                $el: $('a#btn_motor2_cw'),
+                event: "mousedown",
+                route: "/mousedown_btn_motor2_cw",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_motor2_cw'),
-                event: "click",
-                route: "/click_btn_motor2_cw",
+                $el: $('a#btn_motor3_ccw'),
+                event: "mousedown",
+                route: "/mousedown_btn_motor3_ccw",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_motor3_ccw'),
-                event: "click",
-                route: "/click_btn_motor3_ccw",
+                $el: $('a#btn_motor3_cw'),
+                event: "mousedown",
+                route: "/mousedown_btn_motor3_cw",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_motor3_cw'),
-                event: "click",
-                route: "/click_btn_motor3_cw",
+                $el: $('a#btn_motor4_ccw'),
+                event: "mousedown",
+                route: "/mousedown_btn_motor4_ccw",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_motor4_ccw'),
-                event: "click",
-                route: "/click_btn_motor4_ccw",
+                $el: $('a#btn_motor4_cw'),
+                event: "mousedown",
+                route: "/mousedown_btn_motor4_cw",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_motor4_cw'),
-                event: "click",
-                route: "/click_btn_motor4_cw",
+                $el: $('a#btn_motor5_ccw'),
+                event: "mousedown",
+                route: "/mousedown_btn_motor5_ccw",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_motor5_ccw'),
-                event: "click",
-                route: "/click_btn_motor5_ccw",
+                $el: $('a#btn_motor5_cw'),
+                event: "mousedown",
+                route: "/mousedown_btn_motor5_cw",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_motor5_cw'),
-                event: "click",
-                route: "/click_btn_motor5_cw",
+                $el: $('a#btn_motor6_ccw'),
+                event: "mousedown",
+                route: "/mousedown_btn_motor6_ccw",
                 handler: function(data) { }
             },
             {
-                $el: $('a#click_btn_motor6_ccw'),
-                event: "click",
-                route: "/click_btn_motor6_ccw",
-                handler: function(data) { }
-            },
-            {
-                $el: $('a#click_btn_motor6_cw'),
-                event: "click",
-                route: "/click_btn_motor6_cw",
+                $el: $('a#btn_motor6_cw'),
+                event: "mousedown",
+                route: "/mousedown_btn_motor6_cw",
                 handler: function(data) { }
             },
         ]
