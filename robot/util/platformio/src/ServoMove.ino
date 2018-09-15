c1c1ad783fd904574c0a3c0bda1a959d47da3bd5
PeterGhimself <petergranitski@gmail.com>
2018-09-14 23:15:12 -0400

    [#54] Modify arduino sketch to move one DC motor instead of two servos, tested with odroid

--- robot/util/platformio/src/ServoMove.ino
+++ robot/util/platformio/src/ServoMove.ino
@@ -1,12 +1,6 @@
-#include <Servo.h> 
- 
-Servo leftRearServo, rightRearServo;  // create servo object to control a servo 
-                                      // a maximum of eight servo objects can be created 
- 
-int SERVO_PIN_7 = 7;
-int SERVO_PIN_9 = 9;
-int REST = 90;
-int OFFSET = 10;
+int DC_MOTOR_PIN = 11; // PWM 
+int REST = 127;
+int OFFSET = 30;
 int CW = REST - OFFSET;
 int CCW = REST + OFFSET;
 int BUDGE_TIME = 100;
@@ -16,17 +10,15 @@ unsigned long currentTime = 0;
 
 void setup() 
 { 
-    rightRearServo.attach(SERVO_PIN_7);  // attaches the servo on pin 7 to the servo object 
-    rightRearServo.write(REST); // this actually stops the motor
-    leftRearServo.attach(SERVO_PIN_9); 
-    leftRearServo.write(REST);
     Serial.begin(BAUD_RATE);
+    pinMode(DC_MOTOR_PIN, OUTPUT);
+    analogWrite(DC_MOTOR_PIN, REST);
 } 
  
  
 void loop() 
 { 
-
+  
     if (Serial.available()) {
         char cmd = Serial.read();
         Serial.println("cmd: ");
@@ -34,23 +26,22 @@ void loop()
 
         if (cmd == 'w') {
             Serial.println("Moving Forward");
-            leftRearServo.write(CCW); // rotate clockwise
-            rightRearServo.write(CW); // rotate counter clockwise
+            // rotate clockwise
+            analogWrite(DC_MOTOR_PIN, CCW);
             delay(BUDGE_TIME);
             // stop
-            leftRearServo.write(REST);
-            rightRearServo.write(REST);
+            analogWrite(DC_MOTOR_PIN, REST);
             Serial.println("Stop");
         }
         else if (cmd == 's') {
             Serial.println("Moving Back");
-            leftRearServo.write(CW); // rotate counter clockwise
-            rightRearServo.write(CCW); // rotate clockwise
+            // rotate clockwise
+            analogWrite(DC_MOTOR_PIN, CW);
             delay(BUDGE_TIME);
             // stop
-            leftRearServo.write(REST);
-            rightRearServo.write(REST);
+            analogWrite(DC_MOTOR_PIN, REST);
             Serial.println("Stop");
         }
     }
 }
+
