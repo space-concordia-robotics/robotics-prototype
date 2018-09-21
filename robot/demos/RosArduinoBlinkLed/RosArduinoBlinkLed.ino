dce6f6b59d3e7f10f16a3ea644ef4092fe8927aa
PeterGhimself <petergranitski@gmail.com>
2018-09-21 16:37:19 -0400

    [#59] Modify sketch to act as subscriber with callback function which toggles builtin LED of Arduino

--- robot/demos/RosArduinoBlinkLed/RosArduinoBlinkLed.ino
+++ robot/demos/RosArduinoBlinkLed/RosArduinoBlinkLed.ino
@@ -1,6 +1,6 @@
 /*
- * rosserial Publisher Example
- * Prints "hello world!"
+ * rosserial Subscriber Example
+ * Blinks an LED on callback
  */
 
 /* Running the code
@@ -9,29 +9,34 @@
  run rosserial client application that forwards your arduino messages to the rest of ROS
  cmd: rosrun rosserial_python serial_node.py /dev/ttyUSB0 (or ACM0, whatever name of your arduino port is)
  To observe the messages being published
- cmd: rostopic echo chatter
+ cmd: rostopic pub toggle_led std_msgs/Empty --once
 */
 
 #include <ros.h>
-#include <std_msgs/String.h>
+#include <std_msgs/Empty.h>
+int BUILTIN_LED = 13;
 
-ros::NodeHandle  nh;
+ros::NodeHandle nh;
 
-std_msgs::String str_msg;
-ros::Publisher chatter("chatter", &str_msg);
+// must take a constant refrence of a message as argument
+void messageCallback(const std_msgs::Empty& toggle_msg) {
+    digitalWrite(BUILTIN_LED, HIGH-digitalRead(BUILTIN_LED)); // blink the LED
+}
 
-char hello[13] = "hello world!";
+// instantiate subscriber with name toggle_led and attach callback function to it
+ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCallback);
 
 void setup()
 {
-  nh.initNode();
-  nh.advertise(chatter);
+    pinMode(BUILTIN_LED, OUTPUT);
+    // initialize node handle, allowing to create publishers/subscribers and taking care of serial port comms
+    nh.initNode();
+    nh.subscribe(sub);
 }
 
 void loop()
 {
-  str_msg.data = hello;
-  chatter.publish( &str_msg );
+  // handle passing messages to the subscriber callback
   nh.spinOnce();
-  delay(1000);
+  delay(1);
 }
