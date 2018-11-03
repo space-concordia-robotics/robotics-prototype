271eac3fea6d8f215862fa4b816307c75ed8b6cc
Fauzi Ame <fauzi@eliquidscanada.com>
2018-11-03 18:34:57 -0400

    [#65] Update Rest and Maximum Velocity Values

--- robot/demos/SkidSteerServos/SkidSteeringDC.ino
+++ robot/demos/SkidSteerServos/SkidSteeringDC.ino
@@ -6,8 +6,8 @@ int prevThrottle = 49;
 int prevSteering = 49;
 int throttle, steering;
 // Initialize Left & Right Velocity variables to Rest
-int VR = 90;
-int VL = 90;
+int VR = 127;
+int VL = 127;
 SoftwareSerial bluetooth(7, 8);
 
 ArduinoBlue phone(bluetooth);
@@ -57,15 +57,15 @@ void loop() {
 // If statement for CASE 1: steering toward the RIGHT 
   if (steering <= 0 ) {
     deg = mapFloat(steering, -49, 0, -1, 1);
-    VR = map(throttle*deg, -49, 49,0,180);
-    VL = map((-1)*throttle, -49, 49,  0,180);
+    VR = map(throttle*deg, -49, 49,0,255);
+    VL = map((-1)*throttle, -49, 49,  0,255);
   }
 
 // If statement for CASE 1: steering toward the LEFT 
   if (steering >= 0 ) {
     deg = mapFloat(steering, 0, 49, 1, -1);
-    VR = map(throttle, -49, 49, 0,180);
-    VL = map((-1)*throttle*deg, -49, 49,  0,180);
+    VR = map(throttle, -49, 49, 0,255);
+    VL = map((-1)*throttle*deg, -49, 49,  0,255);
 
 } 
   
@@ -73,7 +73,7 @@ void loop() {
   if (prevThrottle != throttle || prevSteering != steering) {
     Serial.print("Throttle: "); Serial.print(throttle); Serial.print("\tSteering: "); Serial.println(steering);
     Serial.print(steering);Serial.print('>'); Serial.print(throttle);Serial.print('o');
-    Serial.print("Right Side: "); Serial.print(vr); Serial.print("\tLeft Side: "); Serial.println(vl);
+    Serial.print("Right Side: "); Serial.print(VR); Serial.print("\tLeft Side: "); Serial.println(VL);
    
     prevThrottle = throttle;
     prevSteering = steering;
