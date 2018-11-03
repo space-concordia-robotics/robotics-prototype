e7678eabb380a1ecd25261ca10eb3a6b07564e93
Fauzi Ame <fauzi@eliquidscanada.com>
2018-11-03 14:12:36 -0400

    Configure Code for DC Motors

--- robot/demos/SkidSteerServos/SkidSteeringDC.ino
+++ robot/demos/SkidSteerServos/SkidSteeringDC.ino
@@ -1,23 +1,9 @@
-#include <ArduinoBlue.h>
-
-#include <SoftwareSerial.h>
-
-#include <Servo.h>
-
-Servo myservo;
-Servo myservo1;
-Servo myservo2;
-Servo myservo3;
-Servo myservo4;
-Servo myservo5;
-
-
 int prevThrottle = 49;
 int prevSteering = 49;
 int throttle, steering;
 // Initialize Left & Right Velocity variables to Rest
-int vr = 90;
-int vl = 90;
+int VR = 90;
+int VL = 90;
 SoftwareSerial bluetooth(7, 8);
 
 ArduinoBlue phone(bluetooth);
@@ -25,15 +11,14 @@ ArduinoBlue phone(bluetooth);
 void setup() {
   // Attach Servos to pins:
   // Right side servos
-  myservo.attach(9);  
-  myservo1.attach(10);  
-  myservo2.attach(11);
+  DC_PIN_1 = 9;
+  DC_PIN_2 = 10;
+  DC_PIN_3 = 11;
   
   //Left side Servos
-  myservo3.attach(3);
-  myservo4.attach(5);
-  myservo5.attach(6);
-
+  DC_PIN_4 = 3;
+  DC_PIN_5 = 5;
+  DC_PIN_6 = 6;
 
   // initialize serial communications at 9600 bps:
   Serial.begin(9600); 
@@ -45,8 +30,6 @@ void setup() {
   Serial.println("setup complete");
 }
 
-float right = 90;
-float left = 90;
 float deg = 0;
 
 
@@ -54,6 +37,7 @@ float deg = 0;
  float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
 void loop() {
   
  // Lead Velocity Value from bluetooth controller. Values range from 0 to 90
@@ -65,20 +49,19 @@ void loop() {
   throttle -= 49;
   steering -= 49;
      
-  delay(200); // add some delay between reads
 
 // If statement for CASE 1: steering toward the RIGHT 
   if (steering <= 0 ) {
     deg = mapFloat(steering, -49, 0, -1, 1);
-    vr = map(throttle*deg, -49, 49,0,180);
-    vl = map((-1)*throttle, -49, 49,  0,180);
+    VR = map(throttle*deg, -49, 49,0,180);
+    VL = map((-1)*throttle, -49, 49,  0,180);
   }
 
 // If statement for CASE 1: steering toward the LEFT 
   if (steering >= 0 ) {
     deg = mapFloat(steering, 0, 49, 1, -1);
-    vr = map(throttle, -49, 49, 0,180);
-    vl = map((-1)*throttle*deg, -49, 49,  0,180);
+    VR = map(throttle, -49, 49, 0,180);
+    VL = map((-1)*throttle*deg, -49, 49,  0,180);
 
 } 
   
@@ -94,13 +77,13 @@ void loop() {
 
 
 // Write velocities for the Wheels on the RIGHT side
-  myservo.write(vr);              
-  myservo1.write(vr);              
-  myservo2.write(vr);
+  analogWrite(DC_PIN_1, VR);
+  analogWrite(DC_PIN_2, VR);
+  analogWrite(DC_PIN_3, VR);
   
 // Write velocities for the Wheels on the Left side  
-  myservo3.write(vl);
-  myservo4.write(vl);
-  myservo5.write(vl);
-
-  delay(100); // add some delay between reads
+  analogWrite(DC_PIN_4, VL);
+  analogWrite(DC_PIN_5, VL);
+  analogWrite(DC_PIN_6, VL);
+  
+delay(100); // add some delay between reads
