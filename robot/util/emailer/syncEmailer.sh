a2a3816bd7c5a3acad1365b16f7879280dc763c3
PeterGhimself <petergranitski@gmail.com>
2018-12-20 19:34:03 -0500

    [#78] Update synchEmailer.sh
    Adding safety check for creating dir for emailer.

--- robot/util/emailer/syncEmailer.sh
+++ robot/util/emailer/syncEmailer.sh
@@ -1,3 +1,14 @@
-cp ./* /home/odroid/emailer
-cd /home/odroid/emailer/
+EMAILER_DIR="/home/odroid/emailer"
+
+if [ ! -d $EMAILER_DIR ]; then
+    mkdir $EMAILER_DIR;
+else
+    echo "There already exists an emailer directory in /home/odroid"
+    echo "Please rename or delete the folder for this script to proceed"
+    exit 1
+fi
+
+cp ./* $EMAILER_DIR
+cd $EMAILER_DIR
 npm install
+
