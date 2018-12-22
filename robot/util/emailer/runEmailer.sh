38ee875cfe7c0e495f08a29fd5e9164f9f5a647a
PeterGhimself <petergranitski@gmail.com>
2018-12-21 22:25:27 -0500

    [#78] Add warning about possible mishap in runEmailer.sh

--- robot/util/emailer/runEmailer.sh
+++ robot/util/emailer/runEmailer.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+# when adding this script to be run on startup whether using cronjob or systemd services
+# make sure to replace the beginning "node" with the output of `which node`
+# otherwise it will not work
 node /home/odroid/emailer/emailIPAddress.js > /home/odroid/emailer/emailer.log
