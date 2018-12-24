7c67d120c8bacb102dcf4073e2031796b245db06
PeterGhimself <petergranitski@gmail.com>
2018-12-23 20:54:44 -0500

    [#78] Changes requested by @Davister in PR #79

--- robot/util/emailer/syncEmailer.sh
+++ robot/util/emailer/syncEmailer.sh
@@ -2,10 +2,6 @@ EMAILER_DIR="/home/odroid/emailer"
 
 if [ ! -d $EMAILER_DIR ]; then
     mkdir $EMAILER_DIR;
-else
-    echo "There already exists an emailer directory in /home/odroid"
-    echo "Please rename or delete the folder for this script to proceed"
-    exit 1
 fi
 
 cp ./* $EMAILER_DIR
