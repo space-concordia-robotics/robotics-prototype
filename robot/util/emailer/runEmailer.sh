75eb12cf81b863efd323ef8e302ae79938b61666
PeterGhimself <petergranitski@gmail.com>
2018-12-27 17:52:42 -0500

    [#72] Adjust runEmailer.sh to only run ip emailer if connection is not set to RoverOBC

--- robot/util/emailer/runEmailer.sh
+++ robot/util/emailer/runEmailer.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-# when adding this script to be run on startup whether using cronjob or systemd services
-# make sure to replace the beginning "node" with the output of `which node`
-# otherwise it will not work
-node /home/odroid/emailer/emailIPAddress.js > /home/odroid/emailer/emailer.log
+EMAILER_LOG="/home/odroid/emailer/emailer.log"
+
+activeConnection=$(nmcli connection show --active)
+echo "activeConnection: $activeConnection" > $EMAILER_LOG
+
+if [[ "$activeConnection" =~ "RoverOBC" ]]; then
+    echo "Rover configuration selected, exiting script" >> $EMAILER_LOG
+    exit 1
+else
+    # WARNING: when adding this script to be run on startup whether using cronjob or systemd services
+    # make sure to replace the beginning "node" with the direct path which you can check with the output of `which node`
+    # otherwise it will not work. Example: "/home/odroid/.nvm/versions/node/v10.13.0/bin/node"
+    node /home/odroid/emailer/emailIPAddress.js >> $EMAILER_LOG
+fi
