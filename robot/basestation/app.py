f69778022f0f5659eb248223817132d954ab40fd
Tatum Alenko <tatum.alenko@gmail.com>
2019-01-04 21:49:34 -0500

    [#74] Refactor retrieval of ip from environment variable
    Made the retrieval and parsing of the ROS_MASTER_URI env variable into its own function to avoid use of global. Also did some minor cleanup.

--- robot/basestation/app.py
+++ robot/basestation/app.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python3
-"""This is the controller page of the Flask GUI
-Flask is light-weight and modular so this is actually all we need to set up a simple HTML page"""
+#!/usr/bin/env python
+"""Flask server controller.
+
+Flask is light-weight and modular so this is actually all we need to set up a simple HTML page.
+"""
 
 import re
 import os
@@ -10,48 +12,52 @@ from flask import jsonify
 
 app = flask.Flask(__name__)
 
-# get rover's IP from global bash environment
-rover_ip = os.environ["ROS_MASTER_URI"]
-rover_ip = re.findall(r"(.*):\d+", rover_ip)[0]
-#print("rover_ip: " + rover_ip)
 
-rover_ip_raw = rover_ip.split("//")[1]
+def fetch_ros_master_uri():
+    """Get and parse ros master IP/URI from environment variable."""
 
-def run_bash(cmd):
-    """Runs bash command supplied as string,
-    executes it and returns tuple of output and error
+    rover_ip = os.environ["ROS_MASTER_URI"]
+    rover_ip = re.findall(r"(.*):\d+", rover_ip)[0]
+    #print("rover_ip: " + rover_ip)
+
+    rover_ip_raw = rover_ip.split("//")[1]
+
+    return rover_ip_raw
+
+
+def run_shell(cmd):
+    """Run script command supplied as string.
+
+    Returns tuple of output and error.
     """
     process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
 
     return output, error
 
+
 # Once we launch this, this will route us to the "/" page or index page and
 # automatically render the Robot GUI
 @app.route("/")
 def index():
-    """Current Landing page, the arm panel"""
+    """Current landing page, the arm panel."""
+    return flask.render_template("AsimovOperation.html", roverIP=fetch_ros_master_uri())
 
-    global rover_ip, rover_ip_raw
 
-    # get rover's IP from global bash environment
-    rover_ip = os.environ["ROS_MASTER_URI"]
-    rover_ip = re.findall(r"(.*):\d+", rover_ip)[0]
-    #print("rover_ip: " + rover_ip)
-
-    rover_ip_raw = rover_ip.split("//")[1]
-    #print("rover_ip_raw: " + rover_ip_raw)
-
-    return flask.render_template("AsimovOperation.html", roverIP=rover_ip)
-
-# Ping Request
 @app.route("/ping_rover")
 def ping_rover():
-    """Ping rover first directly with unix ping command,
-       then using ros ping_acknowledgment service.
-       Send results to frontend to write to console log."""
+    """Pings ROS_MASTER_URI and return response object with resulting outputs.
 
-    ping_output, error = run_bash("ping -c 1 " + rover_ip_raw)
+    Pings rover first directly with Unix ping command,
+    then using ros ping_acknowledgment service.
+
+    Returns JSON object with the following fields:
+    success -- whether requests was successful
+    ping_msg -- output of Unix ping command
+    ros_msg -- output of the ROS ping_acknowledgment service
+    """
+
+    ping_output, error = run_shell("ping -c 1 " + fetch_ros_master_uri())
     ping_output = ping_output.decode()
 
     print("Output: " + ping_output)
@@ -67,7 +73,7 @@ def ping_rover():
     if error:
         print("Error: " + error.decode())
 
-    ros_output, error = run_bash("rosrun ping_acknowledgment ping_response_client.py hello")
+    ros_output, error = run_shell("rosrun ping_acknowledgment ping_response_client.py hello")
     ros_output = ros_output.decode()
 
     print("Pinging rover")
@@ -78,66 +84,79 @@ def ping_rover():
 
     return jsonify(success=True, ping_msg=ping_output, ros_msg=ros_output)
 
+
 # Automatic controls
 @app.route("/click_btn_pitch_up")
 def click_pitch_up():
     print("click_btn_pitch_up")
     return ""
 
+
 @app.route("/click_btn_pitch_down")
 def click_btn_pitch_down():
     print("click_btn_pitch_down")
     return ""
+
 
 @app.route("/click_btn_roll_left")
 def click_btn_roll_left():
     print("click_btn_roll_left")
     return ""
 
+
 @app.route("/click_btn_roll_right")
 def click_btn_roll_right():
     print("click_btn_roll_right")
     return ""
+
 
 @app.route("/click_btn_claw_open")
 def click_btn_claw_open():
     print("click_btn_claw_open")
     return ""
 
+
 @app.route("/click_btn_claw_close")
 def click_btn_claw_close():
     print("click_btn_claw_close")
     return ""
+
 
 @app.route("/click_btn_arm_up")
 def click_btn_arm_up():
     print("click_btn_arm_up")
     return ""
 
+
 @app.route("/click_btn_arm_down")
 def click_btn_arm_down():
     print("click_btn_arm_down")
     return ""
+
 
 @app.route("/click_btn_arm_left")
 def click_btn_arm_left():
     print("click_btn_arm_left")
     return ""
 
+
 @app.route("/click_btn_arm_right")
 def click_btn_arm_right():
     print("click_btn_arm_right")
     return ""
+
 
 @app.route("/click_btn_arm_back")
 def click_btn_arm_back():
     print("click_btn_arm_back")
     return ""
 
+
 @app.route("/click_btn_arm_forward")
 def click_btn_arm_forward():
     print("click_btn_arm_forward")
     return ""
+
 
 # Manual controls
 @app.route("/click_btn_motor1_ccw")
@@ -145,62 +164,72 @@ def click_btn_motor1_ccw():
     print("click_btn_motor1_ccw")
     return ""
 
+
 @app.route("/click_btn_motor1_cw")
 def click_btn_motor1_cw():
     print("click_btn_motor1_cw")
     return ""
+
 
 @app.route("/click_btn_motor2_ccw")
 def click_btn_motor2_ccw():
     print("click_btn_motor2_ccw")
     return ""
 
+
 @app.route("/click_btn_motor2_cw")
 def click_btn_motor2_cw():
     print("click_btn_motor2_cw")
     return ""
+
 
 @app.route("/click_btn_motor3_ccw")
 def click_btn_motor3_ccw():
     print("click_btn_motor3_ccw")
     return ""
 
+
 @app.route("/click_btn_motor3_cw")
 def click_btn_motor3_cw():
     print("click_btn_motor3_cw")
     return ""
+
 
 @app.route("/click_btn_motor4_ccw")
 def click_btn_motor4_ccw():
     print("click_btn_motor4_ccw")
     return ""
 
+
 @app.route("/click_btn_motor4_cw")
 def click_btn_motor4_cw():
     print("click_btn_motor4_cw")
     return ""
+
 
 @app.route("/click_btn_motor5_ccw")
 def click_btn_motor5_ccw():
     print("click_btn_motor5_ccw")
     return ""
 
+
 @app.route("/click_btn_motor5_cw")
 def click_btn_motor5_cw():
     print("click_btn_motor5_cw")
     return ""
+
 
 @app.route("/click_btn_motor6_ccw")
 def click_btn_motor6_ccw():
     print("click_btn_motor6_ccw")
     return ""
 
+
 @app.route("/click_btn_motor6_cw")
 def click_btn_motor6_cw():
     print("click_btn_motor6_cw")
     return ""
 
-app.run(
-    debug=True
-)
+
+app.run(debug=True)
 # add param `host= '0.0.0.0'` if you want to run on your machine's IP address
