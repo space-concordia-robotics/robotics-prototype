5b61874aa955040251f194da4f6873b373f228dc
PeterGhimself <petergranitski@gmail.com>
2019-01-21 18:24:23 -0500

    [#77] Modify Teensy Blink LED sketch to test switching between and communicating with all 4 serial channels at once

--- robot/rover/TeensyBlinkLEDSerial/TeensyBlinkLEDSerial.ino
+++ robot/rover/TeensyBlinkLEDSerial/TeensyBlinkLEDSerial.ino
@@ -3,11 +3,21 @@
 // to 1.8V for the Odroid RX, otherwise you risk damaging the odroid RX pin
 // The teensy is more tolerant so you don't necessarily need to
 // convert the logic level from 1.8 to 3.3, although we still will as a redundancy
-int LED_PIN = 2;
+
+int LED_DEV_0 = 2;
+int LED_DEV_1 = 3;
+int LED_DEV_2 = 4;
+int LED_DEV_3 = 5;
 
 void setup() {
-    pinMode(LED_PIN, OUTPUT);
+    pinMode(LED_DEV_0, OUTPUT);
+    pinMode(LED_DEV_1, OUTPUT);
+    pinMode(LED_DEV_2, OUTPUT);
+    pinMode(LED_DEV_3, OUTPUT);
+
+    // to show a sign of life
     pinMode(LED_BUILTIN, OUTPUT);
+
     digitalWrite(LED_BUILTIN, HIGH);
 
     // To combat latency, try higher baudrates like
@@ -21,6 +31,51 @@ void setup() {
 void loop() {
     // only respond to UART serial messages (not USB)
     if (Serial1.available()) {
+        String cmd = Serial1.readString().trim();
+        Serial.print("cmd: ");
+        Serial.println(cmd);
+
+        if (cmd.equals("1")) {
+            digitalWrite(LED_PIN, HIGH);
+            //Serial1.println("HIGH");
+            Serial.println("HIGH");
+        }
+        else if (cmd.equals("0")) {
+            digitalWrite(LED_PIN, LOW);
+            //Serial1.println("LOW");
+            Serial.println("LOW");
+        }
+    if (Serial2.available()) {
+        String cmd = Serial1.readString().trim();
+        Serial.print("cmd: ");
+        Serial.println(cmd);
+
+        if (cmd.equals("1")) {
+            digitalWrite(LED_PIN, HIGH);
+            //Serial1.println("HIGH");
+            Serial.println("HIGH");
+        }
+        else if (cmd.equals("0")) {
+            digitalWrite(LED_PIN, LOW);
+            //Serial1.println("LOW");
+            Serial.println("LOW");
+        }
+    if (Serial3.available()) {
+        String cmd = Serial1.readString().trim();
+        Serial.print("cmd: ");
+        Serial.println(cmd);
+
+        if (cmd.equals("1")) {
+            digitalWrite(LED_PIN, HIGH);
+            //Serial1.println("HIGH");
+            Serial.println("HIGH");
+        }
+        else if (cmd.equals("0")) {
+            digitalWrite(LED_PIN, LOW);
+            //Serial1.println("LOW");
+            Serial.println("LOW");
+        }
+    if (Serial4.available()) {
         String cmd = Serial1.readString().trim();
         Serial.print("cmd: ");
         Serial.println(cmd);
