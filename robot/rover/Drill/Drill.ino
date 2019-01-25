2c7cd5bb5a7410a010ece532e5cde6b7e2d66162
zouhairghali <zouhair_ghali@hotmail.com>
2019-01-25 17:50:32 -0500

    [#84] Improve command possibilites
    Specifically added indefinite motor control, changed pins and input commands

--- robot/rover/Drill/Drill.ino
+++ robot/rover/Drill/Drill.ino
@@ -1,9 +1,7 @@
-
-
-int Drill = 9;
+int Drill = 5;
 int Drill_direction = 3;
-int Pump = 5;
-int Pump_direction = 7;
+int Pump = 9;
+int Pump_direction = 6;
 
 void setup() {
   
@@ -24,34 +22,67 @@ void loop() {
   if(Serial.available()){
     char cmd = Serial.read();
 
-    if(cmd=='w'){
+    if(cmd=='j'){
       
       digitalWrite(Drill_direction,HIGH);
       analogWrite(Drill,100);
-      delay(500);
+      delay(10000);
       analogWrite(Drill,0);
     }
-    if(cmd=='s'){
+    if(cmd=='l'){
       
       digitalWrite(Drill_direction,LOW);
       analogWrite(Drill,100);
-      delay(500);
+      delay(10000);
       analogWrite(Drill,0);
     }
-    if(cmd=='r'){
+    if(cmd=='u'){
       
       digitalWrite(Pump_direction,HIGH);
       analogWrite(Pump,100);
-      delay(500);
+      delay(10000);
       analogWrite(Pump,0);
     }
-    if(cmd=='f'){
+    if(cmd=='o'){
       
       digitalWrite(Pump_direction,LOW);
       analogWrite(Pump,100);
-      delay(500);
+      delay(10000);
       analogWrite(Pump,0);
     }
+    if(cmd=='a'){
+      //turns drill counter-clockwise
+      digitalWrite(Drill_direction,HIGH);
+      analogWrite(Drill,100);
+      
+    }
+    if(cmd=='d'){
+      //turns drill clockwise
+      digitalWrite(Drill_direction,LOW);
+      analogWrite(Drill,100);
+      
+    }
+    if(cmd=='s'){
+      //stops drill
+      analogWrite(Drill,0);
+    }
+    if(cmd=='e'){
+       //turns pump clockwise
+      digitalWrite(Pump_direction,HIGH);
+      analogWrite(Pump,100);
+      
+    }
+    if(cmd=='q'){
+      //turns pump counter-clockwise
+      digitalWrite(Pump_direction,LOW);
+      analogWrite(Pump,100);
+      
+    }
+    if(cmd=='w'){
+      //stops pump
+      analogWrite(Pump,0);
+    }
+    
   }
   
 }
