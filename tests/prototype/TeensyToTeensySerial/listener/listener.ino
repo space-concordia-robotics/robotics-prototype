607f33058b47ef8cc3e5053302687d11a2e96334
PeterGhimself <petergranitski@gmail.com>
2019-01-26 14:01:04 -0500

    [#77] Change teensys to communicate by sending/receiving strings instead of single chars

--- tests/prototype/TeensyToTeensySerial/listener/listener.ino
+++ tests/prototype/TeensyToTeensySerial/listener/listener.ino
@@ -22,55 +22,28 @@ void setup() {
 void loop() {
   if (Serial1.available()) {
     Serial.println("received data!");
-    //String cmd = Serial.readString().trim();
-    char cmd = Serial1.read();
+    String cmd = Serial1.readString().trim();
+    //char cmd = Serial1.read();
     Serial.print("cmd: ");
     Serial.println(cmd);
 
-    //    if (cmd.equals("one")) {
-    //      Serial.println("1");
-    //      blinkLed(rest);
-    //    } else if (cmd.equals("two")) {
-    //      Serial.println("2");
-    //      blinkLed(rest);
-    //      blinkLed(rest);
-    //    } else if (cmd.equals("three")) {
-    //      Serial.println("3");
-    //      for (int i = 0; i < 3; i++) {
-    //        blinkLed(rest);
-    //      }
-    //    } else if (cmd.equals("four")) {
-    //      Serial.println("4");
-    //      for (int i = 0; i < 4; i++) {
-    //        blinkLed(rest);
-    //      }
-    //    }
-
-    switch (cmd) {
-      case '1':
-        Serial.println("1");
+    if (cmd.equals("one")) {
+      Serial.println("1");
+      blinkLed(rest);
+    } else if (cmd.equals("two")) {
+      Serial.println("2");
+      blinkLed(rest);
+      blinkLed(rest);
+    } else if (cmd.equals("three")) {
+      Serial.println("3");
+      for (int i = 0; i < 3; i++) {
         blinkLed(rest);
-        break;
-      case '2':
-        Serial.println("2");
+      }
+    } else if (cmd.equals("four")) {
+      Serial.println("4");
+      for (int i = 0; i < 4; i++) {
         blinkLed(rest);
-        blinkLed(rest);
-        break;
-      case '3':
-        Serial.println("3");
-        for (int i = 0; i < 3; i++) {
-          blinkLed(rest);
-        }
-        break;
-        case '4':
-          Serial.println("4");
-          for (int i = 0; i < 4; i++) {
-            blinkLed(rest);
-          }
-        break;
-       default:
-        Serial.println("default");
-        break;
+      }
     }
 
   }
--- tests/prototype/TeensyToTeensySerial/speaker/speaker.ino
+++ tests/prototype/TeensyToTeensySerial/speaker/speaker.ino
@@ -1,6 +1,6 @@
 int rest = 200;
 elapsedMillis dt;
-char cmds[] = {'1', '2', '3', '4'};
+String cmds[] = {"one", "two", "three", "four"};
 int i = 0;
 
 void blinkLed(int halfTime) {
@@ -26,32 +26,11 @@ void loop() {
   if (dt > 2000) {
     dt = 0;
     Serial.println("beep");
-    //Serial1.print("one");
-    Serial1.write(cmds[i++]);
+    Serial1.print(cmds[i++]);
+    //Serial1.write(cmds[i++]);
     //Serial1.flush();
     if (i == 4) {
       i = 0;
     }
   }
-//  if (dt > 4000 && dt < 4100) {
-//    dt = 0;
-//    Serial.println("boop");
-//    //Serial1.print("two");
-//    Serial1.write('2');
-//    Serial1.flush();
-//  }
-  /*Serial.print('a');
-  delay(2000);
-  Serial1.write('b');
-  Serial1.flush();
-  Serial.print('b');
-  delay(2000);
-  Serial1.write('c');
-  Serial1.flush();
-  Serial.print('c');
-  delay(2000);
-  Serial1.write('d');
-  Serial1.flush();
-  Serial.print('d');
-  delay(2000);*/
 }
