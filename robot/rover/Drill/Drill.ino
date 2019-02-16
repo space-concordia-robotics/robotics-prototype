ffc7ff9c2d188bbc499fa678cb6d1c6e6970f998
zouhairghali <zouhair_ghali@hotmail.com>
2019-02-16 16:40:29 -0500

    [#84] Remove pump motor, add elevator motor and turning table motor

--- robot/rover/Drill/Drill.ino
+++ robot/rover/Drill/Drill.ino
@@ -1,16 +1,25 @@
+
 int Drill = 5;
 int Drill_direction = 3;
-int Pump = 9;
-int Pump_direction = 6;
+int Elevator = 9;
+int Elevator_direction = 6;
+int Table = 11;
+int Table_direction = 10;
 
 void setup() {
   
-	Serial.begin(9600);
+  Serial.begin(9600);
 
   pinMode(Drill,OUTPUT);
   pinMode(Drill_direction,OUTPUT);
-  pinMode(Pump,OUTPUT);
-  pinMode(Pump_direction,OUTPUT);
+  pinMode(Elevator,OUTPUT);
+  pinMode(Elevator_direction,OUTPUT);
+  pinMode(Table,OUTPUT);
+  pinMode(Table_direction,OUTPUT);
+  delay(100);
+  analogWrite(Drill,0);
+  analogWrite(Elevator,0);
+  analogWrite(Table,0);
   delay(1000);
   
   Serial.print("\nsetup complete");
@@ -22,44 +31,20 @@ void loop() {
   if(Serial.available()){
     char cmd = Serial.read();
 
-    if(cmd=='j'){
-      
-      digitalWrite(Drill_direction,HIGH);
-      analogWrite(Drill,100);
-      delay(10000);
-      analogWrite(Drill,0);
-    }
-    if(cmd=='l'){
-      
-      digitalWrite(Drill_direction,LOW);
-      analogWrite(Drill,100);
-      delay(10000);
-      analogWrite(Drill,0);
-    }
-    if(cmd=='u'){
-      
-      digitalWrite(Pump_direction,HIGH);
-      analogWrite(Pump,100);
-      delay(10000);
-      analogWrite(Pump,0);
-    }
-    if(cmd=='o'){
-      
-      digitalWrite(Pump_direction,LOW);
-      analogWrite(Pump,100);
-      delay(10000);
-      analogWrite(Pump,0);
-    }
     if(cmd=='a'){
       //turns drill counter-clockwise
+      analogWrite(Drill,0);
+      delay(500);
       digitalWrite(Drill_direction,HIGH);
-      analogWrite(Drill,100);
+      analogWrite(Drill,125);
       
     }
     if(cmd=='d'){
       //turns drill clockwise
+      analogWrite(Drill,0);
+      delay(500);
       digitalWrite(Drill_direction,LOW);
-      analogWrite(Drill,100);
+      analogWrite(Drill,125);
       
     }
     if(cmd=='s'){
@@ -67,23 +52,45 @@ void loop() {
       analogWrite(Drill,0);
     }
     if(cmd=='e'){
-       //turns pump clockwise
-      digitalWrite(Pump_direction,HIGH);
-      analogWrite(Pump,100);
+       //turns elevator clockwise
+      analogWrite(Elevator,0);
+      delay(500);
+      digitalWrite(Elevator_direction,HIGH);
+      analogWrite(Elevator,150);
       
     }
     if(cmd=='q'){
-      //turns pump counter-clockwise
-      digitalWrite(Pump_direction,LOW);
-      analogWrite(Pump,100);
+      //turns elevator counter-clockwise
+      analogWrite(Elevator,0);
+      delay(500);
+      digitalWrite(Elevator_direction,LOW);
+      analogWrite(Elevator,150);
       
     }
     if(cmd=='w'){
-      //stops pump
-      analogWrite(Pump,0);
+      //stops elevator
+      analogWrite(Elevator,0);
     }
-    
+    if(cmd=='l'){
+       //turns table clockwise
+      analogWrite(Table,0);
+      delay(500);
+      digitalWrite(Table_direction,HIGH);
+      analogWrite(Table,245);
+      
+    }
+    if(cmd=='j'){
+      //turns table counter-clockwise
+      analogWrite(Table,0);
+      delay(500);
+      digitalWrite(Table_direction,LOW);
+      analogWrite(Table,125);
+      
+    }
+    if(cmd=='k'){
+      //stops Elevator
+      analogWrite(Table,0);
+    }
   }
   
 }
-
