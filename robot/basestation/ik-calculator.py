02828fb63ef9c4e8dd610c7524717c0fa2490f9b
mkaller <maximkaller@gmail.com>
2019-03-11 13:30:27 -0400

    [#91] Add wrist angle control

--- robot/basestation/ik-calculator.py
+++ robot/basestation/ik-calculator.py
@@ -2,13 +2,6 @@
 ############SCRB 2019 INVERSE KINEMATICS PACKAGE############
 ############################################################
 
-#NOTE: The first angle in the code (a1a) is the angle that the base makes
-#with the horizontal. Since the base is immobile, this angle is assumed to
-#be constant. In fact, the properties of the base are not taken into
-#account in this code but are simply added to the final figure.
-#For this reason, each angle has a higher coefficient than its
-#corresponding link. E.g. link 1 corresponds to angle computed_proximal_angle.
-
 #This code uses a closed form solution to determine the orientation of the
 #joints of a planar RRR arm for its end effector to reach a given point.For
 #more information about workings of the code, contact Maxim Kaller.
@@ -40,10 +33,12 @@ minmax = [[0,0], [proximal_min_angle,proximal_max_angle], [distal_min_angle, dis
 #INITIALIZE GLOBAL VARIABLES
 computed_angles = [0, proximal_min_angle, distal_min_angle, wrist_min_angle]
 sample_size = 50
+workspace_rep = [[0,0] for x in range(pow(sample_size,3))]
 
 ###################PYGAME PARAMETERS###################
 Window_X = 640
 Window_Y = 480
+increment = 0.02
 
 BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
@@ -52,6 +47,7 @@ GREEN = (  0, 255,   0)
 RED =   (255,   0,   0)
 
 ###############PYGAME FUNCTIONS###############
+# expected input: array of points for path from start to end
 def draw(self, pts):
 	for i in range(pts):
 		pygame.draw.line(self.screen, self.color, self.projected_position[i][0], self.projected_position[i][1], self.width)
@@ -69,7 +65,6 @@ def GenerateRepresentativeCoordinates(PointArray, offset_x, offset_y, size = 4):
 		resized_array = [0,0]
 		resized_array[0] = int(PointArray[0] * Window_X * Scale_Factor + offset_x)
 		resized_array[1] = int(- PointArray[1] * Window_Y * Scale_Factor + offset_y)
-
 	else:
 		resized_array = [ [[0,0],[0,0]] for x in range(size)]
 		for i in range(0,size):
@@ -96,40 +91,49 @@ class ProjectionView:
 #######################FUNCTIONS#######################
 
 
-def ComputeWorkspace(joint_array, joint_num,minmax_array, length_array):
+def ComputeWorkspace(joint_num,minmax_array, length_array):
 	global sample_size
+	pt = [[0,0] for x in range(pow(sample_size,3))]
+	incremented_angle = [0,0,0]
 
-	for j1 in range(minmax_array[0][0],minmax_array[0][1], (minmax_array[0][1] - minmax_array[0][0])/sample_size):
-		for j2 in range(minmax_array[1][0],minmax_array[1][1], (minmax_array[1][1] - minmax_array[1][0])/sample_size):
-			for j3 in range(minmax_array[2][0],minmax_array[2][1], (minmax_array[2][1] - minmax_array[2][0])/sample_size):
-				pt[ sample_size * sample_size * j1 + sample_size * j2 + j3][0] = length_array[0] * math.cos(j1) + length_array[1] * math.cos(j2) + length_array[2] * math.cos(j3)
-				pt[ sample_size * sample_size * j1 + sample_size * j2 + j3][1] = length_array[0] * math.sin(j1) + length_array[1] * math.sin(j2) + length_array[2] * math.sin(j3)
+	for j1 in range(sample_size):
+		incremented_angle[0] = minmax_array[0][0] + (minmax_array[0][1] - minmax_array[0][0])/(j1+1)
+		for j2 in range(sample_size):
+			incremented_angle[1] = minmax_array[1][0] + (minmax_array[1][1] - minmax_array[1][0])/(j2+1)
+			for j3 in range(sample_size):
+					incremented_angle[2] = minmax_array[2][0] + (minmax_array[2][1] - minmax_array[2][0])/(j3+1)
+
+					pt[ sample_size * sample_size * j1 + sample_size * j2 + j3][0] = length_array[0] * math.cos(incremented_angle[0]) + length_array[1] * math.cos(incremented_angle[1] + incremented_angle[0]) + length_array[2] * math.cos(incremented_angle[2] + incremented_angle[1] + incremented_angle[0])
+					pt[ sample_size * sample_size * j1 + sample_size * j2 + j3][1] = length_array[0] * math.sin(incremented_angle[0]) - length_array[1] * math.sin(incremented_angle[1] + incremented_angle[0]) - length_array[2] * math.sin(incremented_angle[2] + incremented_angle[1] + incremented_angle[0])
 	return pt
 
 
-
-def ComputeIK(X, Y, Z):
+def ComputeIK(X, Y, Z, wrist_angle = -1):
 
 	global computed_angles
 	solution_angles = [[0,0,0,0], [0,0,0,0]]
 	solution_usable = [True, True]
 
 	#Base rotation CALCULATION
-	solution_angles[0][0] = solution_angles[1][0] = math.atan2( Z,X)
+	solution_angles[0][0] = solution_angles[1][0] = math.atan( Z/X)
 	R = math.sqrt( pow(X,2) + pow(Z,2) )
-	if (X <= 0 and Z <= 0):
+	if (X <= 0 or Z <=0 ):
 		R *= -1
-		solution_angles[0][0] += math.pi
+		solution_angles[0][0] *= -1
+		solution_angles[1][1] *= -1
 
-	#Check to see if point is too far
-	#REPLACE LATER WITH FANCIER SHIT
-	beta = math.atan2(Y - base_length , R) #Calculates beta, which is the sum of the angles of all links
-	Wrist_X = R - wrist_length * math.cos(beta) #Calculates wrist X coordinate
-	Wrist_Y = (Y - base_length) - wrist_length * math.sin(beta) #Calculates wrist Y coordinate
+	if(wrist_angle is -1):
+		beta = math.atan2(Y - base_length , R) #Calculates beta, which is the sum of the angles of all links
+		Wrist_X = R - wrist_length * math.cos(beta) #Calculates wrist X coordinate
+		Wrist_Y = (Y - base_length) - wrist_length * math.sin(beta) #Calculates wrist Y coordinate
+	else:
+		Wrist_X = R - wrist_length * math.cos(wrist_angle) #Calculates wrist X coordinate
+		Wrist_Y = (Y - base_length) - wrist_length * math.sin(wrist_angle) #Calculates wrist Y coordinate
+		beta = math.atan2(Wrist_Y  , Wrist_X) #Calculates beta, which is the sum of the angles of all links
 
 	if math.sqrt(pow(Wrist_X,2) + pow(Wrist_Y,2)) > proximal_length + distal_length:
 		print('ERROR: SET POINT BEYOND ARM WORKSPACE')
-		return 0
+		return computed_angles
 
 
 	cosine_distal_angle = (pow(Wrist_X,2) + pow(Wrist_Y,2) - pow(proximal_length,2) - pow(distal_length,2))/(2 * proximal_length * distal_length)
@@ -151,10 +155,12 @@ def ComputeIK(X, Y, Z):
 		solution_angles[0][1] = beta - phi
 		solution_angles[1][1] = beta + phi
 
-	#WRIST ANGLE CALCULATION: REPLACE WITH FANCY SHIT LATER
-	solution_angles[0][3] = beta - (solution_angles[0][1] + solution_angles[0][2])
-	solution_angles[1][3] = beta - (solution_angles[1][1] + solution_angles[1][2])
-
+	if(wrist_angle is -1):
+		solution_angles[0][3] = beta - (solution_angles[0][1] + solution_angles[0][2])
+		solution_angles[1][3] = beta - (solution_angles[1][1] + solution_angles[1][2])
+	else:
+		solution_angles[0][3] = wrist_angle - (solution_angles[0][1] + solution_angles[0][2])
+		solution_angles[1][3] = wrist_angle - (solution_angles[1][1] + solution_angles[1][2])
 
 	#ELIMIATION OF ONE OF THE ANGLE SETS
 
@@ -162,11 +168,9 @@ def ComputeIK(X, Y, Z):
 		if solution_angles[0][i] > minmax[i][1] or solution_angles[0][1] < minmax[i][0]:
 			solution_usable[0] = False
 			solution = 1
-			print('One Solution is not possible')
 		if solution_angles[1][i] > minmax[i][1] or solution_angles[1][1] < minmax[i][0]:
 			solution_usable[1] = False
 			solution = 0
-			print('One Solution is not possible')
 
 	if (solution_usable[0] is False) and (solution_usable[1] is False):
 		print('Error: No arm configuration available for specified set point')
@@ -183,7 +187,7 @@ def ComputeIK(X, Y, Z):
 
 	return solution_angles[solution]
 
-
+# Side view
 def UpdateArmSideValues(angles):
 
 	base_start_point = [0,0]
@@ -197,6 +201,7 @@ def UpdateArmSideValues(angles):
 
 	return [ [base_start_point, base_end_point], [proximal_start_point, proximal_end_point], [distal_start_point, distal_end_point] , [wrist_start_point, wrist_end_point] ]
 
+# top view
 def UpdateArmTopValues(angles):
 	arm_start_point = [0,0]
 	arm_radius = proximal_length * math.cos(angles[1]) + distal_length * math.cos(angles[1] + angles[2]) + wrist_length * math.cos(angles[1] + angles[2] + angles[3])
@@ -205,50 +210,56 @@ def UpdateArmTopValues(angles):
 	return [[arm_start_point, arm_end_point]]
 
 #########################MAIN#########################
-
 #################PYGAME INIT#################
 pygame.init()
 pygame.font.init()
 screen = pygame.display.set_mode((Window_X,Window_Y))
 pygame.display.set_caption("Inverse Kinematics Module")
-mode = 1
 
 Sideview = ProjectionView(RED, Window_X/4, 3*Window_Y/4, screen)
-Topview = ProjectionView(RED, 3*Window_X/4, Window_Y/4, screen)
+Topview = ProjectionView(RED, 3*Window_X/4, 3*Window_Y/8, screen)
 
 done = False
 clock = pygame.time.Clock()
 
+set_point = [0.3, 0.6, 0.5]
+wrist_angle = 0
 
-set_point = [0.4,0.3,0.2]
+workspace_enable = False
+if workspace_enable:
+	workspace = ComputeWorkspace(4,minmax, length_array)
 #################PYGAME LOOP#################
 while not done:
 
 	#Check if user closed window
     clock.tick(10)
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    	if event.type == pygame.QUIT:
             done=True
-        if event.type == pygame.KEYDOWN:
+    	if event.type == pygame.KEYDOWN:
             	if event.key == pygame.K_UP:
-            		set_point[1] += 0.02
+            		set_point[1] += increment
             	if event.key == pygame.K_DOWN:
-            		set_point[1] -= 0.02
+            		set_point[1] -= increment
             	if event.key == pygame.K_RIGHT:
-            		set_point[0] += 0.02
+            		set_point[0] += increment
             	if event.key == pygame.K_LEFT:
-            		set_point[0] -= 0.02
+            		set_point[0] -= increment
+            	if event.key == pygame.K_PERIOD:
+            		set_point[2] += increment
+            	if event.key == pygame.K_COMMA:
+            		set_point[2] -= increment
 
-	#Update arm view
-    screen.fill(WHITE)
+		#Update arm view
+    	screen.fill(WHITE)
 
 	#INVERSE KINEMATICS MODE
-    if mode is 1:
     	set_point_2D = [math.sqrt( pow(set_point[0],2) + pow(set_point[2],2) ), set_point[1]]
     	if (set_point[0] <= 0 and set_point[2] <= 0):
     		set_point_2D[0] *= -1
     	top_point = [set_point[0], set_point[2]]
-    	computed_angles = ComputeIK(set_point[0], set_point[1], set_point[2])
+    	computed_angles = ComputeIK(set_point[0], set_point[1], set_point[2] , wrist_angle)
+
 
 		#Sideview
     	Sideview.actual_position = UpdateArmSideValues(computed_angles)
@@ -258,13 +269,13 @@ while not done:
     	drawText(screen, "Side View", BLACK, [Sideview.X, Sideview.Y + 50])
 
 
-
 		#Topview
     	Topview.actual_position = UpdateArmTopValues(computed_angles)
     	Topview.projected_position = GenerateRepresentativeCoordinates(Topview.actual_position, Topview.X, Topview.Y, 1)
     	draw(Topview, 1)
     	pygame.draw.circle(screen, RED, [int(Topview.projected_position[0][0][0]),int(Topview.projected_position[0][0][1])] , 10)
     	drawText(screen, "Top View", BLACK, [Topview.X, Topview.Y + 50])
+    	#drawText(screen, computed_angles, BLACK, [0,0])
 
 		#Draw a circle around target point
     	desired_side_point = GenerateRepresentativeCoordinates(set_point_2D , Sideview.X, Sideview.Y, 0)
@@ -274,9 +285,10 @@ while not done:
     	pygame.draw.circle(screen, BLUE, desired_top_point , 15, 3)
 
 	#WORKSPACE MODE
-    if mode is 0:
-    	for i in range(0, math.pow(sample_size,3) ):
-    		pygame.draw.circle(screen, BLUE, desired_point , 1)
+    	if workspace_enable is True:
+    		for i in range(0, int(math.pow(sample_size,3))):
+    			workspace_rep[i] = GenerateRepresentativeCoordinates(workspace[i] , Sideview.X, Sideview.Y, 0)
+    			pygame.draw.circle(screen, BLUE, workspace_rep[i] , 4)
 
 
 	#This is required to update the display
