cc4af8e60ae2295340cd6cc24efc26777aeb6e4f
Joshua Glazer <vashmata@gmail.com>
2019-04-18 00:21:23 -0400

    [#24] Replace m1 pins with m3 pins for homing test

--- robot/rover/ArmDriverUnit/PinSetup.h
+++ robot/rover/ArmDriverUnit/PinSetup.h
@@ -46,14 +46,12 @@
 #define TRIGGER_DELAY 10 //!< how long to wait to make sure a limit switch was pressed and ignore bouncing
 
 // DC motors
-
+/*
 #define M1_DIR_PIN          5 //!< chooses the direction the motor turns in
 #define M1_PWM_PIN          6 //!< the speed of the motor is controlled by the pwm signal
 // 7&8 are on port D with bits 2&3 respectively
 #define M1_ENCODER_PORT    GPIOD_PDIR //!< the input register for the port connected to the encoder pins
-/*! \brief the position of the lower encoder pin bit. The encoder interrupt code
- * expects this to function correctly.
-*/
+//! \brief the position of the lower encoder pin bit. The encoder interrupt code expects this to function correctly.
 #define M1_ENCODER_SHIFT   CORE_PIN7_BIT
 #define M1_ENCODER_A        7 //!< encoder A pin
 #define M1_ENCODER_B        8 //!< encoder B pin
@@ -63,38 +61,56 @@
 #define M1_LIMIT_SW_CCW_SHIFT CORE_PIN10_BIT
 #define M1_LIMIT_SW_CW        9
 #define M1_LIMIT_SW_CCW       10
+*/
+// swapping m1 pins with m3 pins so i can control m3
+// using the current setup since m1 isn't being used right now
+// and I need to test m2-4 for homing
+#define M1_DIR_PIN            20
+#define M1_PWM_PIN            21
+#define M1_ENCODER_PORT       GPIOB_PDIR
+#define M1_ENCODER_SHIFT      CORE_PIN19_BIT
+#define M1_ENCODER_A          19
+#define M1_ENCODER_B          18
+#define M1_LIMIT_SW_CW_PORT   GPIOC_PDIR
+#define M1_LIMIT_SW_CCW_PORT  GPIOC_PDIR
+#define M1_LIMIT_SW_CW_SHIFT  CORE_PIN22_BIT
+#define M1_LIMIT_SW_CCW_SHIFT CORE_PIN23_BIT
+#define M1_LIMIT_SW_CW        22
+#define M1_LIMIT_SW_CCW       23
+// end of swap
 #define M1_ENCODER_RESOLUTION 48
-#define M1_GEAR_RATIO      40.0 //!< \todo fix this based on gear reduction inside and outside motor
-#define M1_MIN_HARD_ANGLE   -350.0 //!< \todo work this out maybe with max or dharik
-#define M1_MAX_HARD_ANGLE   350.0 //!< \todo work this out maybe with max or dharik
-#define M1_MIN_SOFT_ANGLE   -325.0
-#define M1_MAX_SOFT_ANGLE   325.0
+#define M1_GEAR_RATIO         40.0 //!< \todo fix this based on gear reduction inside and outside motor
+#define M1_MIN_HARD_ANGLE     -350.0 //!< \todo work this out maybe with max or dharik
+#define M1_MAX_HARD_ANGLE     350.0 //!< \todo work this out maybe with max or dharik
+#define M1_MIN_SOFT_ANGLE     -325.0
+#define M1_MAX_SOFT_ANGLE     325.0
 
-#define M2_PWM_PIN         30
-#define M2_DIR_PIN         31
+#define M2_PWM_PIN            30
+#define M2_DIR_PIN            31
 // 26&27 are on port A with bits 14&15 respectively
-#define M2_ENCODER_PORT    GPIOA_PDIR
-#define M2_ENCODER_SHIFT   CORE_PIN26_BIT
-#define M2_ENCODER_A       26
-#define M2_ENCODER_B       27
+#define M2_ENCODER_PORT       GPIOA_PDIR
+#define M2_ENCODER_SHIFT      CORE_PIN26_BIT
+#define M2_ENCODER_A          26
+#define M2_ENCODER_B          27
 #define M2_LIMIT_SW_FLEX_PORT    GPIOA_PDIR //!< input register for the port associated to flex limit switch pin
 #define M2_LIMIT_SW_EXTEND_PORT  GPIOB_PDIR //!< input register for the port associated to extend limit switch pin
 #define M2_LIMIT_SW_FLEX_SHIFT   CORE_PIN28_BIT //!< position of the limit switch flex pin bit
 #define M2_LIMIT_SW_EXTEND_SHIFT CORE_PIN29_BIT //!< position of the limit switch extend pin bit
-#define M2_LIMIT_SW_FLEX   28 //!< limit switch flex pin
-#define M2_LIMIT_SW_EXTEND 29 //!< limit switch extend pin
+#define M2_LIMIT_SW_FLEX      28 //!< limit switch flex pin
+#define M2_LIMIT_SW_EXTEND    29 //!< limit switch extend pin
 #define M2_ENCODER_RESOLUTION 48
 //! planetary gear motor chained to worm gear drive
-#define M2_GEAR_RATIO      188.611 //*20.0 //99.508*20
-#define M2_MIN_HARD_ANGLE   -65.0 //!< the flexion angle at which the joint presses the limit switch
-#define M2_MAX_HARD_ANGLE   50.0 //!< the extension angle at which the joint presses the limit switch
-#define M2_MIN_SOFT_ANGLE   -55.0 //!< a safety margin is added to the flexion angle to avoid ever hitting the limit switch after homing is complete
-#define M2_MAX_SOFT_ANGLE   40.0 //!< a safety margin is added to the extension angle to avoid ever hitting the limit switch after homing is complete
+#define M2_GEAR_RATIO         188.611 //*20.0 //99.508*20
+#define M2_MIN_HARD_ANGLE     -65.0 //!< the flexion angle at which the joint presses the limit switch
+#define M2_MAX_HARD_ANGLE     50.0 //!< the extension angle at which the joint presses the limit switch
+#define M2_MIN_SOFT_ANGLE     -55.0 //!< a safety margin is added to the flexion angle to avoid ever hitting the limit switch after homing is complete
+#define M2_MAX_SOFT_ANGLE     40.0 //!< a safety margin is added to the extension angle to avoid ever hitting the limit switch after homing is complete
 
-#define M3_PWM_PIN         21
+/*
 //#define M3_ENABLE_PIN      17
 //#define M3_STEP_PIN        21
 #define M3_DIR_PIN         20 // stays the same
+#define M3_PWM_PIN         21
 // 19&18 are on port B with bits 2&3 respectively
 #define M3_ENCODER_PORT    GPIOB_PDIR
 #define M3_ENCODER_SHIFT   CORE_PIN19_BIT
@@ -106,14 +122,31 @@
 #define M3_LIMIT_SW_EXTEND_SHIFT  CORE_PIN23_BIT
 #define M3_LIMIT_SW_FLEX          22
 #define M3_LIMIT_SW_EXTEND        23
+*/
+// swapping m1 pins with m3 pins so i can control m3
+// using the current setup since m1 isn't being used right now
+// and I need to test m2-4 for homing
+#define M3_DIR_PIN            5
+#define M3_PWM_PIN            6
+#define M3_ENCODER_PORT       GPIOD_PDIR
+#define M3_ENCODER_SHIFT      CORE_PIN7_BIT
+#define M3_ENCODER_A          7
+#define M3_ENCODER_B          8
+#define M3_LIMIT_SW_FLEX_PORT   GPIOC_PDIR
+#define M3_LIMIT_SW_EXTEND_PORT  GPIOC_PDIR
+#define M3_LIMIT_SW_FLEX_SHIFT  CORE_PIN9_BIT
+#define M3_LIMIT_SW_EXTEND_SHIFT CORE_PIN10_BIT
+#define M3_LIMIT_SW_FLEX      9
+#define M3_LIMIT_SW_EXTEND    10
+// end of swap
 #define M3_ENCODER_RESOLUTION 48
 //#define M3_STEP_RESOLUTION 1.8 // I think it's the same for all our steppers
 //! belt reduction chained to worm gear drive
-#define M3_GEAR_RATIO      188.611*(40.0/14.0)*18.0
-#define M3_MIN_HARD_ANGLE   -145.0
-#define M3_MAX_HARD_ANGLE   65.0
-#define M3_MIN_SOFT_ANGLE   -135.0
-#define M3_MAX_SOFT_ANGLE   55.0
+#define M3_GEAR_RATIO         188.611*(40.0/14.0)*18.0
+#define M3_MIN_HARD_ANGLE     -145.0
+#define M3_MAX_HARD_ANGLE     65.0
+#define M3_MIN_SOFT_ANGLE     -135.0
+#define M3_MAX_SOFT_ANGLE     55.0
 
 // stepper
 
@@ -136,10 +169,10 @@
 // the 1.3846 is a value i calculated based on the discrepancy between expected and actual angles
 //! belt reduction chained to worm gear drive
 #define M4_GEAR_RATIO      (30.0/18.0)*20.0/1.3846 //(30.0/18.0)*20.0 
-#define M4_MIN_HARD_ANGLE   -90.0
-#define M4_MAX_HARD_ANGLE   75.0
-#define M4_MIN_SOFT_ANGLE   -80.0
-#define M4_MAX_SOFT_ANGLE   65.0
+#define M4_MIN_HARD_ANGLE  -90.0
+#define M4_MAX_HARD_ANGLE  75.0
+#define M4_MIN_SOFT_ANGLE  -80.0
+#define M4_MAX_SOFT_ANGLE  65.0
 
 // servos
 
