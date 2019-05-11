78f182ac11b237eee32262f1c89ac88b16a26008
PeterGhimself <petergranitski@gmail.com>
2019-05-10 20:26:53 -0400

    [#83] Force resolution down and coerce FPS from 1-5
    This greatly reduces the lag of the video stream as well as the time it takes for the system to respond
    to a shutdown request while the camera stream is active.
    With an average Bandwidth of 5Mbits/sec, the results where about 300-400ms delay in the camera stream and I estimate the latency of the video stream is between 300-700ms.

--- robot/rospackages/src/task_handler/scripts/start_stream.sh
+++ robot/rospackages/src/task_handler/scripts/start_stream.sh
@@ -13,7 +13,7 @@ port=`tail -1 tmp`
 port="$(echo -e "${port}" | sed -e 's/^[[:space:]]*//')"
 echo "port: $port"
 
-/usr/local/bin/mjpg_streamer  -i "input_uvc.so -r 1280x720 -m 50000 -n -f 25 -d $port" -o "output_http.so -p 8090 -w /usr/local/share/mjpg-streamer/www/"
+/usr/local/bin/mjpg_streamer  -i "input_uvc.so -r 640x480 -m 50000 -n -f 1 -d $port" -o "output_http.so -p 8090 -w /usr/local/share/mjpg-streamer/www/"
 
 # to install dependencies:
 # $ git clone https://github.com/jacksonliam/mjpg-streamer.git
