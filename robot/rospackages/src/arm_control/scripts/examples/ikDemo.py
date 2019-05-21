9a9a12b591d40665d7c7972a3aa507bcbfff99cc
Josh Glazer <vashmata@gmail.com>
2019-05-21 17:32:34 -0400

    [#99][#91] Add a bit more to ik demo

--- robot/rospackages/src/arm_control/scripts/examples/ikDemo.py
+++ robot/rospackages/src/arm_control/scripts/examples/ikDemo.py
@@ -4,7 +4,13 @@ from ik_calculator import *
 
 asimov=Arm(4,length_array,minmax,[0,math.pi/2,0,0])
 print( asimov.getCurrentAngles() )
+
 position = asimov.computeFK()
-position[1]-=0.0001 #distance a bit from the extreme endpoint
 print( asimov.computeFK() )
+
+position[1]-=0.0001 #distance a bit from the extreme endpoint
+
+angles = asimov.computeIK(position)
 print( asimov.computeIK(position) )
+
+print( asimov.computeFK(angles[0]) )
