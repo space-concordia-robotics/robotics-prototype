970c2c11df7aa7493a072c155b9dcf299c2c21d2
PeterGhimself <petergranitski@gmail.com>
2019-05-24 01:47:07 -0400

    [#101] Reduce waiting time for self poll in start() method from 4 to 3 seconds

--- robot/rospackages/src/task_handler/scripts/Listener.py
+++ robot/rospackages/src/task_handler/scripts/Listener.py
@@ -41,9 +41,9 @@ class Listener:
             self.p1_pid = self.p1.pid
 
             # allow some time to pass
-            # specifically waiting for 4 seconds since command listener scripts
+            # specifically waiting for 3 seconds since command listener scripts
             # take an extra 2-3 seconds time to identify if the correct MCU is connected
-            time.sleep(4)
+            time.sleep(3)
 
             poll = self.p1.poll()
 
