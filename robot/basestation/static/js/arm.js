0f9884fbd573928471b3e4464a822864c94e9d06
Josh Glazer <vashmata@gmail.com>
2019-05-24 22:30:37 -0400

    [#99] Integrate arguments into task handler requests

--- robot/basestation/static/js/arm.js
+++ robot/basestation/static/js/arm.js
@@ -1,10 +1,3 @@
-let mockArmTable = false
-
-// update console log
-if (mockArmTable) {
-  setInterval(mockArmTableLog, 1000)
-}
-
 // @TODO: fix implementation of odroid rx pub/sub to work on event triggers
 // rather than through polling
 // update odroid rx data every second
@@ -48,6 +41,8 @@ function toggleToManual () {
 }
 
 $(document).ready(function () {
+  //checkTaskStatuses()
+
   $('#ping-odroid').on('click', function (event) {
     if (millisSince(lastCmdSent) > PING_THROTTLE_TIME) {
       appendToConsole('pinging odroid')
@@ -88,36 +83,19 @@ $(document).ready(function () {
     event.preventDefault()
     // click makes it checked during this time, so trying to enable
     if ($('#toggle-arm-listener-btn').is(':checked')) {
-      if (
-        $('button#mux')
-          .text()
-          .includes('Arm')
-      ) {
-        requestTask('arm_listener', 1, '#toggle-arm-listener-btn', function (
-          msgs
-        ) {
-          console.log('msgs[0]', msgs[0])
-          if (msgs.length == 2) {
-            console.log('msgs[1]', msgs[1])
-            // if already running
-            if (msgs[1].includes('already running')) {
-              $('#toggle-arm-listener-btn')[0].checked = true
-            } else {
-              $('#toggle-arm-listener-btn')[0].checked = false
-            }
+      var serialType = 'usb' // i hardcoded this for testing
+      if ( $('button#mux').text().includes('Arm') || serialType == 'usb') {
+        requestTask('arm_listener', 1, '#toggle-arm-listener-btn', function (msgs) {
+          console.log(msgs)
+          if (msgs[0]) {
+            $('#toggle-arm-listener-btn')[0].checked = true
           } else {
-            if (msgs[0]) {
-              $('#toggle-arm-listener-btn')[0].checked = false
-            } else {
-              $('#toggle-arm-listener-btn')[0].checked = true
-            }
+            $('#toggle-arm-listener-btn')[0].checked = false
           }
-        })
+        }, 'usb')
         // console.log('returnVals', returnVals)
       } else {
-        appendToConsole(
-          'Cannot turn arm listener on if not in arm mux channel!'
-        )
+        appendToConsole('Cannot turn arm listener on if not in arm mux channel!')
       }
     } else {
       // closing arm listener
@@ -154,7 +132,7 @@ $(document).ready(function () {
           // failed to open stream
           $('img#camera-feed')[0].src = '../static/images/stream-offline.jpg'
         }
-      })
+      }, '/dev/ttyArmScienceCam')
     } else {
       requestTask('camera_stream', 0, '#toggle-arm-stream-btn', function (msgs) {
         if (msgs[0]) {
@@ -304,7 +282,6 @@ document.addEventListener('keydown', function (event) {
     lastCmdSent = new Date().getTime()
   }
 })
-
 // arm mcu ping
 document.addEventListener('keydown', function (event) {
   if (
@@ -316,7 +293,6 @@ document.addEventListener('keydown', function (event) {
     lastCmdSent = new Date().getTime()
   }
 })
-
 // print commands list
 document.addEventListener('keydown', function (event) {
   if (
@@ -330,445 +306,212 @@ document.addEventListener('keydown', function (event) {
   }
 })
 
-if (mockArmTable) {
-  // manual controls
+// Implement game loop
+var keyState = {}
+window.addEventListener(
+  'keydown',
+  function (e) {
+    keyState[e.keyCode || e.which] = true
+  },
+  true
+)
+window.addEventListener(
+  'keyup',
+  function (e) {
+    keyState[e.keyCode || e.which] = false
+  },
+  true
+)
+
+function gameLoop () {
   let $serialCmdInput = $('#serial-cmd-input')
 
-  // motor 1
-  document.addEventListener('keydown', function (event) {
-    if (!$serialCmdInput.is(':focus') && event.code === 'KeyW') {
+  if (millisSince(lastCmdSent) > MANUAL_CONTROL_THROTTLE_TIME) {
+    let budgeArray = ['~', '~', '~', '~', '~', '~']
+    let i = 0
+    let toBudge = false
+    // 'w' --> m1 ccw
+    if (!$serialCmdInput.is(':focus') && keyState[87]) {
       toggleToManual()
       $('#click_btn_motor1_ccw > button').css(
         'background-color',
         'rgb(255, 0, 0)'
       )
 
-      if (mockArmTable) {
-        let currentAngle = $('#m1-angle').text()
-
-        if (currentAngle > -350) {
-          // simulate motor angles
-          $('#m1-angle').text(parseFloat(currentAngle) - 1)
-          // simulate motor currents
-          $('#m1-current').text('3.5')
-        }
-      }
+      budgeArray[i] = 'fwd'
+      toBudge = true
+      lastCmdSent = new Date().getTime()
     }
-  })
-
-  document.addEventListener('keydown', function (event) {
-    if (!$serialCmdInput.is(':focus') && event.code === 'KeyS') {
+    // 's' --> m1 cw
+    else if (!$serialCmdInput.is(':focus') && keyState[83]) {
       toggleToManual()
       $('#click_btn_motor1_cw > button').css(
         'background-color',
         'rgb(255, 0, 0)'
       )
 
-      if (mockArmTable) {
-        let currentAngle = $('#m1-angle').text()
-
-        if (currentAngle < 350) {
-          $('#m1-angle').text(parseFloat(currentAngle) + 1)
-          // simulate motor currents
-          $('#m1-current').text('3.5')
-        }
-      }
+      budgeArray[i] = 'back'
+      toBudge = true
+      lastCmdSent = new Date().getTime()
     }
-  })
-
-  // motor 2
-  document.addEventListener('keydown', function (event) {
-    if (!$serialCmdInput.is(':focus') && event.code === 'KeyE') {
+    i += 1
+    // 'e' --> m2 ccw
+    if (!$serialCmdInput.is(':focus') && keyState[69]) {
       toggleToManual()
       $('#click_btn_motor2_ccw > button').css(
         'background-color',
         'rgb(255, 0, 0)'
       )
 
-      if (mockArmTable) {
-        let currentAngle = $('#m2-angle').text()
-
-        if (currentAngle > -75) {
-          $('#m2-angle').text(parseFloat(currentAngle) - 1)
-          // simulate motor currents
-          $('#m2-current').text('3.5')
-        }
-      }
+      budgeArray[i] = 'fwd'
+      toBudge = true
+      lastCmdSent = new Date().getTime()
     }
-  })
-
-  document.addEventListener('keydown', function (event) {
-    if (!$serialCmdInput.is(':focus') && event.code === 'KeyD') {
+    // 'd' --> m2 cw
+    else if (!$serialCmdInput.is(':focus') && keyState[68]) {
       toggleToManual()
       $('#click_btn_motor2_cw > button').css(
         'background-color',
         'rgb(255, 0, 0)'
       )
 
-      if (mockArmTable) {
-        let currentAngle = $('#m2-angle').text()
-
-        if (currentAngle < 55) {
-          $('#m2-angle').text(parseFloat(currentAngle) + 1)
-          // simulate motor currents
-          $('#m2-current').text('3.5')
-        }
-      }
+      budgeArray[i] = 'back'
+      toBudge = true
+      lastCmdSent = new Date().getTime()
     }
-  })
-
-  // motor 3
-  document.addEventListener('keydown', function (event) {
-    if (!$serialCmdInput.is(':focus') && event.code === 'KeyR') {
+    i += 1
+    // 'r' --> m3 ccw
+    if (!$serialCmdInput.is(':focus') && keyState[82]) {
       toggleToManual()
       $('#click_btn_motor3_ccw > button').css(
         'background-color',
         'rgb(255, 0, 0)'
       )
 
-      if (mockArmTable) {
-        let currentAngle = $('#m3-angle').text()
-
-        if (currentAngle > -155) {
-          $('#m3-angle').text(parseFloat(currentAngle) - 1)
-        }
-      }
+      budgeArray[i] = 'fwd'
+      toBudge = true
+      lastCmdSent = new Date().getTime()
     }
-  })
-
-  document.addEventListener('keydown', function (event) {
-    if (!$serialCmdInput.is(':focus') && event.code === 'KeyF') {
+    // 'f' --> m3 cw
+    else if (!$serialCmdInput.is(':focus') && keyState[70]) {
       toggleToManual()
       $('#click_btn_motor3_cw > button').css(
         'background-color',
         'rgb(255, 0, 0)'
       )
 
-      if (mockArmTable) {
-        let currentAngle = $('#m3-angle').text()
-
-        if (currentAngle < 35) {
-          $('#m3-angle').text(parseFloat(currentAngle) + 1)
-        }
-      }
+      budgeArray[i] = 'back'
+      toBudge = true
+      lastCmdSent = new Date().getTime()
     }
-  })
-
-  // motor 4
-  document.addEventListener('keydown', function (event) {
-    if (!$serialCmdInput.is(':focus') && event.code === 'KeyT') {
+    i += 1
+    // 't' --> m4 ccw
+    if (!$serialCmdInput.is(':focus') && keyState[84]) {
       toggleToManual()
       $('#click_btn_motor4_ccw > button').css(
         'background-color',
         'rgb(255, 0, 0)'
       )
 
-      if (mockArmTable) {
-        let currentAngle = $('#m4-angle').text()
-
-        if (currentAngle > -55) {
-          $('#m4-angle').text(parseFloat(currentAngle) - 1)
-        }
-      }
+      budgeArray[i] = 'fwd'
+      toBudge = true
+      lastCmdSent = new Date().getTime()
     }
-  })
-
-  document.addEventListener('keydown', function (event) {
-    if (!$serialCmdInput.is(':focus') && event.code === 'KeyG') {
+    // 'g' --> m4 cw
+    else if (!$serialCmdInput.is(':focus') && keyState[71]) {
       toggleToManual()
       $('#click_btn_motor4_cw > button').css(
         'background-color',
         'rgb(255, 0, 0)'
       )
 
-      if (mockArmTable) {
-        // no angle limits
-        let currentAngle = $('#m4-angle').text()
-
-        if (currentAngle < 40) {
-          $('#m4-angle').text(parseFloat(currentAngle) + 1)
-        }
-      }
+      budgeArray[i] = 'back'
+      toBudge = true
+      lastCmdSent = new Date().getTime()
     }
-  })
-
-  // motor 5
-  document.addEventListener('keydown', function (event) {
-    if (!$serialCmdInput.is(':focus') && event.code === 'KeyY') {
+    i += 1
+    // 'y' --> m5 ccw
+    if (!$serialCmdInput.is(':focus') && keyState[89]) {
       toggleToManual()
       $('#click_btn_motor5_ccw > button').css(
         'background-color',
         'rgb(255, 0, 0)'
       )
 
-      if (mockArmTable) {
-        // no angle limits
-        let currentAngle = $('#m5-angle').text()
-        $('#m5-angle').text(parseFloat(currentAngle) - 1)
-      }
+      budgeArray[i] = 'fwd'
+      toBudge = true
+      lastCmdSent = new Date().getTime()
     }
-  })
-
-  document.addEventListener('keydown', function (event) {
-    if (!$serialCmdInput.is(':focus') && event.code === 'KeyH') {
+    // 'h' --> m5 cw
+    else if (!$serialCmdInput.is(':focus') && keyState[72]) {
       toggleToManual()
       $('#click_btn_motor5_cw > button').css(
         'background-color',
         'rgb(255, 0, 0)'
       )
 
-      if (mockArmTable) {
-        // no angle limits
-        let currentAngle = $('#m5-angle').text()
-        $('#m5-angle').text(parseFloat(currentAngle) + 1)
-      }
+      budgeArray[i] = 'back'
+      toBudge = true
+      lastCmdSent = new Date().getTime()
     }
-  })
-
-  // motor 6
-  document.addEventListener('keydown', function (event) {
-    if (!$serialCmdInput.is(':focus') && event.code === 'KeyU') {
+    i += 1
+    // 'u' --> m6 ccw
+    if (!$serialCmdInput.is(':focus') && keyState[85]) {
       toggleToManual()
       $('#click_btn_motor6_ccw > button').css(
         'background-color',
         'rgb(255, 0, 0)'
       )
 
-      if (mockArmTable) {
-        let currentAngle = $('#m6-angle').text()
-
-        if (currentAngle > 0) {
-          $('#m6-angle').text(parseFloat(currentAngle) - 1)
-        }
-      }
+      budgeArray[i] = 'fwd'
+      toBudge = true
+      lastCmdSent = new Date().getTime()
     }
-  })
-
-  document.addEventListener('keydown', function (event) {
-    if (!$serialCmdInput.is(':focus') && event.code === 'KeyJ') {
+    // 'j' --> m6 cw
+    else if (!$serialCmdInput.is(':focus') && keyState[74]) {
       toggleToManual()
       $('#click_btn_motor6_cw > button').css(
         'background-color',
         'rgb(255, 0, 0)'
       )
 
-      if (mockArmTable) {
-        let currentAngle = $('#m6-angle').text()
-
-        if (currentAngle < 75) {
-          $('#m6-angle').text(parseFloat(currentAngle) + 1)
-        }
-      }
-    }
-  })
-} else {
-  // Implement game loop
-  var keyState = {}
-  window.addEventListener(
-    'keydown',
-    function (e) {
-      keyState[e.keyCode || e.which] = true
-    },
-    true
-  )
-  window.addEventListener(
-    'keyup',
-    function (e) {
-      keyState[e.keyCode || e.which] = false
-    },
-    true
-  )
-
-  function gameLoop () {
-    let $serialCmdInput = $('#serial-cmd-input')
-
-    if (millisSince(lastCmdSent) > MANUAL_CONTROL_THROTTLE_TIME) {
-      let budgeArray = ['~', '~', '~', '~', '~', '~']
-      let i = 0
-      let toBudge = false
-      // 'w' --> m1 ccw
-      if (!$serialCmdInput.is(':focus') && keyState[87]) {
-        toggleToManual()
-        $('#click_btn_motor1_ccw > button').css(
-          'background-color',
-          'rgb(255, 0, 0)'
-        )
-
-        budgeArray[i] = 'fwd'
-        toBudge = true
-        lastCmdSent = new Date().getTime()
-      }
-      // 's' --> m1 cw
-      else if (!$serialCmdInput.is(':focus') && keyState[83]) {
-        toggleToManual()
-        $('#click_btn_motor1_cw > button').css(
-          'background-color',
-          'rgb(255, 0, 0)'
-        )
-
-        budgeArray[i] = 'back'
-        toBudge = true
-        lastCmdSent = new Date().getTime()
-      }
-      i += 1
-      // 'e' --> m2 ccw
-      if (!$serialCmdInput.is(':focus') && keyState[69]) {
-        toggleToManual()
-        $('#click_btn_motor2_ccw > button').css(
-          'background-color',
-          'rgb(255, 0, 0)'
-        )
-
-        budgeArray[i] = 'fwd'
-        toBudge = true
-        lastCmdSent = new Date().getTime()
-      }
-      // 'd' --> m2 cw
-      else if (!$serialCmdInput.is(':focus') && keyState[68]) {
-        toggleToManual()
-        $('#click_btn_motor2_cw > button').css(
-          'background-color',
-          'rgb(255, 0, 0)'
-        )
-
-        budgeArray[i] = 'back'
-        toBudge = true
-        lastCmdSent = new Date().getTime()
-      }
-      i += 1
-      // 'r' --> m3 ccw
-      if (!$serialCmdInput.is(':focus') && keyState[82]) {
-        toggleToManual()
-        $('#click_btn_motor3_ccw > button').css(
-          'background-color',
-          'rgb(255, 0, 0)'
-        )
-
-        budgeArray[i] = 'fwd'
-        toBudge = true
-        lastCmdSent = new Date().getTime()
-      }
-      // 'f' --> m3 cw
-      else if (!$serialCmdInput.is(':focus') && keyState[70]) {
-        toggleToManual()
-        $('#click_btn_motor3_cw > button').css(
-          'background-color',
-          'rgb(255, 0, 0)'
-        )
-
-        budgeArray[i] = 'back'
-        toBudge = true
-        lastCmdSent = new Date().getTime()
-      }
-      i += 1
-      // 't' --> m4 ccw
-      if (!$serialCmdInput.is(':focus') && keyState[84]) {
-        toggleToManual()
-        $('#click_btn_motor4_ccw > button').css(
-          'background-color',
-          'rgb(255, 0, 0)'
-        )
-
-        budgeArray[i] = 'fwd'
-        toBudge = true
-        lastCmdSent = new Date().getTime()
-      }
-      // 'g' --> m4 cw
-      else if (!$serialCmdInput.is(':focus') && keyState[71]) {
-        toggleToManual()
-        $('#click_btn_motor4_cw > button').css(
-          'background-color',
-          'rgb(255, 0, 0)'
-        )
-
-        budgeArray[i] = 'back'
-        toBudge = true
-        lastCmdSent = new Date().getTime()
-      }
-      i += 1
-      // 'y' --> m5 ccw
-      if (!$serialCmdInput.is(':focus') && keyState[89]) {
-        toggleToManual()
-        $('#click_btn_motor5_ccw > button').css(
-          'background-color',
-          'rgb(255, 0, 0)'
-        )
-
-        budgeArray[i] = 'fwd'
-        toBudge = true
-        lastCmdSent = new Date().getTime()
-      }
-      // 'h' --> m5 cw
-      else if (!$serialCmdInput.is(':focus') && keyState[72]) {
-        toggleToManual()
-        $('#click_btn_motor5_cw > button').css(
-          'background-color',
-          'rgb(255, 0, 0)'
-        )
-
-        budgeArray[i] = 'back'
-        toBudge = true
-        lastCmdSent = new Date().getTime()
-      }
-      i += 1
-      // 'u' --> m6 ccw
-      if (!$serialCmdInput.is(':focus') && keyState[85]) {
-        toggleToManual()
-        $('#click_btn_motor6_ccw > button').css(
-          'background-color',
-          'rgb(255, 0, 0)'
-        )
-
-        budgeArray[i] = 'fwd'
-        toBudge = true
-        lastCmdSent = new Date().getTime()
-      }
-      // 'j' --> m6 cw
-      else if (!$serialCmdInput.is(':focus') && keyState[74]) {
-        toggleToManual()
-        $('#click_btn_motor6_cw > button').css(
-          'background-color',
-          'rgb(255, 0, 0)'
-        )
-
-        budgeArray[i] = 'back'
-        toBudge = true
-        lastCmdSent = new Date().getTime()
-      }
-
-      // 'z' --> stop all motors
-      if (!$serialCmdInput.is(':focus') && keyState[81]) {
-        $('button#stop-all-motors').css('background-color', 'rgb(255, 0, 0)')
-        sendArmCommand('stop')
-        lastCmdSent = new Date().getTime()
-      }
-
-      else if (toBudge) {
-        let cmd = 'budge '
-        for (var motor in budgeArray) {
-          cmd += budgeArray[motor]
-          if (motor != 5) {
-            cmd += ' '
-          }
-        }
-        sendArmCommand(cmd)
-      }
-
-      // 'o' --> reset angle values
-      if (!$serialCmdInput.is(':focus') && keyState[79]) {
-        $('button#reset-motor-angles').css('background-color', 'rgb(255, 0, 0)')
-        sendArmCommand('reset')
-        lastCmdSent = new Date().getTime()
-      }
-
-      // redraw/reposition your object here
-      // also redraw/animate any objects not controlled by the user
+      budgeArray[i] = 'back'
+      toBudge = true
+      lastCmdSent = new Date().getTime()
     }
 
-    setTimeout(gameLoop, 10)
+    // 'z' --> stop all motors
+    if (!$serialCmdInput.is(':focus') && keyState[81]) {
+      $('button#stop-all-motors').css('background-color', 'rgb(255, 0, 0)')
+      sendArmCommand('stop')
+      lastCmdSent = new Date().getTime()
+    }
+
+    else if (toBudge) {
+      let cmd = 'budge '
+      for (var motor in budgeArray) {
+        cmd += budgeArray[motor]
+        if (motor != 5) {
+          cmd += ' '
+        }
+      }
+      sendArmCommand(cmd)
+    }
+
+    // 'o' --> reset angle values
+    if (!$serialCmdInput.is(':focus') && keyState[79]) {
+      $('button#reset-motor-angles').css('background-color', 'rgb(255, 0, 0)')
+      sendArmCommand('reset')
+      lastCmdSent = new Date().getTime()
+    }
+
+    // redraw/reposition your object here
+    // also redraw/animate any objects not controlled by the user
   }
-  gameLoop()
+
+  setTimeout(gameLoop, 10)
 }
+gameLoop()
 
 // In any case
 // the following code just makes the buttons stop lighting up
--- robot/basestation/static/js/roslibjs/ros-helpers.js
+++ robot/basestation/static/js/roslibjs/ros-helpers.js
@@ -5,6 +5,7 @@ function initRosWeb () {
 
   ros.on('connection', function () {
     appendToConsole('Connected to websocket server.')
+    checkTaskStatuses()
   })
 
   ros.on('error', function (error) {
@@ -54,7 +55,7 @@ function initRosWeb () {
     messageType: 'std_msgs/String'
   })
 
-  // setup a publisher for the arm_command topic
+  // setup a publisher for the ik_command topic
   ik_command_publisher = new ROSLIB.Topic({
     ros: ros,
     name: 'ik_command',
@@ -87,16 +88,66 @@ function initRosWeb () {
     appendToConsole(message.data)
   })
 
-  /* rover controls (placeholder descriptions, names are subject to change) */
+  /* rover commands */
 
   // setup a client for the rover_request service
+  rover_request_client = new ROSLIB.Service({
+    ros: ros,
+    name: 'rover_request',
+    serviceType: 'ArmRequest' // for now... might change
+  })
 
-  // setup a publisher for the rover_command topic
+  // setup a publisher for the arm_command topic
+  rover_command_publisher = new ROSLIB.Topic({
+    ros: ros,
+    name: 'rover_command',
+    messageType: 'std_msgs/String'
+  })
 
-  // setup a subscriber for the rover_vels topic
+  // setup a subscriber for the rover_joint_states topic
+  rover_joint_states_listener = new ROSLIB.Topic({
+    ros: ros,
+    name: 'rover_joint_states',
+    messageType: 'sensor_msgs/JointState'
+  })
+
+  rover_joint_states_listener.subscribe(function (message) {
+    //TODO: make sure the indices are correct... DONE?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?
+    $('#left-front-rpm').text(message.velocity[0])
+    $('#right-front-rpm').text(message.velocity[1])
+    $('#left-mid-rpm').text(message.velocity[2])
+    $('#right-mid-rpm').text(message.velocity[3])
+    $('#left-back-rpm').text(message.velocity[4])
+    $('#right-back-rpm').text(message.velocity[5])
+  })
 
   // setup a subscriber for the gps_data topic
+  rover_nav_listener = new ROSLIB.Topic({
+    ros: ros,
+    name: 'rover_nav',
+    messageType: 'sensor_msgs/JointState' //subject to change
+  })
+
+  rover_nav_listener.subscribe(function (message) {
+    //TODO: place the data somewhere, call ros node, etc?
+    // idk how this will work exactly, maybe a ros python node does stuff
+    // this could still display the data though i guess
+  })
+
+  // setup a subscriber for the arm_feedback topic
+  rover_feedback_listener = new ROSLIB.Topic({
+    ros: ros,
+    name: 'rover_feedback',
+    messageType: 'std_msgs/String'
+  })
+
+  rover_feedback_listener.subscribe(function (message) {
+    appendToConsole(message.data)
+  })
+
 }
+
+/* functions used in main code */
 
 function requestMuxChannel (elemID, callback) {
   let dev = elemID[elemID.length - 1]
@@ -150,27 +201,35 @@ function requestSerialCommand (command, callback) {
   })
 }
 
-function requestTask (reqTask, reqStatus, buttonID, callback) {
-  let request = new ROSLIB.ServiceRequest({ task: reqTask, status: reqStatus })
+function requestTask (reqTask, reqStatus, buttonID, callback, reqArgs = '') {
+  var request
+  if (reqArgs=='') {
+    request = new ROSLIB.ServiceRequest({ task: reqTask, status: reqStatus })
+  } else {
+    request = new ROSLIB.ServiceRequest({ task: reqTask, status: reqStatus, args : reqArgs })
+  }
   let sentTime = new Date().getTime()
 
-  console.log(request)
+  console.log('request:', request)
   if (reqStatus == 0) {
     appendToConsole('Sending request to stop ' + reqTask + ' task')
   } else if (reqStatus == 1) {
     appendToConsole('Sending request to start ' + reqTask + ' task')
+  } else if (reqStatus == 2) {
+    appendToConsole('Sending request to check ' + reqTask + ' task status')
   }
 
   task_handler_client.callService(request, function (result) {
     let latency = millisSince(sentTime)
-    console.log(result)
-    let msg = result.response//.slice(0, result.response.length - 1) // remove newline character
+    console.log('result:', result)
+    let msg = result.response
     if (
       msg.includes('Failed') ||
       msg.includes('shutdown request') ||
-      msg.includes('unavailable')
+      msg.includes('unavailable') ||
+      msg.includes('is already running') ||
+      msg.includes('not available')
     ) {
-      console.log('really neehaw')
       // how to account for a lack of response?
       appendToConsole('Request failed. Received "' + msg + '"')
       callback([false, msg])
@@ -182,11 +241,50 @@ function requestTask (reqTask, reqStatus, buttonID, callback) {
         $(buttonID)[0].checked = false
       } else if (reqStatus == 1) {
         $(buttonID)[0].checked = true
+      } else if (reqStatus == 2) {
+        if (msg.includes('not')) {
+          $(buttonID)[0].checked = false
+        } else {
+          $(buttonID)[0].checked = true
+        }
       }
-      console.log('really yeehaw')
-      callback([true])
+      callback([true, msg])
     }
   })
+}
+
+function checkTaskStatuses() {
+  if (window.location.pathname == '/') {
+    // check arm listener status
+    requestTask('arm_listener', 2, '#toggle-arm-listener-btn', function(msgs) {
+      console.log(msgs)
+      appendToConsole(msgs)
+      if (msgs[0]) {
+        console.log('tru')
+      } else {
+        console.log('fals')
+      }
+    })
+    // check arm camera stream status
+    requestTask('camera_stream', 2, '#toggle-arm-stream-btn', function(msgs) {
+      console.log(msgs)
+      appendToConsole(msgs)
+      if (msgs[0]) {
+        console.log('truu')
+      } else {
+        console.log('falss')
+      }
+    })
+  } else if (window.location.pathname == '/rover') {
+    console.log('rover page')
+    //do the same but for front and rear cameras
+  } else if (window.location.pathname == '/science') {
+    console.log('rover page')
+    //do the same but for front and rear cameras
+  } /*else if (window.location.pathname == '/rover') { //pds
+    console.log('rover page')
+    //do the same but for front and rear cameras
+  }*/
 }
 
 function sendArmCommand (cmd) {
@@ -194,13 +292,6 @@ function sendArmCommand (cmd) {
   console.log(command)
   appendToConsole('Sending "' + cmd + '" to arm Teensy')
   arm_command_publisher.publish(command)
-}
-
-function sendIkCommand (position, wristAngle) {
-  let command = new ROSLIB.Message({ x : position[0], y : position[1], z : position[2], wrist : wristAngle })
-  console.log(command)
-  appendToConsole('Sending "' + cmd + '" to ikNode')
-  ik_command_publisher.publish(command)
 }
 
 /*
@@ -218,6 +309,46 @@ function sendArmRequest (command, callback) {
   appendToConsole('Sending request to execute command \"' + command + '\"')
 
   arm_request_client.callService(request, function (result) {
+    let latency = millisSince(sentTime)
+    console.log(result)
+    let msg = result.response//.slice(0, result.response.length - 1) // remove newline character
+    if (!result.success) {
+      // how to account for a lack of response?
+      appendToConsole('Request failed. Received "' + msg + '"')
+      // return false
+      callback([false, msg])
+    } else {
+      appendToConsole(
+        'Received "' + msg + '" with ' + latency.toString() + ' ms latency'
+      )
+      // return true
+      callback([true, msg])
+    }
+  })
+}
+
+function sendRoverCommand (cmd) {
+  let command = new ROSLIB.Message({ data: cmd })
+  console.log(command)
+  appendToConsole('Sending "' + cmd + '" to rover Teensy')
+  rover_command_publisher.publish(command)
+}
+
+/*
+// example usage
+sendRoverRequest('this is a command', function (succeeded) {
+  console.log(succeeded ? 'command succeeded' : 'command failed')
+})
+*/
+
+function sendRoverRequest (command, callback) {
+  let request = new ROSLIB.ServiceRequest({ msg: command })
+  let sentTime = new Date().getTime()
+
+  console.log(request)
+  appendToConsole('Sending request to execute command \"' + command + '\"')
+
+  rover_request_client.callService(request, function (result) {
     let latency = millisSince(sentTime)
     console.log(result)
     let msg = result.response//.slice(0, result.response.length - 1) // remove newline character
