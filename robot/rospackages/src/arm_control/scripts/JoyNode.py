aa70cd451bdf8626f4a76177afb7ef9f9f7f5401
Josh Glazer <vashmata@gmail.com>
2019-07-20 17:17:50 -0400

    [#108] Create ROS node for joystick

---
+++ robot/rospackages/src/arm_control/scripts/JoyNode.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from Joy_class import Astro_Joy
+import rospy
+from std_msgs.msg import String
+
+if __name__ == '__main__':
+    rospy.init_node('joy_node',anonymous = False)
+    rospy.loginfo('Initialized joy_node for pub functionality')
+
+    arm_topic = '/arm_command'
+    rospy.loginfo('Beginning to publish to '+arm_topic+' topic')
+    armPub = rospy.Publisher(arm_topic, String, queue_size = 10)
+    
+    wheels_topic = '/rover_command'
+    rospy.loginfo('Beginning to publish to '+wheels_topic+' topic')
+    wheelPub = rospy.Publisher(wheels_topic, String, queue_size = 10)
+
+    my_joy = Astro_Joy(30,30,True)
+    # joystick instance (maximum throttle,fine steering,controlled throttle)
+
+    try:
+        while not rospy.is_shutdown():
+            #if my_joy.isArm:
+            message = my_joy.arm()
+            if message is not None:
+                armPub.publish(message)
+            #elif my_joy.isWheels:
+                #message = my_joy.wheels()
+                #if message is not None:
+                    #elif my_joy.isWheels:
+                    #wheelPub.publish(message)
+            rospy.sleep(.001)  # suitable delays are already in the wheels method
+    except rospy.ROSInterruptException:
+        pass
+
+    def shutdown_hook():
+        rospy.logwarn('Terminating joy_node')
+        rospy.sleep(0.3)
+
+    rospy.on_shutdown(shutdown_hook)
