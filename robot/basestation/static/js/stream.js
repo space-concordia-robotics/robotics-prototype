3d79edb0f3851c13be9c4d51ff5ee85f4b6105aa
KoaTheKoala <koa.wells@hotmail.com>
2019-09-04 22:18:52 -0400

    [#151] Fix stream JS to run locally

--- robot/basestation/static/js/stream.js
+++ robot/basestation/static/js/stream.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 $(document).ready(() => {
   $('#arm-stream-btn').on('click', function (event) {
       event.preventDefault()
@@ -108,4 +109,38 @@ $(document).ready(() => {
               '../static/images/stream-offline.jpg'
       }
   })
+=======
+$(document).ready(function () {
+    $('#arm-stream-btn').on('click', function (event) {
+        // click makes it checked during this time, so trying to enable
+        if ($('#arm-stream-btn').is(':checked')) {
+            $('img#arm-camera-feed')[0].src =
+            'http://localhost:8080/stream?topic=/ArmCamera/image_raw'
+        } else {
+            $('img#arm-camera-feed')[0].src =
+                '../static/images/stream-offline.jpg'
+        }
+    })
+    $('#front-stream-btn').on('click', function (event) {
+        // click makes it checked during this time, so trying to enable
+        if ($('#front-stream-btn').is(':checked')) {
+            $('img#front-camera-feed')[0].src =
+            'http://localhost:8080/stream?topic=/FrontCamera/image_raw'
+        } else {
+            $('img#front-camera-feed')[0].src =
+                '../static/images/stream-offline.jpg'
+        }
+    })
+
+    $('#rear-stream-btn').on('click', function (event) {
+        // click makes it checked during this time, so trying to enable
+        if ($('#rear-stream-btn').is(':checked')) {
+            $('img#rear-camera-feed')[0].src =
+            'http://localhost:8080/stream?topic=/RearCamera/image_raw'
+        } else {
+            $('img#rear-camera-feed')[0].src =
+                '../static/images/stream-offline.jpg'
+        }
+    })
+>>>>>>> Stashed changes
 })
