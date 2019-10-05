e4e5f994bfc81697d24f6314117ca8777cf2ac81
mattskv <msklivas@live.com>
2019-10-04 23:25:06 -0400

    [#162] Symbol check/modify & error msg change

--- branch-name-verification.sh
+++ branch-name-verification.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #If there is already a commit message this hook is disabled
-if [[ "$2" = "commit" ]]; then
+if [[ "$2" = "checkout" ]]; then
     #statements
     exit 0
 fi
@@ -8,10 +8,13 @@ fi
   BRANCHES_TO_SKIP=(master develop staging test)
 
 #Error message to be returned if branch name is not named properly
-BRANCH_NAME_ERROR="Branch name is not named properly, please see wiki for formating: https://github.com/space-concordia-robotics/robotics-prototype/wiki/Work-Flow"
+BRANCH_NAME_ERROR="Branch name is not named properly, please see wiki for formating: https://github.com/space-concordia-robotics/robotics-prototype/wiki/Git-Workflow-and-Conventions"
 
 # Get the current branch name
 BRANCH_NAME=$(git symbolic-ref --short HEAD)
+
+# Trims down the branch name to not include the issue number.
+TRIM_NUM=$(echo $BRANCH_NAME | sed -e 's/[0-9]//g' )
 
 # Trims down the branch name to only include the issue number.
 TRIMMED=$(echo $BRANCH_NAME | sed -e 's/[^0-9][^0-9]*\([0-9][0-9]*\).*/\1/g' )
@@ -30,4 +33,22 @@ fi
  else
     echo $BRANCH_NAME_ERROR
     exit 1
+fi
+#Checks to see if branch name includes any upper case letters. If it does, it exits and tells user to check wiki
+if [[ $TRIM_NUM =~ [\$A-Z] ]];then
+# If any upper case letters are in the branch name,
+  echo $BRANCH_NAME_ERROR
+  exit 1
+fi
+#Checks to see if branch name includes a hypen seperator before the issue number. If not, it exits and tells user to check wiki
+if [[ "${TRIM_NUM: -1}" != *-* ]];then
+# If a hyphen is not found,
+  echo $BRANCH_NAME_ERROR
+  exit 1
+fi
+#Checks to see if branch name includes various other symbols. If so, it exits and tells user to check wiki
+if [[ $BRANCH_NAME == *[\/'!'\@\$\%\,\.\_\+\£\=\¬\<\>\]]* ]];then
+# If any of these symbols are found,
+  echo $BRANCH_NAME_ERROR
+  exit 1
 fi
