870b1b0f8311608f8879c241dccfb856775d2fda
PeterGhimself <petergranitski@gmail.com>
2019-12-26 22:55:59 -0500

    [#151] Reduce default stream resolution to 360p and add table of max supported streams for different resolutions on odroid after testing

--- robot/rospackages/src/task_handler/scripts/start_ros_stream.sh
+++ robot/rospackages/src/task_handler/scripts/start_ros_stream.sh
@@ -31,4 +31,18 @@ fi
 # so in local mode you will get topic names like 'video0Cam', 'video1Cam', and so on
 # in competition mode you will get specifically 'FrontCam', 'RearCam', and 'ArmScienceCam'
 topicName="${port:5}Cam"
-rosrun cv_camera cv_camera_node _device_path:=$port _image_width:=1280 _image_height:=720 __name:=$topicName
+
+# These results are from a test which was run with 3 logitech c920
+# all connected through the same usb hub to the 2.0 hub port of the odroid xu4
+# Last test done on: 26/12/2019
+# +---------------------------------+--------------------------+
+# |resolution --> (width x height)  | max simultaneous streams |
+# |---------------------------------|--------------------------|
+# |240p  --> 352x240                | 3                        |
+# |360p  --> 480x360                | 2                        |
+# |480p  --> 858x480, SD            | 1                        |
+# |720p  --> 1280x720, HD           | 1                        |
+# |1080p --> 1920x1080, full HD     | 0                        |
+# +---------------------------------+--------------------------+
+
+rosrun cv_camera cv_camera_node _device_path:=$port _image_width:=480 _image_height:=360 __name:=$topicName
