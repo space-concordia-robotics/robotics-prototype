d221908a6160f01ccd0a3c2d1e5dedcc6a4ccbe6
PeterGhimself <petergranitski@gmail.com>
2019-12-30 00:33:47 -0500

    [#151][#244] Change requests from @MartensCedric on PR #248 part 6

--- robot/basestation/static/js/roslibjs/ros-helpers.js
+++ robot/basestation/static/js/roslibjs/ros-helpers.js
@@ -320,7 +320,6 @@ function requestTask (
 function checkTaskStatuses () {
   if ($('#camera-local-mode-btn').length >= 1) {
     let isLocal = getCookie('localCameras')
-    console.log('yoooooo')
 
     if (isLocal == '1') {
       $('#camera-local-mode-btn')[0].checked = true
