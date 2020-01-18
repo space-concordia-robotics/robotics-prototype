45e46eef9b4d949b49d092c591f7d92c8a2f1349
AFknUsername <github@freiman.cc>
2020-01-18 14:11:39 -0500

    [#224] Make is so that user does not have to press enter to continue ROS installation

--- EnvironmentSetup.sh
+++ EnvironmentSetup.sh
@@ -61,7 +61,7 @@ then
     
     wget https://raw.githubusercontent.com/ROBOTIS-GIT/robotis_tools/master/install_ros_kinetic.sh
     chmod 755 ./install_ros_kinetic.sh
-    bash ./install_ros_kinetic.sh -y
+    yes "" | bash ./install_ros_kinetic.sh -y
     rm ./install_ros_kinetic.sh
 	
 	source ~/.bashrc
