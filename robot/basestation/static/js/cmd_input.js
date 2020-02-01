39b1b8d306f83d31d396b02a0bc79a7aa1c6312a
ninjaweeaboo <williamwells2013@hotmail.com>
2020-02-01 13:40:27 -0500

    [#140] Add some error handling

--- robot/basestation/static/js/cmd_input.js
+++ robot/basestation/static/js/cmd_input.js
@@ -2,10 +2,20 @@ $(document).ready(() => {
   const send_cmd = $('#send-serial-cmd')
   const cmd_input = $('#serial-cmd-input')
 
-  function sendCMD() {
+  function handleCMD() {
     const full_cmd = cmd_input.val()
     const prefix = full_cmd.split(' ').shift()
     const cmd = full_cmd.split(' ').pop()
+    if (cmd != '' && cmd != full_cmd) {
+      sendCMD(prefix, cmd)
+    }
+
+    else {
+      appendToConsole('no command to send')
+    }
+  }
+
+  function sendCMD(prefix, cmd) {
     if (prefix == 'rover') {
       sendRoverCommand(cmd)
     } else if (prefix == 'arm') {
@@ -20,17 +30,17 @@ $(document).ready(() => {
     clearCmdInput()
   }
 
+  function clearCmdInput () {
+    $(cmd_input).val('')
+  }
+
   send_cmd.click(function () {
-    sendCMD()
+    handleCMD()
   })
 
   document.getElementById('serial-cmd-input').addEventListener('keyup', function (event) {
     if (event.code === 'Enter') {
-      sendCMD()
+      handleCMD()
     }
   })
-
-  function clearCmdInput () {
-    $(cmd_input).val('')
-  }
 })
