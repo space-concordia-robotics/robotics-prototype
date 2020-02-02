8da2aad501f1a789c37003abd4d648aa9588b520
adrientremblay <adrien@adrientremblay.com>
2020-02-02 00:43:20 -0500

    [#229] Changes requested by @PeterGhimself in PR #272 take two

--- robot/basestation/static/js/app.js
+++ robot/basestation/static/js/app.js
@@ -1,4 +1,5 @@
 PING_THROTTLE_TIME = 1000
+errorSoundURL = '../../static/sound/error.mp3'
 /* eslint-disable no-unused-lets */
 $(document).ready(() => {
   // @TODO: fix this hacky workaround (refer to issue #142)
@@ -257,5 +258,5 @@ function pingOdroid (timeoutVal = REQUEST_TIMEOUT) {
 plays an error sound once -> warning will not play if the document has not been interacted with yet
 */
 function errorSound (){
-  new Audio('../../static/sound/error.mp3').play()
+  new Audio(errorSoundURL).play()
 }
