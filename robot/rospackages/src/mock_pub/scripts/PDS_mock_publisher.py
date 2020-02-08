aa83f1fdc2be6ded05240d2d2d8d042eec053046
adrientremblay <adrien@adrientremblay.com>
2020-02-08 17:03:54 -0500

    [#284] Adding 'rise' mode which publishes to battery_voltage topic

--- robot/rospackages/src/mock_pub/scripts/PDS_mock_publisher.py
+++ robot/rospackages/src/mock_pub/scripts/PDS_mock_publisher.py
@@ -1,15 +1,47 @@
+import sys
+import time
 import rospy
 from std_msgs.msg import Float32
 
+#decimals for all pubs
+DECIMALS = 2
+
+# temp values -> should be extracted from the js file later
+voltage_max = 14;
+voltage_min = 3;
+
+def get_time_millis():
+  millis = int(round(time.time() * 1000))
+  return millis
+
+
 def PDS_pub():
-  pub = rospy.Publisher('PDS_mock_publisher', Float32, queue_size=10)
-  rospy.init_node('PDS_pub', anonymous=True)
+  try:
+    mode = sys.argv[1]
+  except:
+    print('please give a mode value!')
+    sys.exit()
+
+  pub = rospy.Publisher('battery_voltage', Float32, queue_size=10)
+  rospy.init_node('PDS_mock_pub', anonymous=True)
   rate = rospy.Rate(10) # 10hz
-  while not rospy.is_shutdown():
-    pub.publish(420)
-    rate.sleep()
+
+  if (mode == 'rise'):
+    start_vol = 4 #volts
+    speed = 1 #volts per second
+
+    while not rospy.is_shutdown():
+      time_now = get_time_millis()
+      pub_vol = round(start_vol +  speed * ((time_now - start_time) / 1000), DECIMALS)
+      pub.publish(pub_vol)
+      print(pub_vol)
+      rate.sleep()
+  else :
+    print('Invalid mode!')
 
 if __name__ == '__main__':
+  start_time = get_time_millis();
+
   try:
     PDS_pub()
   except rospy.ROSInterruptException:
