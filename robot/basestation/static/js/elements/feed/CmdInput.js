912c584ba3e092195c25afe96e32a4cd21db488b
ninjaweeaboo <williamwells2013@hotmail.com>
2020-02-13 15:29:50 -0500

    [#140] Add docstrings to new js functions

--- robot/basestation/static/js/elements/feed/CmdInput.js
+++ robot/basestation/static/js/elements/feed/CmdInput.js
@@ -4,6 +4,7 @@ $(document).ready(() => {
   const INVALID_CMD_MSG = 'invalid command, please check input and try again'
   const INVALID_MODULE_MSG = 'Invalid module, please input module before command. Valid modules are rover, arm, IK and PDS'
 
+  // Get full command and split into its prefix and command to send to module
   function getCMD() {
     const full_cmd = cmd_input.val()
     const prefix = full_cmd.split(' ').shift().toLowerCase()
@@ -14,6 +15,7 @@ $(document).ready(() => {
     clearCmdInput()
   }
 
+  // Check if the command is valid
   function isCMDValid(full_cmd, cmd) {
     if ((cmd == '' && full_cmd != '') || (cmd == full_cmd)) {
       if (full_cmd != '') {
@@ -25,6 +27,7 @@ $(document).ready(() => {
     }
   }
 
+  // Send command to module, print error if invalid module was inputted
   function sendCMD(prefix, cmd) {
     switch(prefix) {
       case 'rover':
@@ -44,12 +47,15 @@ $(document).ready(() => {
     }
   }
 
+  // Clear command input field
   function clearCmdInput () {
     $(cmd_input).val('')
   }
 
+  // Run getCMD in order to process command inputted in field, when send command button is clicked
   send_cmd.click(getCMD)
 
+  // Run getCMD in order to process command inputted into box, when enter key is pressed while focused on input field
   document.getElementById('serial-cmd-input').addEventListener('keyup', function (event) {
     if (event.code === 'Enter') {
       getCMD()
