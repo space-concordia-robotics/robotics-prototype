1e7f618ca780d24045fecc066ba13393c5c44960
badgerwild <jklimock@gmail.com>
2020-02-20 16:07:46 -0500

    [#192] refactor script class for better portablility and oop

--- robot/rospackages/src/image_scp/image_scp.py
+++ robot/rospackages/src/image_scp/image_scp.py
@@ -1,11 +1,32 @@
 from paramiko import SSHClient
 from scp import SCPClient
-from config import (host, user, remote_path_1, local)
-ssh = SSHClient()
-ssh.load_system_host_keys()
-ssh.connect(hostname=host, username=user)
+#from congif import (host, user, remote_path_1, local)
 
-scp = SCPClient(ssh.get_transport())
+class remote_connection:
+    def __init__(self, host, user, remote_path, file_name):
+        self.host = host
+        self.user = user
+        self.remote_path = remote_path
+        self.file_name = file_name
 
-scp.put(local, recursive=True,
-        remote_path=remote_path_1)
+    def connect(self):
+        self.client = SSHClient()
+        self.client.load_system_host_keys()
+        self.client.connect(hostname=self.host, username=self.user)
+        self.scp = SCPClient(self.client.get_transport())
+
+    def disconnect(self):
+        self.client.close()
+        self.scp.close()
+
+
+
+
+#ssh = SSHClient()
+#ssh.load_system_host_keys()
+#ssh.connect(hostname="login.encs.concordia.ca", username='user')
+
+#scp = SCPClient(ssh.get_transport())
+
+#scp.put('/test2.png', recursive=True,
+ #       remote_path='remotepath    e)
