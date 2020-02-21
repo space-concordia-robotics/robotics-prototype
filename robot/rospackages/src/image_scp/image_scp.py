d2d71d4ee57d8507e6bb3cba6999eeaaeec72ae2
badgerwild <jklimock@gmail.com>
2020-02-20 19:22:46 -0500

    [#192] refactor to impliment script with classes and functions

--- robot/rospackages/src/image_scp/image_scp.py
+++ robot/rospackages/src/image_scp/image_scp.py
@@ -1,6 +1,7 @@
 from paramiko import SSHClient
-from scp import SCPClient
-#from congif import (host, user, remote_path_1, local)
+from scp import SCPClient, SCPException
+import argparse
+from congif import (host, user, remote_path_1, local)
 
 class remote_connection:
     def __init__(self, host, user, remote_path, file_name):
@@ -19,7 +20,25 @@ class remote_connection:
         self.client.close()
         self.scp.close()
 
+    def send_image(self, image):
+        try:
+            self.scp.put(image, recursive=True, remote_path=self.remote_path)
+        except SCPException as error:
+            raise error
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="the file that gets sent")
+    parser.add_argument("image_file", action="append")
+    args = parser.parse_args()
+    to_transfer = args.image_file[0]
+
+    sender = remote_connection(host, user, remote_path_1, to_transfer)
+    sender.connect()
+    sender.send_image(sender.file_name)
+    sender.disconnect()
+
+
+    print(args.image_file[0])
 
 
 #ssh = SSHClient()
@@ -29,4 +48,4 @@ class remote_connection:
 #scp = SCPClient(ssh.get_transport())
 
 #scp.put('/test2.png', recursive=True,
- #       remote_path='remotepath    e)
+ #       remo te_path='remotepath    e)
