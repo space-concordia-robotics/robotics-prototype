9f40828a583d76856f5173309ddafee966be9c2a
adrientremblay <adrien@adrientremblay.com>
2020-03-07 17:24:53 -0500

    [#284] Refactor to use resuable function for voltage pub

--- robot/rospackages/src/mcu_control/scripts/PDSMockPublisher.py
+++ robot/rospackages/src/mcu_control/scripts/PDSMockPublisher.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import sys
 import time
 import rospy
 from std_msgs.msg import Float32
@@ -7,46 +6,55 @@ from decimal import *
 
 getcontext().prec = 4
 
-def PDS_pub():
-  pub = rospy.Publisher('battery_voltage', Float32, queue_size=10)
-  rospy.init_node('PDS_mock_pub', anonymous=True)
-  rate = rospy.Rate(10) #hz
 
-  # temp values -> should be extracted from the js file later
-  voltage_max = Decimal(20.0)
-  voltage_min = Decimal(10.0)
-  voltage_safe = Decimal(15.0)
-  speed = Decimal((0, (0, 1), -1))
-
-  voltage_current = voltage_safe #starting voltage
-  voltage_target = voltage_safe #default value
-
-  while not rospy.is_shutdown():
+def shift_val(val, publisher, state_param_name, min_val, max_val, safe, speed):
     try:
-      mode = rospy.get_param('PDS_mock_mode_voltage')
+      mode = rospy.get_param(state_param_name)
     except:
       print('PDS_mock_mode parameter error!')
       sys.exit()
 
     if (mode == 'stable'):
-      voltage_target = voltage_safe
+      target = safe
     elif (mode == 'rise'): 
-      voltage_target = voltage_max
+      target = max_val
     elif (mode == 'fall'):
-      voltage_target = voltage_min
+      target = min_val
     else :
       print('Invalid mode!')
       sys.exit()
 
-    direction = voltage_target - voltage_current
-    print(voltage_current)
+    direction = target - val 
     if (abs(direction) >= speed):
       if direction > 0:
-        voltage_current += speed
+        val += speed 
       elif direction < 0:
-        voltage_current -= speed
+        val -= speed
+    
+    publisher.publish(val)
+    return val
 
-    pub.publish(voltage_current)
+def PDS_pub():
+  pub_voltage = rospy.Publisher('battery_voltage', Float32, queue_size=10)
+  rospy.init_node('PDS_mock_pub', anonymous=True)
+  rate = rospy.Rate(10) #hz
+  
+  # voltage values 
+  voltage_max = Decimal(20.0)
+  voltage_min = Decimal(10.0)
+  voltage_safe = Decimal(15.0)
+  voltage_speed = Decimal((0, (0, 1), -1))#0.1
+  voltage_current = voltage_safe
+
+  # temperature values
+  temperature_max = Decimal(100.0)
+  temperature_min = Decimal(-20.0)
+  temperature_safe = Decimal(50.0)
+  speed_temperature = Decimal(1)  
+
+  while not rospy.is_shutdown():
+    voltage_current = shift_val(voltage_current, pub_voltage, 'PDS_mock_mode_voltage', voltage_min, voltage_max, voltage_safe, voltage_speed)
+    
     rate.sleep()
 
 if __name__ == '__main__':
@@ -55,7 +63,3 @@ if __name__ == '__main__':
     PDS_pub();
   except rospy.ROSInterruptException:
     pass
-      
-
-
-
