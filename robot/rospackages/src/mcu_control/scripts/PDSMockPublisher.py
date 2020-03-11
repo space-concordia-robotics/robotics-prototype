ff539cd6ca4d2ee6caad3e1a6c829da58ca53c4c
adrientremblay <adrien@adrientremblay.com>
2020-03-11 12:06:45 -0400

    [#284] Add temperature functionality

--- robot/rospackages/src/mcu_control/scripts/PDSMockPublisher.py
+++ robot/rospackages/src/mcu_control/scripts/PDSMockPublisher.py
@@ -60,16 +60,22 @@ def PDS_pub():
   while not rospy.is_shutdown():
     #shift values
     voltage_current = shift_val(voltage_current, 'PDS_mock_mode_voltage', voltage_min, voltage_max, voltage_safe, voltage_speed)
-    #temperajhgt
+    temperature1_current = shift_val(temperature1_current, 'PDS_mock_mode_temp1', temperature_min, temperature_max, temperature_safe, temperature_speed)
+    temperature2_current = shift_val(temperature2_current, 'PDS_mock_mode_temp2', temperature_min, temperature_max, temperature_safe, temperature_speed)
+    temperature3_current = shift_val(temperature3_current, 'PDS_mock_mode_temp3', temperature_min, temperature_max, temperature_safe, temperature_speed)
+ 
         
     #publish values
     pub_voltage.publish(voltage_current)    
-    pub_temperature.publish(Point(1, 2, 3))
+    pub_temperature.publish(Point(temperature1_current, temperature2_current, temperature3_current))
 
     rate.sleep()
 
 if __name__ == '__main__':
   rospy.set_param('PDS_mock_mode_voltage', 'stable')
+  rospy.set_param('PDS_mock_mode_temp1', 'stable')
+  rospy.set_param('PDS_mock_mode_temp2', 'stable')
+  rospy.set_param('PDS_mock_mode_temp3', 'stable')
   try:
     PDS_pub();
   except rospy.ROSInterruptException:
