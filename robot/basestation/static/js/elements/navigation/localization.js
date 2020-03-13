96a0c778a4a3244604dcd90e312c1a772fc67df6
AFknUsername <github@freiman.cc>
2020-03-13 12:51:35 -0400

    [#56] WIP: still need to implement proper ROS

--- robot/basestation/static/js/elements/navigation/localization.js
+++ robot/basestation/static/js/elements/navigation/localization.js
@@ -1,3 +1,4 @@
+//$(document).ready(() => {
 let rover_heading = 0
 let rover_x = 0
 let rover_y = 0
@@ -12,20 +13,36 @@ let keys = { //javascript keycodes
     's': 83,
     'd': 68
 }
+let PIXELS_PER_METER = 5
+let base_lat = $('#antenna-latitude').text()
+let base_long = $('#antenna-longitude').text()
+let CANVAS_HEIGHT = 300
+let CANVAS_WIDTH = 450
 
-let base_lat
-let base_long
-$(document).ready(() => {
-    // the elements might not be initialized yet, must test
-    base_lat = $('#antenna-latitude').text()
-    base_long = $('#antenna-longitude').text()
-})
+let BASE_POSITION = {
+    'x': 0,
+    'y': 0
+}
+let BASE_DIAMETER = 10
+
+let ROVER_POINT_A = {
+    'x': 0,
+    'y': -15
+}
+let ROVER_POINT_B = {
+    'x': 10,
+    'y': 10
+}
+let ROVER_POINT_C = {
+    'x': -10,
+    'y': 10
+}
 
 function setup() {
-    let canvas = createCanvas(450, 300)
+    let canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT)
     canvas.parent('#canvasContainer')
     canvas_pos = $('#defaultCanvas0').offset()
-    angleMode(DEGREES)
+    angleMode(DEGREES) // makes p5.js use DEG instead of RAD
 }
 
 function draw() {
@@ -41,7 +58,6 @@ function draw() {
     if (keyIsDown(keys['d'])) { // spin cw
         rover_heading += 1
     }
-    adjustmap_rotation_deg() // keep rover_heading within 0-360
     if (keyIsDown(keys['w'])) { // move forward
         rover_x += sin(rover_heading);
         rover_y -= cos(rover_heading);
@@ -52,11 +68,11 @@ function draw() {
     }
 
     // canvas static
-    background(225) // draw background
+    background(225) // draw background with RGB(255,255,255)
     translate(width / 2, height / 2) // make the (0,0) point the center of canvas
     scale(zoom_value) // zoom to given value
     draw_grid() // draw the grid for reference
-    circle(0, 0, 10) // draw circle base station
+    circle(BASE_POSITION['x'], BASE_POSITION['y'], BASE_DIAMETER) // draw circle base station
 
     // canvas dynamic
     draw_rover()
@@ -82,29 +98,20 @@ function draw_grid(message) {
 
 function draw_rover() {
     check_rover_position() // get the heading, lat, and long values from ROS topic
-    translate(rover_x, rover_y) // move to rover position
-    rotate(rover_heading) // rotate to rover heading
-    triangle(0, -15, 10, 10, -10, 10) // draw triangle rover
+    translate(rover_x, rover_y) // move the rover
+    rotate(rover_heading) // rotate the rover
+    triangle(ROVER_POINT_A['x'], ROVER_POINT_A['y'], ROVER_POINT_B['x'], ROVER_POINT_B['y'], ROVER_POINT_C['x'], ROVER_POINT_C['y']) // draw triangle rover
 }
 
 // register mouse wheel event and do the zoom
 function mouseWheel(e) {
-    if (winMouseX > canvas_pos['left'] && winMouseX < canvas_pos['left'] + width && winMouseY > canvas_pos['top'] && winMouseY < canvas_pos['top'] + height) {
-        let newzoom_value = zoom_value - e.delta / 500
+    if (winMouseX > canvas_pos['left'] && winMouseX < canvas_pos['left'] +
+        width && winMouseY > canvas_pos['top'] && winMouseY < canvas_pos['top'] + height) {
+        let newzoom_value = zoom_value - e.delta / 500 // delta is too big for the scale() fn, so must be divided by 500 to reduce amount fo zoom per scroll
         if (newzoom_value >= MIN_ZOOM && newzoom_value <= MAX_ZOOM) {
             zoom_value = newzoom_value
             return false
         }
-    }
-}
-
-// function which keeps heading within 0-360
-function adjustmap_rotation_deg() {
-    if (-rover_heading < 0) {
-        rover_heading -= 360
-    }
-    if (-rover_heading == 360) {
-        rover_heading += 360
     }
 }
 
@@ -119,11 +126,10 @@ function check_rover_position() {
 
 // function which converts the values into something usable by the canvas
 function adjust_values_to_cartesian(rover_lat, rover_long, rover_heading) {
-    let angle = Direction(base_lat, base_long, rover_lat, rover_long)
-    let distance = Distance(base_lat, base_long, rover_lat, rover_long)
+    let direction = direction_to_rover(base_lat, base_long, rover_lat, rover_long)
+    let distance = distance_to_rover(base_lat, base_long, rover_lat, rover_long)
 
-    // multiply result by 5 to move 5 pixels per meter
-    // needs to be confirmed
-    rover_x = 5 * distance * Math.cos(angle)
-    rover_y = 5 * distance * Math.sin(angle)
+    rover_x = PIXELS_PER_METER * distance * Math.cos(direction)
+    rover_y = PIXELS_PER_METER * distance * Math.sin(direction)
 }
+//})
