cd3cd9d71a36e0edcecab1c131bfe38365d3b230
AFknUsername <github@freiman.cc>
2020-03-17 14:52:52 -0400

    [#56] Organize functions and implement canvas resizing

--- robot/basestation/static/js/elements/navigation/localization.js
+++ robot/basestation/static/js/elements/navigation/localization.js
@@ -41,7 +41,104 @@ let sketch = function(sketch) {
     }
 
     sketch.draw = function() {
-        // all of the manual controls
+
+        if (sketch.keyIsPressed == true) {
+            controls()
+        }
+
+        sketch.background(225)
+        drawResizeButton()
+        sketch.translate(sketch.width / 2, sketch.height / 2)
+        sketch.scale(zoomValue)
+        drawGrid()
+        // (x, y, diameter)
+        sketch.circle(0, 0, 10)
+        drawRover()
+    }
+
+    /* ----------------------------------------
+    FUNCTIONS THAT DRAW THINGS
+    */
+
+    function drawRover() {
+        sketch.push()
+        sketch.translate(roverX, roverY)
+        sketch.rotate(roverHeading)
+        // 3 points (x, y, x, y, x ,y)
+        sketch.triangle(0, -15, 10, 10, -10, 10)
+        sketch.pop()
+    }
+
+    function drawGrid() {
+        sketch.push()
+        sketch.stroke(200)
+        sketch.fill(120)
+
+        for (let x = -10000; x < 10000; x += 100) {
+            sketch.line(x, -10000, x, 10000)
+            sketch.text(x, x + 1, 12)
+        }
+
+        for (let y = -10000; y < 10000; y += 100) {
+            sketch.line(-10000, y, 10000, y)
+            sketch.text(y, 1, y + 12)
+        }
+
+        sketch.pop()
+    }
+
+    function drawResizeButton() {
+        sketch.push()
+        sketch.fill(250, 0, 0)
+        sketch.arc(sketch.width, sketch.height, 20, 20, 180, 270, sketch.PIE)
+        sketch.pop()
+    }
+
+
+    /* ----------------------------------------
+    p5 EVENT FUNCTIONS
+    */
+
+    // register mouse wheel event and zoom accordingly
+    sketch.mouseWheel = function(e) {
+        if (sketch.mouseX > 0 && sketch.mouseX < sketch.width && sketch.mouseY > 0 && sketch.mouseY < sketch.height) {
+            let newZoomValue = zoomValue - e.delta / 500
+            if (newZoomValue >= MIN_ZOOM && newZoomValue <= MAX_ZOOM) {
+                zoomValue = newZoomValue
+                return false
+            }
+        }
+    }
+
+    // checks if mouse is being dragged accross screen
+    sketch.mouseDragged = function() {
+
+        // canvas resize
+        let r = 10
+        let d = sketch.dist(sketch.mouseX, sketch.mouseY, sketch.width, sketch.height)
+        if (d < r) {
+            sketch.mouseReleased = function() {
+                sketch.resizeCanvas(sketch.mouseX, sketch.mouseY)
+            }
+        }
+        return false
+    }
+
+    /* ----------------------------------------
+    UTILITY FUNCTIONS
+    */
+
+    // function which converts the values into something usable by the canvas
+    function adjustValuesToCartesian(roverLat, roverLong) {
+        let direction = directionToRover(baseLat, baseLong, roverLat, roverLong) // angle in degrees
+        let distance = distanceToRover(baseLat, baseLong, roverLat, roverLong) // distance in meters
+
+        roverX = distance * Math.cos(direction)
+        roverY = distance * Math.sin(direction)
+    }
+
+    // implements manual controls.
+    function controls() {
         if (sketch.keyIsDown(keys['r'])) { //reset position
             roverHeading = 0
             zoomValue = 1
@@ -61,58 +158,5 @@ let sketch = function(sketch) {
             roverX -= sketch.sin(roverHeading)
             roverY += sketch.cos(roverHeading)
         }
-
-        sketch.background(225)
-        sketch.translate(sketch.width / 2, sketch.height / 2)
-
-        sketch.scale(zoomValue)
-        drawGrid()
-        // (x,y,diameter)
-        sketch.circle(0, 0, 10)
-        drawRover()
-    }
-
-    // function to draw the lines and values on canvas
-    function drawGrid(message) {
-        sketch.stroke(200)
-        sketch.fill(120)
-        for (let x = -10000; x < 10000; x += 100) {
-            sketch.line(x, -10000, x, 10000)
-            sketch.text(x, x + 1, 12)
-        }
-        for (let y = -10000; y < 10000; y += 100) {
-            sketch.line(-10000, y, 10000, y)
-            sketch.text(y, 1, y + 12)
-        }
-    }
-
-    // register mouse wheel event and do the zoom
-    sketch.mouseWheel = function(e) {
-        if (sketch.mouseX > 0 && sketch.mouseX < sketch.width && sketch.mouseY > 0 && sketch.mouseY < sketch.height) {
-            let newZoomValue = zoomValue - e.delta / 500
-            if (newZoomValue >= MIN_ZOOM && newZoomValue <= MAX_ZOOM) {
-                zoomValue = newZoomValue
-                return false
-            }
-        }
-    }
-
-    // draws rover at the apropriate coordinates and heading
-    function drawRover() {
-        sketch.push()
-        sketch.translate(roverX, roverY)
-        sketch.rotate(roverHeading)
-        // 3 points (x, y, x, y, x ,y)
-        sketch.triangle(0, -15, 10, 10, -10, 10)
-        sketch.pop()
-    }
-
-    // function which converts the values into something usable by the canvas
-    function adjustValuesToCartesian(roverLat, roverLong) {
-        let direction = directionToRover(baseLat, baseLong, roverLat, roverLong) // angle in degrees
-        let distance = distanceToRover(baseLat, baseLong, roverLat, roverLong) // distance in meters
-
-        roverX = distance * Math.cos(direction)
-        roverY = distance * Math.sin(direction)
     }
 }
