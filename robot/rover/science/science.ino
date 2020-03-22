a3edfd0798478a6c91af12d97ce12fb6ef362e36
steamypotato <hoopahydra@gmail.com>
2020-03-22 01:10:30 -0400

    [#204] better names, a few comments

--- robot/rover/science/science.ino
+++ robot/rover/science/science.ino
@@ -107,6 +107,7 @@ void turnTable (int cuvette, int desiredPosition);// sends the wanted cuvette to
 void debouncing(void); //pure magic
 float photoChoice(int led); // choses the photoresistor from which to read the voltage
 String getValue(String data, char separator, int index);//splits input string to feed multiple variables
+int getCount(String data, char target);
 
 Servo table;
 
@@ -376,7 +377,7 @@ void loop() {
       }
       else if (cmd == "eup") {
         // sets elevator direction clockwise
-        elevatorDirection = 1- 0;
+        elevatorDirection = 1 - 0;
         digitalWrite(ELEVATOR_DIRECTION, 1 - elevatorDirection);
         UART_PORT.println("SCIENCE eup done");
       }
@@ -716,7 +717,7 @@ void elevatorBottomInterrupt () {
   }
   analogWrite(ELEVATOR, 0);
   previousElevatorState = 'n';
-  elevatorDirection = 1- 0;
+  elevatorDirection = 1 - 0;
 }
 
 void cuvettePosition() {
