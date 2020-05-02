9ab7e9964f4f0c84ba3b92ea28ed27517dc6d7d9
Shirin Panahi Moghadam <34474709+shayrin0@users.noreply.github.com>
2020-05-02 15:25:56 -0400

    [#258] fix coding conventions for JS
    Co-authored-by: Cedric Martens <cedricmartens98@gmail.com>

--- robot/basestation/static/js/elements/feed/camera.js
+++ robot/basestation/static/js/elements/feed/camera.js
@@ -342,6 +342,6 @@ $(document).ready(() => {
   })
 
   $('.camera-popup' ).click(function() {
-    window.open('/camerapopup',"", 'height='+screen.height+', width='+screen.width);
+    window.open('/camerapopup', "", 'height=' + screen.height + ', width=' + screen.width);
   })
 })
