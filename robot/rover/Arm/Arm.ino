c638460c2236b8fefd0fcdd1519a04040bd59bde
Joshua Glazer <josh.glazer@spaceconcordia.ca>
2020-05-16 13:54:11 -0400

    [#316] Remove spaces between arrows
    Co-authored-by: PeterGhimself <petergranitski@gmail.com>

--- robot/rover/Arm/Arm.ino
+++ robot/rover/Arm/Arm.ino
@@ -440,9 +440,9 @@ void loop() {
                   UART_PORT.print("ARM $E,Error: requested motor ");
                   UART_PORT.print(i + 1);
                   UART_PORT.print(" angle is not within angle limits: ");
-                  UART_PORT.print(motorArray[i] -> minJointAngle);
+                  UART_PORT.print(motorArray[i]->minJointAngle);
                   UART_PORT.print(" and ");
-                  UART_PORT.println(motorArray[i] -> maxJointAngle);
+                  UART_PORT.println(motorArray[i]->maxJointAngle);
 #elif defined(DEBUG_MODE) || defined(USER_MODE)
                   // this is SUPER DUPER GROSS
                   int tempVal = i + 1;
