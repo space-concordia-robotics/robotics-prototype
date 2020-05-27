0f353500306a80663a8bd79dc242da8b931b0feb
MartensCedric <cedricmartens98@gmail.com>
2020-05-27 19:12:58 -0400

    [#310] Made it api style

--- robot/rospackages/src/ping_acknowledgment/scripts/ping_response_client.py
+++ robot/rospackages/src/ping_acknowledgment/scripts/ping_response_client.py
@@ -3,54 +3,59 @@
 import sys
 import datetime
 import rospy
-from std_msgs.msg import Float32
 from ping_acknowledgment.srv import *
 
+#
+# ping_response_client
+# 
+# Use without arguments to obtain detailed timestamps and latency data
+# Use option "api" to just receive the time it took in millisecond for the ping.
+#
 
-def ping_response_client(msg):
+
+def ping_server():
     # convenience function that blocks until 'ping_response' is available
     #rospy.wait_for_service('ping_response')
 
     try:
         ping_response = rospy.ServiceProxy('ping_response', PingResponse)
-        resp1 = ping_response(msg)
+        resp1 = ping_response("")
         return resp1.response
     except rospy.ServiceException as e:
         return "Service call failed: " + str(e)
 
 
 def usage():
-    return "usage: {:s} [msg]".format(sys.argv[0])
+    return "usage: {:s} [OPTIONS] [api]".format(sys.argv[0])
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 2:
         print(usage())
 
+    api = False
+    if len(sys.argv) == 2 and sys.argv[1] == "api":
+        api = True
+
+
     rospy.init_node("ping_response_client")
-
-    if len(sys.argv) != 1:
-        # Figure out if being called from roslaunch or roscore/python
-        if "__name:=" in sys.argv[1]:
-            msg = rospy.get_param("~ping_msg")
-        else:
-            msg = sys.argv[1]
-    else:
-        msg = 'silence'
-        print("Warning: no message argument, sending 'silence' to ping response server\n" + usage())
-
     sent = datetime.datetime.now()
     sent_ts = sent.strftime('%Y-%m-%dT%H:%M:%S') + ('-%02d' % (sent.microsecond / 10000))
-    print("Pinging server with ping_acknowledgment service")
-    print(sent_ts)
-    print("---")
-
-    print("Response: " + ping_response_client(msg))
+    
+    ping_server()
     received = datetime.datetime.now()
     received_st = sent.strftime('%Y-%m-%dT%H:%M:%S') + ('-%02d' % (received.microsecond / 10000))
-    print(received_st)
+
+    if not api:
+        print("Pinging server with ping_acknowledgment service")
+        print(sent_ts)
+        print("---")
+
+        print("Received Response")
+        print(received_st)
 
     diff = received - sent
-    print("Latency: " + str(diff.total_seconds() * 1000) + " ms")
-    ping = rospy.Publisher('/ping_value', Float32, queue_size=10, latch=True)
-    ping.publish(Float32(diff.total_seconds() * 1000))
+    if api:
+        print(diff.microseconds / 1000)
+    else:
+        print("Latency: " + str(diff.total_seconds() * 1000) + " ms")
