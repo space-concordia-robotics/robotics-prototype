0d244c1e6492fcfccdb2c03511218cd54fb28b62
MartensCedric <cedricmartens98@gmail.com>
2020-06-06 18:37:10 -0400

    [#138] Reindent navbar.js

--- robot/basestation/static/js/elements/feed/navbar.js
+++ robot/basestation/static/js/elements/feed/navbar.js
@@ -1,89 +1,89 @@
 $(document).ready(() => {
   function append_css(file) {
-      logInfo('Using theme : ' + file)
+    logInfo('Using theme : ' + file)
 
-      var link = document.createElement('link')
-      link.href = '/static/css/' + file + '.css'
-      link.type = 'text/css'
-      link.rel = 'stylesheet'
-      link.media = 'screen,print'
+    var link = document.createElement('link')
+    link.href = '/static/css/' + file + '.css'
+    link.type = 'text/css'
+    link.rel = 'stylesheet'
+    link.media = 'screen,print'
 
-      document.getElementsByTagName('head')[0].appendChild(link)
+    document.getElementsByTagName('head')[0].appendChild(link)
   }
 
   function prefer_theme(name) {
-      logInfo('Found theme : ' + name)
-      setCookie('theme', name, 365)  // setCookie() from helpers.js
-      append_css('themes/' + name)
+    logInfo('Found theme : ' + name)
+    setCookie('theme', name, 365)  // setCookie() from helpers.js
+    append_css('themes/' + name)
   }
 
   preferred_theme = getCookie('theme')  // getCookie() from helpers.js
-    if (preferred_theme != '') {
-      prefer_theme(preferred_theme)
-    } else {
-      prefer_theme('lofi')
-    }
+  if (preferred_theme != '') {
+    prefer_theme(preferred_theme)
+  } else {
+    prefer_theme('lofi')
+  }
 
   $('#theme-mantis').click(function () {
-      prefer_theme('mantis')
+    prefer_theme('mantis')
   })
   $('#theme-lofi').click(function () {
-      prefer_theme('lofi')
+    prefer_theme('lofi')
   })
 
 
   function checkListenerStates() {
     let listener = ''
-      let url = window.location.pathname
-      let toggleButtonID = 'toggle-'
+    let url = window.location.pathname
+    let toggleButtonID = 'toggle-'
 
-      if (url == '/rover') {
-        listener = ROVER_LISTENER_TASK
-          toggleButtonID += 'rover-listener-btn'
-      } else if (url == '/' || url == '/arm') {
-        listener = ARM_LISTENER_TASK
-          toggleButtonID += 'arm-listener-btn'
-      } else if (url == '/science') {
-        listener = SCIENCE_LISTENER_TASK
-          toggleButtonID += 'science-listener-btn'
-      } else if (url == '/pds') {
-        listener = PDS_LISTENER_TASK
-          toggleButtonID += 'pds-listener-btn'
-      }
-      else{
-        return; // Pages that don't have this button should not run the code below (See #367)
-      }
+    if (url == '/rover') {
+      listener = ROVER_LISTENER_TASK
+      toggleButtonID += 'rover-listener-btn'
+    } else if (url == '/' || url == '/arm') {
+      listener = ARM_LISTENER_TASK
+      toggleButtonID += 'arm-listener-btn'
+    } else if (url == '/science') {
+      listener = SCIENCE_LISTENER_TASK
+      toggleButtonID += 'science-listener-btn'
+    } else if (url == '/pds') {
+      listener = PDS_LISTENER_TASK
+      toggleButtonID += 'pds-listener-btn'
+    }
+    else{
+      return; // Pages that don't have this button should not run the code below (See #367)
+    }
 
     let serialType = $('#serial-type').text()
 
-      requestTask(
-          listener,
-          STATUS_CHECK,
-          function (msgs) {
-            console.log('msgs', msgs)
-              if (msgs[1].includes('not') || msgs[1].includes('timeout')) {
-                $('#' + toggleButtonID)[0].checked = false
-              } else {
-                $('#' + toggleButtonID)[0].checked = true
-              }
-          },
-          serialType
-          )
+    requestTask(
+        listener,
+        STATUS_CHECK,
+        function (msgs) {
+          console.log('msgs', msgs)
+          if (msgs[1].includes('not') || msgs[1].includes('timeout')) {
+            $('#' + toggleButtonID)[0].checked = false
+          } else {
+            $('#' + toggleButtonID)[0].checked = true
+          }
+        },
+        serialType
+    )
   }
 
   checkListenerStates()
 
-    function isListenerOpen () {
-      return (
-          (window.location.pathname == '/rover' &&
-           $('#toggle-rover-listener-btn')[0].checked == true) ||
-          (window.location.pathname == '/' &&
-           $('#toggle-arm-listener-btn')[0].checked == true) ||
-          (window.location.pathname == '/science' &&
-           $('#science-listener-btn')[0].checked == true)
-          // pds Listener
-          )
-    }
+  function isListenerOpen () {
+    return (
+        (window.location.pathname == '/rover' &&
+            $('#toggle-rover-listener-btn')[0].checked == true) ||
+        (window.location.pathname == '/' &&
+            $('#toggle-arm-listener-btn')[0].checked == true) ||
+        (window.location.pathname == '/science' &&
+            $('#science-listener-btn')[0].checked == true)
+        // pds Listener
+    )
+  }
 
   // select mux channel using mux_select service
   $("a[id^='mux-']").mouseup(e => {
@@ -125,39 +125,39 @@ $(document).ready(() => {
   $('#send-serial-btn').mouseup(function() {
     // b
     let cmd = $('#serial-cmd-input').val()
-      let buttonText = $('button#mux').text()
-      if (buttonText.includes('Select Device Channel')) {
-        appendToConsole(
-            'Unable to send serial command. Try opening a mux channel.'
-            )
-      } else {
-        // if the appropriate listener is open, send a command to it
-        if (
-            buttonText.includes('Rover') &&
-            $('#toggle-rover-listener-btn')[0].checked == true
-           ) {
-          // sendRoverCommand(cmd) // rover commands not yet implemented
-        } else if (
-            buttonText.includes('Arm') &&
-            $('#toggle-arm-listener-btn')[0].checked == true
-            ) {
-          sendArmCommand(cmd)
-        } else if (buttonText.includes('Science')) {
-          // science buttons unknown
-          // sendScienceCommand(cmd) // science commands not yet implemented
-        } else if (buttonText.includes('PDS')) {
-          // pds buttons unknown
-          // sendPdsCommand(cmd) // pds commands not yet implemented
-        }
-        // no listener is open, send generic request
-        else if (!buttonText.includes('Select Device Channel')) {
-          requestSerialCommand(cmd, function(msgs) {
-            console.log(msgs)
-              if (msgs[0]) {
-                console.log('nice')
-              } else { console.log('not nice') } })
-        }
+    let buttonText = $('button#mux').text()
+    if (buttonText.includes('Select Device Channel')) {
+      appendToConsole(
+          'Unable to send serial command. Try opening a mux channel.'
+      )
+    } else {
+      // if the appropriate listener is open, send a command to it
+      if (
+          buttonText.includes('Rover') &&
+          $('#toggle-rover-listener-btn')[0].checked == true
+      ) {
+        // sendRoverCommand(cmd) // rover commands not yet implemented
+      } else if (
+          buttonText.includes('Arm') &&
+          $('#toggle-arm-listener-btn')[0].checked == true
+      ) {
+        sendArmCommand(cmd)
+      } else if (buttonText.includes('Science')) {
+        // science buttons unknown
+        // sendScienceCommand(cmd) // science commands not yet implemented
+      } else if (buttonText.includes('PDS')) {
+        // pds buttons unknown
+        // sendPdsCommand(cmd) // pds commands not yet implemented
       }
+      // no listener is open, send generic request
+      else if (!buttonText.includes('Select Device Channel')) {
+        requestSerialCommand(cmd, function(msgs) {
+          console.log(msgs)
+          if (msgs[0]) {
+            console.log('nice')
+          } else { console.log('not nice') } })
+      }
+    }
   })
 
   $('#serial-cmd-input').on('keyup', function(e) {
@@ -196,6 +196,6 @@ $(document).ready(() => {
 */
 function navModalMessage (title, body){
   $('.modal-title').text(title)
-    $('.modal-msg').text(body)
-    $('.modal').modal({show: true})
+  $('.modal-msg').text(body)
+  $('.modal').modal({show: true})
 }
