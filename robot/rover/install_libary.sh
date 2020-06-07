3f61a4087ad1a7d84cd72f922b077725a308fb36
steamypotato <hoopahydra@gmail.com>
2020-06-06 21:10:51 -0400

    [#362] Shell script for installing a library

---
+++ robot/rover/install_libary.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+lib_name=$1
+lib_version=$2
+lib_url=$3
+install_path=~/Arduino/libraries
+curl -fsSlL  $lib_url/archive/$lib_version.tar.gz > $lib_name.tar.gz
+tar -xf $lib_name.tar.gz -C $install_path
+mv $install_path/${lib_name}-library-$lib_version $install_path/${lib_name}
