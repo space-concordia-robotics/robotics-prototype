4f66ee0672d37705ff22b4b1eca2917b807ac928
badgerwild <jklimock@gmail.com>
2020-06-07 15:03:19 -0400

    [#192] Fix leftover code as per PR
    remove files that are no longer needed, add config.txt template file, correct errors related to incorrect file path for image files.

--- robot/rospackages/src/image_scp/config.py
+++
@@ -1,6 +0,0 @@
-#enter host computer address
-host = "HOST_URL"
-#enter user name for host computer
-user = "USER NAME FOR HOST"
-#enter desired image path on host computer
-remote_path_1 = "PATH FOR IMAGE ON HOST COMPUTER"
--- robot/rospackages/src/image_scp/congif.py
+++
@@ -1,6 +0,0 @@
-from os import environ
-
-host = environ.get('login.encs.concordia.ca')
-user = environ.get('j_klimo')
-remote_path_1 = environ.get('/home/j/j_klimo/Documents/images')
-local = ('/home/jason/images/test1.png')
