12977bcc665858fd140d8bb9df7d9cef3a5e990d
steamypotato <hoopahydra@gmail.com>
2020-06-08 01:28:08 -0400

    [#362] Install all libraries using bash script

--- robot/rover/test.sh
+++ robot/rover/test.sh
@@ -6,4 +6,6 @@ lib_url=$3
 install_path=~/Arduino/libraries
 curl -fsSlL  $lib_url/archive/$lib_version.tar.gz > $lib_name.tar.gz
 tar -xf $lib_name.tar.gz -C $install_path
-mv $install_path/${lib_name}-library-$lib_version $install_path/${lib_name}
+mv $install_path/${lib_name}-library-$lib_version $install_path/${lib_name}sh
+
+echo "Installed library ${lib_name} ${lib_url} at $install_path"
