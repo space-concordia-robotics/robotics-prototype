33806dcf3938a2932afee2040a356bd27bb332e9
PeterGhimself <petergranitski@gmail.com>
2020-06-14 00:56:26 -0400

    [#402] Improve safety checks, refactor
    Result of more thorough testing of cases.

--- install_arduino_teensyduino.sh
+++ install_arduino_teensyduino.sh
@@ -1,37 +1,54 @@
 #/usr/bin/env bash
 
-# NOTE: if you stop the process in the middle of the downloads, make sure to delete the newly downloaded files before re-running this script
+# NOTE: Running this script in sudo will cause the checks for environment vars to fail
+# To complete the installation run `sudo install.sh` in the arduino IDE folder
 
 ARDUINO_IDE_VERSION="1.8.12"
 TEENSYDUINO_VERSION="152"
 ARDUINO_IDE_FOLDER="$HOME/arduino-$ARDUINO_IDE_VERSION"
+ARDUINO_DOWNLOAD_URL="https://downloads.arduino.cc/arduino-$ARDUINO_IDE_VERSION-linux64.tar.xz"
+TEENSYDUINO_DOWNLOAD_URL="https://www.pjrc.com/teensy/td_$TEENSYDUINO_VERSION/TeensyduinoInstall.linux64"
 
-# check if arduino path set, will
-if [ -z ${ARDUINO_PATH+x} ]; then
+count=`ls -1 ./arduino-$ARDUINO_IDE_VERSION-* 2>/dev/null | wc -l`   
+
+if [ $count != 0 ]; then
+    echo "deleting left overs..."
+    rm "arduino-$ARDUINO_IDE_VERSION-"*
+fi
+
+# check if arduino path set
+if [ -z ${ARDUINO_PATH} ]; then
     echo "Environment variable ARDUINO_PATH is unset"
     echo "Setting to $ARDUINO_IDE_FOLDER"
     echo "export ARDUINO_PATH="$ARDUINO_IDE_FOLDER"" >> ~/.bashrc
-    ARDUINO_PATH="$HOME/arduino-$ARDUINO_IDE_VERSION"
+    ARDUINO_PATH="$ARDUINO_IDE_FOLDER"
 else
     echo "ARDUINO_PATH is already set to '$ARDUINO_PATH'"
-    
-    # check if safe to proceed with arduino IDE installation
-    if [ "$ARDUINO_PATH" == "$ARDUINO_IDE_FOLDER" ] && [ ! -z "$(ls -A $ARDUINO_PATH)" ]; then
-        echo "Skipping arduino IDE installation step"
-    else
-        echo "Installing arduino IDE"
-        mkdir -p ~/Arduino/libraries &&
-        wget https://downloads.arduino.cc/arduino-$ARDUINO_IDE_VERSION-linux64.tar.xz &&
-        tar xf arduino-$ARDUINO_IDE_VERSION-linux64.tar.xz -C $HOME &&
-        bash "$HOME/arduino-$ARDUINO_IDE_VERSION/install.sh"
-    fi
+fi
+
+count=`ls -1 $ARDUINO_IDE_FOLDER/arduino* 2>/dev/null | wc -l`   
+ 
+# check if arduino IDE already installed in target folder
+if [ "$ARDUINO_PATH" == "$ARDUINO_IDE_FOLDER" ] && [ $count != 0 ]; then
+    echo "Skipping arduino IDE installation step"
+else
+    echo "Installing arduino IDE"
+    mkdir -p ~/Arduino/libraries &&
+    wget $ARDUINO_DOWNLOAD_URL &&
+    tar xf arduino-$ARDUINO_IDE_VERSION-linux64.tar.xz -C $HOME &&
+        
+    # the following line requires sudo rights to complete the symlink
+    # but everything else will work
+    bash "$HOME/arduino-$ARDUINO_IDE_VERSION/install.sh"
+        
+    # clean up
+    rm arduino-$ARDUINO_IDE_VERSION-linux64.tar.xz
 fi
 
 echo "Installing Teensyduino"
-curl -fSL https://www.pjrc.com/teensy/td_$TEENSYDUINO_VERSION/TeensyduinoInstall.linux64 -o TeensyduinoInstall.linux64 &&
+curl -fSL $TEENSYDUINO_DOWNLOAD_URL -o TeensyduinoInstall.linux64 &&
 chmod +x TeensyduinoInstall.linux64 &&
 ./TeensyduinoInstall.linux64 --dir=$ARDUINO_PATH
 
 # clean up
-rm arduino-$ARDUINO_IDE_VERSION-linux64.tar.xz
 rm TeensyduinoInstall.linux64
