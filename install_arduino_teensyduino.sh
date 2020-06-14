f2dd39fee0802909f98cb36be3c1103927faa1c6
PeterGhimself <petergranitski@gmail.com>
2020-06-13 21:06:42 -0400

    [#402] Add cleanup, remove cds

--- install_arduino_teensyduino.sh
+++ install_arduino_teensyduino.sh
@@ -5,12 +5,15 @@
 export ARDUINO_IDE_VERSION="1.8.12"
 export ARDUINO_PATH="$HOME/arduino-$ARDUINO_IDE_VERSION"
 export TEENSYDUINO_VERSION="152"
+
 mkdir -p ~/Arduino/libraries &&
-cd "$HOME/Downloads" &&
 wget https://downloads.arduino.cc/arduino-$ARDUINO_IDE_VERSION-linux64.tar.xz &&
 tar xf arduino-$ARDUINO_IDE_VERSION-linux64.tar.xz -C /home/$USER/ &&
 bash "$HOME/arduino-$ARDUINO_IDE_VERSION/install.sh"
 curl -fSL https://www.pjrc.com/teensy/td_$TEENSYDUINO_VERSION/TeensyduinoInstall.linux64 -o TeensyduinoInstall.linux64 &&
 chmod +x TeensyduinoInstall.linux64 &&
 ./TeensyduinoInstall.linux64 --dir=$ARDUINO_PATH &&
-cd -
+
+# clean up
+rm arduino-$ARDUINO_IDE_VERSION-linux64.tar.xz
+rm TeensyduinoInstall.linux64
