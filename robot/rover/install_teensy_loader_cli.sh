11274d229f64086986207e5f60af5450bea4c286
steamypotato <hoopahydra@gmail.com>
2020-06-26 17:30:43 -0400

    [#382] Refactor

--- robot/rover/install_teensy_loader_cli.sh
+++ robot/rover/install_teensy_loader_cli.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
+
 if [ -f "/etc/udev/rules.d/49-teensy.rules" ]; then
-	echo "Permissions file already exists"
+	echo "teensy rules already installed"
 else
-	echo "Downloading permissions..."
-	wget -P /etc/udev/rules.d/ "https://www.pjrc.com/teensy/49-teensy.rules" 
+	echo "Downloading teensy rules..."
+	sudo wget -P /etc/udev/rules.d/ "https://www.pjrc.com/teensy/49-teensy.rules" 
 fi 
 
+	sudo apt install --no-upgrade libusb-dev
 
-if  dpkg --get-selections | grep -q "^$pkg[[:space:]]*libusb-dev$" >/dev/null; then
-	echo "Installing libsub-dev"
-	apt-get install libusb-dev;
+if [ -f "${ARDUINO_PATH}"/hardware/tools/teensy_loader_cli ]; then
+	echo "Teensy Loader CLI already installed"
 else
-	echo "Libusb-dev already installed"
-fi
-
-if [ -e "${ARDUINO_PATH}"/hardware/tools/teensy_loader_cli ]; then
-	echo "Teensy loader CLI already installed"
-else
-	echo "Installing Teensy Loader CLI" 
+	echo "Installing Teensy Loader CLI..." 
 	curl https://github.com/PaulStoffregen/teensy_loader_cli/archive/master.zip -L -o teensy_loader_cli.zip
 	unzip -q teensy_loader_cli.zip 
 	make -s -C teensy_loader_cli-master
 	mv teensy_loader_cli-master/teensy_loader_cli "${ARDUINO_PATH}"/hardware/tools
 	rm -rf teensy_loader_cli.zip teensy_loader_cli-master
-
 fi 
 
 
