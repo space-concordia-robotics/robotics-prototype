cc2101be29439dff33f0150d423d5c21aa9d0065
badgerwild <jklimock@gmail.com>
2020-07-30 21:37:15 -0400

    [#192] Add function to handle automation of RSA key if not present

--- robot/basestation/image_scp/image_scp.py
+++ robot/basestation/image_scp/image_scp.py
@@ -9,16 +9,43 @@ The ssh connection depends on the user having an rsa key pair between the base s
 and the rover.
 """
 import os
-from paramiko import SSHClient
+import os.path
+from os import path
+import subprocess
+from subprocess import Popen, PIPE
+from paramiko import SSHClient, RSAKey
+from paramiko.auth_handler import SSHException
 from scp import SCPClient, SCPException
 import transferconfig as cfg
 
 class RemoteConnection:
 
-    def __init__(self, rover_ip, user, rover_file_path):
+    def __init__(self, rover_ip, user, rover_file_path, key_path):
         self.rover_ip = rover_ip
         self.user = user
         self.rover_file_path = rover_file_path
+        self.key_path = key_path
+        self.ssh_key = None
+
+    def generate_rsa_key(self):
+        """generates a defauls rsa key in the .ssh folder. Will push rsa key to the rover.
+        Will only run if a key is not found with name specified in transferconfig.py. Default name
+        is id_rsa
+        alter name in transferconfig.py if you want to set up a discrete key for this usage."""
+        key_name = "ToDo"
+        subprocess.run(f'ssh-keygen -f {self.key_path} -t rsa', shell=True)
+
+    def check_ssh_keys(self):
+        """checks if system has an ssh key for the server"""
+        path = os.path.expanduser(self.key_path)
+        #print(path)
+        try:
+            RSAKey.from_private_key_file(path, password='Mackie66')
+            print("key found")
+        except (FileNotFoundError, SSHException):
+            print('key is not found')
+            self.generate_rsa_key()
+
     def connect(self):
         """initiate the connection to the host machine"""
         self.client = SSHClient()
@@ -39,10 +66,19 @@ class RemoteConnection:
             print(error + "Directory was not found")
 
 
+
+
+
 if __name__ == "__main__":
 
     image_retriver = RemoteConnection(cfg.user['rover_ip'], cfg.user['username'],
-                                      cfg.user['rover_image_dir'])
-    image_retriver.connect()
-    image_retriver.get_images(cfg.user['basestation_dir'])
-    image_retriver.disconnect()
+                                      cfg.user['rover_image_dir'], cfg.user['key_path'])
+    beans = image_retriver.check_ssh_keys()
+   # if not beans:
+    #    image_retriver.generate_rsa_key()
+     #   print("all done")
+   # else:
+    #    print("failed")
+    #image_retriver.connect()
+    #image_retriver.get_images(cfg.user['basestation_dir'])
+    #image_retriver.disconnect()
