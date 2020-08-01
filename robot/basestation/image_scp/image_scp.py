d2b69bda1ae6295f7766a4eb97ad288570e1f7ed
badgerwild <jklimock@gmail.com>
2020-08-01 14:31:50 -0400

    [#192] Add functionality to push rsa key to rover
    add time stamp to image destination and password key to config file.

--- robot/basestation/image_scp/image_scp.py
+++ robot/basestation/image_scp/image_scp.py
@@ -5,53 +5,58 @@ a directory back to the base station. A new directory is created on the base sta
 User details, rover ip, image directory, and destination directory are all updated via
 a dictonary in transferconfig.py
 
-The ssh connection depends on the user having an rsa key pair between the base station
-and the rover.
 """
 import os
 import os.path
 from os import path
 import subprocess
 from subprocess import Popen, PIPE
-from paramiko import SSHClient, RSAKey
-from paramiko.auth_handler import SSHException
+from paramiko import SSHClient, AutoAddPolicy, RSAKey
+from paramiko.auth_handler import AuthenticationException, SSHException
 from scp import SCPClient, SCPException
 import transferconfig as cfg
 
 class RemoteConnection:
 
-    def __init__(self, rover_ip, user, rover_file_path, key_path):
+    def __init__(self, rover_ip, user, rover_file_path, key_path, rsa_password):
         self.rover_ip = rover_ip
         self.user = user
         self.rover_file_path = rover_file_path
+        self.rsa_password = rsa_password
         self.key_path = key_path
         self.ssh_key = None
-
-    def generate_rsa_key(self):
-        """generates a defauls rsa key in the .ssh folder. Will push rsa key to the rover.
-        Will only run if a key is not found with name specified in transferconfig.py. Default name
-        is id_rsa
-        alter name in transferconfig.py if you want to set up a discrete key for this usage."""
-        key_name = "ToDo"
-        subprocess.run(f'ssh-keygen -f {self.key_path} -t rsa', shell=True)
+        self.scp = None
 
     def check_ssh_keys(self):
-        """checks if system has an ssh key for the server"""
+        """checks if system has an RSA key for the server. if key is not found
+        will generate a new key at the location specified in the config file"""
         path = os.path.expanduser(self.key_path)
-        #print(path)
         try:
-            RSAKey.from_private_key_file(path, password='Mackie66')
+            RSAKey.from_private_key_file(path, password=self.rsa_password)
             print("key found")
-        except (FileNotFoundError, SSHException):
+        except FileNotFoundError:
             print('key is not found')
-            self.generate_rsa_key()
+            subprocess.run(f'ssh-keygen -f {self.key_path} -t rsa', shell=True)
+            print(f'RSA key generated at {self.key_path}')
+        except SSHException:
+            print('There is a problem with the key, double check that your encription'+
+                  ' password is correct')
 
     def connect(self):
-        """initiate the connection to the host machine"""
-        self.client = SSHClient()
-        self.client.load_system_host_keys()
-        self.client.connect(hostname=self.rover_ip, username=self.user)
-        self.scp = SCPClient(self.client.get_transport())
+        """initiate the connection to the host machine. If the rsa key has not been
+        pushed to rover, function will handle it"""
+        try:
+            self.client = SSHClient()
+            self.client.load_system_host_keys()
+            self.client.set_missing_host_key_policy(AutoAddPolicy())
+            self.client.connect(hostname=self.rover_ip, username=self.user,
+                                password=self.rsa_password)
+            self.scp = SCPClient(self.client.get_transport())
+            print(f'sucessfully connected to {self.rover_file_path}')
+        except SSHException:
+            subprocess.run(f'ssh-copy-id -i {self.key_path} {self.user}@{self.rover_ip}',
+                           shell=True)
+            subprocess.run('ssh-add')
 
     def disconnect(self):
         self.client.close()
@@ -66,19 +71,12 @@ class RemoteConnection:
             print(error + "Directory was not found")
 
 
-
-
-
 if __name__ == "__main__":
 
     image_retriver = RemoteConnection(cfg.user['rover_ip'], cfg.user['username'],
-                                      cfg.user['rover_image_dir'], cfg.user['key_path'])
+                                      cfg.user['rover_image_dir'], cfg.user['key_path'],
+                                      cfg.user['rsa_password'])
     beans = image_retriver.check_ssh_keys()
-   # if not beans:
-    #    image_retriver.generate_rsa_key()
-     #   print("all done")
-   # else:
-    #    print("failed")
-    #image_retriver.connect()
-    #image_retriver.get_images(cfg.user['basestation_dir'])
-    #image_retriver.disconnect()
+    image_retriver.connect()
+    image_retriver.get_images(cfg.user['basestation_dir'])
+    image_retriver.disconnect()
--- robot/basestation/image_scp/transferconfig.py
+++ robot/basestation/image_scp/transferconfig.py
@@ -2,17 +2,31 @@
 """
 Update user credentials here for username to access the rover, the rover ip address,
 diectory where the images are located on the rover, and the destination directory on the base
-station. basestation_dir will make a new directory with the name entered here and copy the
-contents from the rover image directory
+station.
 
-Note that a rsa key pair must be set up between the rover and basestation for this script to
-work
+Basestation_dir will make a new directory with the name entered here and copy the
+contents from the rover_image_dir.
 
-Details listed here will not be ignored by git
+key_path is the path to your rsa key, with the default name of id_rsa. If it does not exist
+a new key will be created in this location.
+
+rsa_ password is the password used to encrypt your rsa key. If your key is not encrypted,
+then leave None.
+
+Changes made here will not be ignored by git so do not push any senstive information.
+
+
 
 """
-user = {'rover_ip':'',
-        'username': '',
-        'rover_image_dir': '',
-        'basestation_dir':''
+
+import getpass
+import datetime
+
+user = {'rover_ip':'ip',
+        'username': 'username',
+        'rover_image_dir': 'rover_dir',
+        'basestation_dir':'basestation_dir'+str(datetime.datetime.now()
+                                                .strftime("_%Y-%m-%d_%H:%M:%S")),
+        'key_path': '/home/'+ getpass.getuser()+'/.ssh/id_rsa',
+        'rsa_password':'None'
         }
