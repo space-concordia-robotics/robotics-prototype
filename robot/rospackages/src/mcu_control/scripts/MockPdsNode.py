a6fe10fed33a30b3f4573193908a4be59553a777
Michael Lesko-Krleza <michael.lk@spaceconcordia.ca>
2020-08-10 17:09:02 -0400

    [#284] [#284] Add temperature and voltage parameter server functions

--- robot/rospackages/src/mcu_control/scripts/MockPdsNode.py
+++ robot/rospackages/src/mcu_control/scripts/MockPdsNode.py
@@ -5,8 +5,7 @@ import rospy
 import sys
 from std_msgs.msg import Float32
 from geometry_msgs.msg import Point
-from mcu_control.msg import ThermistorTemps as ThermistorTemps
-from mcu_control.msg import Voltage as Voltage
+from mcu_control.msg import ThermistorTemps, Voltage
 from decimal import *
 
 
@@ -26,6 +25,8 @@ class TemperatureMonitor(object):
         self._temps = temps
 
 
+#TODO: Use only ONE get parameter method to get ALL values (scalable)
+#TODO: Take parameterState and values[] instead of voltages[] and temps[]
 def get_voltage_parameter(parameterState, voltages):
     try:
         param = rospy.get_param(parameterState)
@@ -35,10 +36,13 @@ def get_voltage_parameter(parameterState, voltages):
 
     currentGetVoltage = 0
     if (param == "high"):
+        target = voltages[0][1]
         currentGetVoltage = voltages[0][1]
     elif (param == "normal"):
+        target = voltages[1][1]
         currentGetVoltage = voltages[1][1]
     elif (param == "low"):
+        target = voltages[2][1]
         currentGetVoltage = voltages[2][1]
     else:
         rospy.loginfo("Failed: Invalid parameter entered")
@@ -67,90 +71,103 @@ def get_temp_parameter(parameterState, temps):
 
 
 #def publish_mock_data(voltages, temps):
-def publish_mock_data():
+def publish_mock_data(voltages, temps):
 
     #Initialize node
     node_name = "mock_pds_node"
     rospy.init_node(node_name, anonymous=True)
     rospy.loginfo("Initialized: {} node for mock PDS values".format(node_name))
     rate = rospy.Rate(10)
-    pubVoltageTest = rospy.Publisher('battery_voltage', Float32, queue_size=10)
-    pubTempTest = rospy.Publisher('battery_temps', Point, queue_size=10)
+    voltage = Voltage()
+    thermistorTemps = ThermistorTemps()
+    pubVoltage = rospy.Publisher('battery_voltage', Voltage, queue_size=10)
+    pubTemp = rospy.Publisher('battery_temps', ThermistorTemps, queue_size=10)
 
-    #Set voltages high, normal and low to be parametrized
-    voltages = []
-    voltages.append(("high", 20))
-    voltages.append(("normal", 15))
-    voltages.append(("low", 10))
-    #Set temperatures high, normal and low to be parametrized
-    temps = []
-    temps.append(("high", 100))
-    temps.append(("normal", 50))
-    temps.append(("low", -20))
+    # pubVoltageTest = rospy.Publisher('battery_voltage', Float32, queue_size=10)
+    # pubTempTest = rospy.Publisher('battery_temps', Point, queue_size=10)
 
+    #Get previous state before changing parameter
+    prevVoltageState = rospy.get_param("PDS_mock_voltage")
+    prevTemp1State = rospy.get_param("PDS_mock_temp1")
+    prevTemp2State = rospy.get_param("PDS_mock_temp2")
+    prevTemp3State = rospy.get_param("PDS_mock_temp3")
+
+    #Initialize variables for previous voltage and temp
+    prevVoltageValue = None
+    prevTempStates = []
+    prevTempValues = []
+    prevTempStates.append(("PDS_mock_temp1", prevTemp1State))
+    prevTempStates.append(("PDS_mock_temp2", prevTemp2State))
+    prevTempStates.append(("PDS_mock_temp3", prevTemp3State))
+
+    #Set voltage value for particular parameter
+    if (prevVoltageState == "high"):
+        prevVoltageValue = ("PDS_mock_voltage", voltages[0][1])
+    elif (prevVoltageState == "normal"):
+        prevVoltageValue = ("PDS_mock_voltage", voltages[1][1])
+    elif (prevVoltageState == "low"):
+        prevVoltageValue = ("PDS_mock_voltage", voltages[2][1])
+    else:
+        rospy.loginfo("Failed: Invalid parameter entered")
+
+    #Set temperature values for particular parameters
+    for x in range(len(prevTempStates)):
+        if (prevTempStates[x][1] == "high"):
+            prevTempValues.append((prevTempStates[x][0], temps[0][1]))
+        elif (prevTempStates[x][1] == "normal"):
+            prevTempValues.append((prevTempStates[x][0], temps[1][1]))
+        elif (prevTempStates[x][1] == "low"):
+            prevTempValues.append((prevTempStates[x][0], temps[2][1]))
+        else:
+            rospy.loginfo("Failed: Invalid parameter entered")
+
+    rospy.loginfo("Thermistor temp values size: " + str(len(prevTempStates)))
+
+    rospy.loginfo("PDS_mock_voltage : " + str(prevVoltageValue))
+    rospy.loginfo("PDS_mock_temp1 : " + str(prevTempValues[0]))
+    rospy.loginfo("PDS_mock_temp2 : " + str(prevTempValues[1]))
+    rospy.loginfo("PDS_mock_temp3 : " + str(prevTempValues[2]))
+
+    #Acquire parameter settings
     while not rospy.is_shutdown():
         currentVoltage = get_voltage_parameter("PDS_mock_voltage", voltages)
         currentTemp1 = get_temp_parameter("PDS_mock_temp1", temps)
         currentTemp2 = get_temp_parameter("PDS_mock_temp2", temps)
         currentTemp3 = get_temp_parameter("PDS_mock_temp3", temps)
 
-        pubVoltageTest.publish(currentVoltage)
-        pubTempTest.publish(Point(currentTemp1, currentTemp2, currentTemp3))
+        #Set data to voltage and thermistortemp types
+        voltage.data = float(currentVoltage)
+        thermistorTemps.therm1 = float(currentTemp1)
+        thermistorTemps.therm2 = float(currentTemp2)
+        thermistorTemps.therm3 = float(currentTemp3)
+
+        #Publish voltage and thermistortemps
+        pubVoltage.publish(voltage)
+        pubTemp.publish(thermistorTemps)
 
         rate.sleep()
 
 
-# def main():
-
-#     #Initialize node
-#     node_name = "mock_pds_node"
-#     rospy.init_node(node_name, anonymous=True)
-#     rospy.loginfo("Initialized: {} node for mock PDS values".format(node_name))
-#     rate = rospy.Rate(10)
-
-#     pubVoltageTest = rospy.Publisher('battery_voltage', Float32, queue_size=10)
-#     pubTempTest = rospy.Publisher('battery_temps', Float32, queue_size=10)
-
-#     #pubVoltage = rospy.Publisher('battery_voltage', Voltage, queue_size=10)
-#     #pubTemp = rospy.Publisher('battery_temps', ThermistorTemps, queue_size=10)
-
-#     #Set voltages high, normal and low to be parametrized
-#     voltages = []
-#     voltages.append(("high", 20))
-#     voltages.append(("normal", 15))
-#     voltages.append(("low", 10))
-
-#     #Set temperatures high, normal and low to be parametrized
-#     temps = []
-#     temps.append(("high", 100))
-#     temps.append(("normal", 50))
-#     temps.append(("low", -20))
-
-#     #Create objects to publish voltage and temperature
-#     voltageMonitor = VoltageMonitor(pubVoltage, voltages)
-#     tempMonitor = TemperatureMonitor(pubTemp, temps)
-
-#     try:
-#         while not rospy.is_shutdown():
-#             publish_mock_pds_data(voltages, temps)
-#             rate.sleep()
-
-#     except rospy.ROSInterruptException:
-#         pass
-
-#     #Publish values after computing
-#     #pubVoltage.publish(voltages[0])
-#     #pubTemp.publish(temps[0], temps[1], temps[2])
-
-#     #rospy.spin()
-
 if __name__ == '__main__':
+
+    #Set voltages high, normal and low to be parametrized
+    voltages = []
+    voltages.append(("high", 20))
+    voltages.append(("normal", 15))
+    voltages.append(("low", 10))
+
+    #Set temperatures high, normal and low to be parametrized
+    temps = []
+    temps.append(("high", 100))
+    temps.append(("normal", 50))
+    temps.append(("low", -20))
+
     try:
         #Initialize parameters
-        rospy.set_param("PDS_mock_voltage", "high")
-        rospy.set_param("PDS_mock_temp1", "normal")
-        rospy.set_param("PDS_mock_temp2", "normal")
-        rospy.set_param("PDS_mock_temp3", "normal")
+        rospy.set_param("PDS_mock_voltage", voltages[1][0])
+        rospy.set_param("PDS_mock_temp1", temps[1][0])
+        rospy.set_param("PDS_mock_temp2", temps[1][0])
+        rospy.set_param("PDS_mock_temp3", temps[1][0])
         rospy.loginfo("Success: Values published")
     except:
         rospy.loginfo("Fail: Values not published")
@@ -160,43 +177,8 @@ if __name__ == '__main__':
     rospy.loginfo(rospy.get_param("PDS_mock_temp2"))
     rospy.loginfo(rospy.get_param("PDS_mock_temp3"))
 
-    # #Initialize node
-    # node_name = "mock_pds_node"
-    # rospy.init_node(node_name, anonymous=True)
-    # rospy.loginfo("Initialized: {} node for mock PDS values".format(node_name))
-    # rate = rospy.Rate(10)
-    # pubVoltageTest = rospy.Publisher('battery_voltage', Float32, queue_size=10)
-    # pubTempTest = rospy.Publisher('battery_temps', Point, queue_size=10)
-
-    #pubVoltage = rospy.Publisher('battery_voltage', Voltage, queue_size=10)
-    #pubTemp = rospy.Publisher('battery_temps', ThermistorTemps, queue_size=10)
-
-    # #Set voltages high, normal and low to be parametrized
-    # voltages = []
-    # voltages.append(("high", 20))
-    # voltages.append(("normal", 15))
-    # voltages.append(("low", 10))
-
-    ## Set temperatures high, normal and low to be parametrized
-    # temps = []
-    # temps.append(("high", 100))
-    # temps.append(("normal", 50))
-    # temps.append(("low", -20))
-
-    #Create objects to publish voltage and temperature
-    #voltageMonitor = VoltageMonitor(pubVoltage, voltages)
-    #tempMonitor = TemperatureMonitor(pubTemp, temps)
-
-    #voltageMonitor = VoltageMonitor(pubVoltageTest, voltages)
-    #tempMonitor = TemperatureMonitor(pubTempTest, temps)
-
+    #Publish all new parameter settings set by user
     try:
-        publish_mock_data()
-
-        # while not rospy.is_shutdown():
-        #     pubVoltageTest.publish(voltages[2][1])
-        #     pubTempTest.publish(Point(temps[2][1], temps[2][1], temps[2][1]))
-        #     #publish_mock_pds_data(voltages, temps)
-
+        publish_mock_data(voltages, temps)
     except rospy.ROSInterruptException:
         pass
