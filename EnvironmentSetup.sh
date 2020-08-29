420cc090a032fad6497aef4a5e584b33835ffecc
PeterGhimself <petergranitski@gmail.com>
2020-08-29 13:04:59 -0400

    [#422] Minor refactor, add util shortcut alias

--- EnvironmentSetup.sh
+++ EnvironmentSetup.sh
@@ -4,12 +4,15 @@
 APPEND_TO_BASH="
 
 #------ ROBOTICS SETTINGS ------
-#competition mode
+# competition mode
 #export ROS_MASTER_URI=http://172.16.1.30:11311
 #export ROS_HOSTNAME=$USER
+# local mode
+export ROS_MASTER_URI=http://localhost:11311
+export ROS_HOSTNAME=localhost
 
-. ~/Programming/robotics-prototype/robot/rospackages/devel/setup.bash
-. ~/Programming/robotics-prototype/venv/bin/activate
+source ~/Programming/robotics-prototype/robot/rospackages/devel/setup.bash
+source ~/Programming/robotics-prototype/venv/bin/activate
 source ~/Programming/robotics-prototype/robot/basestation/config/.bash_aliases
 
 #------ POST ROS INSTALLATION ------
@@ -19,8 +22,6 @@ alias gs='git status'
 alias gp='git pull'
 alias cw='cd ~/Programming/robotics-prototype/robot/rospackages/'
 alias cm='cw && catkin_make'
-export ROS_MASTER_URI=http://localhost:11311
-export ROS_HOSTNAME=localhost
 "
 
 APPEND_TO_BASH_ALIASES='
@@ -40,7 +41,8 @@ alias rover="cd $ROVER"
 alias arm="cd $ROVER/ArmDriverUnit"
 alias wheels="cd $ROVER/MobilePlatform"
 alias rostings="cd $ROSPACKAGES"
-alias mcunode="cd $ROSPACKAGES/src/mcu_control/scripts"'
+alias mcu="cd $ROSPACKAGES/src/mcu_control/scripts"
+alias util="cd $ROBOTICS_WS/robot/util"'
 
 FINAL_MESSAGE="
 
