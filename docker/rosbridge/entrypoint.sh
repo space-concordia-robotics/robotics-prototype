55f57d9d6ccfdc7ceee21ceaad61c4fc64e7ada7
Mohamed Lemine Ould El-Hadj <mohamedlemineelhadj@outlook.com>
2020-10-08 20:08:32 -0400

    [#418] Add rospackages to the rosbridge container

--- docker/rosbridge/entrypoint.sh
+++ docker/rosbridge/entrypoint.sh
@@ -3,4 +3,6 @@ set -e
 
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
+source "/home/spaceuser/Programming/robotics-prototype/robot/rospackages/devel/setup.bash"
+
 roslaunch --wait rosbridge_server rosbridge_websocket.launch
