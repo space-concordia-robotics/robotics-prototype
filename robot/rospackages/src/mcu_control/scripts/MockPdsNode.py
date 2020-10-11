5b4a8cc0b5038d8987c090ce7eec1f0cef12ee1e
Michael Lesko-Krleza <michaelles97@gmail.com>
2020-10-11 02:45:23 -0400

    [#284] Fix docstring

--- robot/rospackages/src/mcu_control/scripts/MockPdsNode.py
+++ robot/rospackages/src/mcu_control/scripts/MockPdsNode.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python3
 
-"""Simulator used to acquire and set states initialized
-either manually by user in CLI or through .launch file"""
+"""Simulator used to acquire and set parameters to
+user defined states, which are initialized either
+manually by the user in CLI or through .launch file"""
 
 import time
 import sys
