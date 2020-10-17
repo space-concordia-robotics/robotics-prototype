3387f275e18eda2f406ae11dda98104e68c576da
Cedric Martens <cedricmartens98@gmail.com>
2020-10-17 11:49:08 -0400

    [#462] Add parsing commands to commandcenter

--- robot/rover/science/include/CommandCenter.h
+++ robot/rover/science/include/CommandCenter.h
@@ -7,7 +7,23 @@
 
 
 class CommandCenter {
+    public:
 
+        /**
+         * Executes the command with the name command with the arguments args
+         * @param commandName The name of the command to execute
+         * @param args The array of arguments as const char*
+         */
+        virtual void executeCommand(const char* commandName, const char** args);
+
+        /**
+         * Takes as an input a rawCommand in the form commandName=arg1,arg2,arg3,...
+         * Parses the command and returns by reference commandName and a const char* array of arguments
+         *
+         * @param rawCommand
+         * @return returns true if the command is valid or not
+         */
+        virtual bool processCommand(const char* rawCommand, char* commandName, char** arguments) const;
 };
 
 
