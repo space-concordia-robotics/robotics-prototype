e8178b1404b18b2b544187660a793f57b4769a29
Mohamed Lemine Ould El-Hadj <mohamedlemineelhadj@outlook.com>
2020-10-22 13:47:08 -0400

    [#418] Start local_rover.launch in the background

--- docker/rosbridge/entrypoint.sh
+++ docker/rosbridge/entrypoint.sh
@@ -5,4 +5,7 @@ set -e
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/home/spaceuser/Programming/robotics-prototype/robot/rospackages/devel/setup.bash"
 
+# Not recommended but doable. See: https://docs.docker.com/config/containers/multi-service_container/
+roslaunch --wait robot/util/rosRoverStart/local_rover.launch & 
+
 roslaunch --wait rosbridge_server rosbridge_websocket.launch
