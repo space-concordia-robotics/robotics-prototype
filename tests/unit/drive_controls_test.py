a6c0bb6f6f0f65300c3f3a479c118ef53a1a5a58
Cedric Martens <cedricmartens98@gmail.com>
2020-10-31 14:28:43 -0400

    [#436] Add more unit tests

--- tests/unit/drive_controls_test.py
+++ tests/unit/drive_controls_test.py
@@ -1,4 +1,4 @@
-from robot.rospackages.src.mcu_control.scripts.RoverNode import twist_to_rover_command
+from robot.rospackages.src.mcu_control.scripts.RoverNode import twist_to_rover_command, accelerate_value
 import robot.rospackages.src.mcu_control.scripts.RoverNode as rn
 
 def test_twist_to_rover():
@@ -18,3 +18,12 @@ def test_twist_to_rover():
     half_max_negative =  twist_to_rover_command(-rn.max_speed * 0.5, -rn.max_angular_speed * 0.5)
 
     assert(half_max_negative == str(round(rn.max_throttle * -0.5)) + ":" + str(round(rn.max_steering * -0.5)))
+
+def test_acceleration():
+    assert(accelerate_value(0.3, 0.5, 0.1, 0) == 0.3)
+    assert(accelerate_value(0.3, 0.5, 0.1, 1000) == 0.4)
+    assert(accelerate_value(0.2, 0.6, 0.5, 500) == 0.45)
+    assert(accelerate_value(0.3, 0.5, 10, 1000) == 0.5)
+    assert(accelerate_value(0, -0.2, 0.1, 250) == -0.025)
+    assert(accelerate_value(-0.5, -0.5, 0.1, 250) == -0.5)
+
