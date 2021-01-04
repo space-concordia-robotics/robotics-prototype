debaee65c7be906f98cb86f89f321accc91a56c1
Cedric Martens <cedricmartens98@gmail.com>
2021-01-04 11:41:34 -0500

    [#452] Got the buttons working

--- robot/rover/PDS/PDS2/pds_test_gui/app.py
+++ robot/rover/PDS/PDS2/pds_test_gui/app.py
@@ -1,6 +1,48 @@
 from flask import Flask, render_template
+import serial
+import sys
+import time
 app = Flask(__name__)
+
+temperatures = [15, 15, 15, 15]
+currents = [1, 1, 1, 1]
 
 @app.route('/')
 def main_page():
     return render_template('main.html')
+
+
+@app.route('/fanoff')
+def fan_off():
+    # Send fan off command
+    print("Sending Fan off command")
+    return ('', 204)
+
+@app.route('/fanon')
+def fan_on():
+    # Send fan on command
+    print("Sending Fan on command")
+    return ('', 204)
+
+@app.route('/motoroff')
+def motoroff():
+    # Send motor off command
+    print("Sending motor off command")
+    return ('', 204)
+
+@app.route('/motoron')
+def motoron():
+    # Send motor on command
+    print("Sending motor on command")
+    return ('', 204)
+
+@app.route('/temperatures')
+def get_temperatures():
+    return (temperatures, 200)
+
+# have a loop that collects telemetry
+
+while True:
+    time.sleep(0.5)
+    # update telemetry
+    temperatures = [10, 10, 10, 10] # from device
