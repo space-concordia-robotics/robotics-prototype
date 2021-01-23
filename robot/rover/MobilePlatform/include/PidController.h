fc9b6bb57f1d0cb878fc2f2745b5ebf6901c19d1
ginywiny <michaelles97@gmail.com>
2021-01-23 16:09:00 -0500

    [#502] Rename includes folder to include

---
+++ robot/rover/MobilePlatform/include/PidController.h
@@ -0,0 +1,28 @@
+#ifndef PIDCONTROLLER_H
+#define PIDCONTROLLER_H
+#include "Helpers.h"
+#include "Pins.h"
+#include "Globals.h"
+class PidController {
+public:
+    // motor-dependent constants... currently arbitrary values. to be set in setup() probably
+    PidController();
+    float updatePID(volatile float currentAngle, float desiredAngle);
+    void setJointVelocityTolerance(float tolerance);
+    void setOutputLimits(float minVal, float maxVal, float zeroVal);
+    void setGainConstants(float kp, float ki, float kd);
+    float getMaxOutputValue(void);
+    float getMinOutputValue(void);
+    float getJointVelocityTolerance(void);
+    void printPidParameters(void);
+private:
+    float kp, ki, kd;
+    float jointVelocityTolerance;
+    float maxOutputValue, minOutputValue, slowestSpeed;
+    elapsedMillis dt;
+    float pTerm, iTerm, dTerm;
+    float error, previousError;
+    float errorSum;
+    float pidSum; // pid output, must be checked before assigning this value to pidOutput
+};
+#endif
