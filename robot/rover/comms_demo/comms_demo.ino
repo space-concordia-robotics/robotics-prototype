184af05b3382a58f6452fa123bc45180a8dca7b0
Cedric Martens <cedricmartens98@gmail.com>
2021-03-24 20:48:57 -0400

    [#499] Add the project correctly

--- robot/rover/comms_demo/comms_demo.ino
+++ robot/rover/comms_demo/comms_demo.ino
@@ -2,17 +2,20 @@
 #include "include/commands/DemoCommandCenter.h"
 #include <cstdint>
 
-internal_comms::CommandCenter* commandCenter = new DemoCommandCenter();
+const uint8_t TX_TEENSY_4_0_PIN = 1;
+const uint8_t RX_TEENSY_4_0_PIN = 0;
+const uint8_t ENABLE_PIN = 10; // THIS IS A PLACE HOLDER UNTIL FLOW CONTROL CAN BE IMPLEMENTED
 
+internal_comms::CommandCenter* commandCenter = new DemoCommandCenter();
 
 void loop()
 {
-    if(Serial.available() > 0)
-        commandCenter->readCommand();
+//    if(Serial.available() > 0)
+//        commandCenter->readCommand();
 
 }
 
 void setup()
 {
-    commandCenter->startSerial(TX_TEENSY_4_0_PIN, RX_TEENSY_4_0_PIN, ENABLE_PIN);
+//    commandCenter->startSerial(TX_TEENSY_4_0_PIN, RX_TEENSY_4_0_PIN, ENABLE_PIN);
 }
