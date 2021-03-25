03b2d710b0c4f0c2506a544f5bd6d38a22fcc5a0
Cedric Martens <cedricmartens98@gmail.com>
2021-03-24 20:52:36 -0400

    [#499] Fix linking error

--- robot/rover/comms_demo/comms_demo.ino
+++ robot/rover/comms_demo/comms_demo.ino
@@ -10,12 +10,12 @@ internal_comms::CommandCenter* commandCenter = new DemoCommandCenter();
 
 void loop()
 {
-//    if(Serial.available() > 0)
-//        commandCenter->readCommand();
+    if(Serial.available() > 0)
+        commandCenter->readCommand();
 
 }
 
 void setup()
 {
-//    commandCenter->startSerial(TX_TEENSY_4_0_PIN, RX_TEENSY_4_0_PIN, ENABLE_PIN);
+    commandCenter->startSerial(TX_TEENSY_4_0_PIN, RX_TEENSY_4_0_PIN, ENABLE_PIN);
 }
