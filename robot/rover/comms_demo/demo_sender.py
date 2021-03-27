adc85895170b111a2894e06a020e38666a6a6993
VimFreeman <github@freiman.cc>
2021-03-27 13:33:59 -0400

    [#0] Make the demo more relevant

--- robot/rover/comms_demo/demo_sender.py
+++ robot/rover/comms_demo/demo_sender.py
@@ -16,14 +16,12 @@ def main():
     ser.write(b'\x00\x02\x03\x01\x04')
     while(True):
         if ser.in_waiting > 0:
-            print(ser.read(ser.in_waiting))
-
-            # commandID = ser.read()
-            # print(int.from_bytes(commandID, "big"))
-            # argsLen = ser.read()
-            # print(int.from_bytes(argsLen, "big"))
-            # args = ser.read(int.from_bytes(argsLen, "big"))
-            # print(args)
+            commandID = ser.read()
+            print(int.from_bytes(commandID, "big"))
+            argsLen = ser.read()
+            print(int.from_bytes(argsLen, "big"))
+            args = ser.read(int.from_bytes(argsLen, "big"))
+            print(args)
             break
     ser.close()
 
