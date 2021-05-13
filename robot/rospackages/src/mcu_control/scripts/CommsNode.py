dee0f4a82c5133415cb2bfa95b15b7c3b2dd05ce
realWilliamWells <williamwells2013@hotmail.com>
2021-05-12 23:07:44 -0300

    [#42069] Change command names on front end and add command parsing in comms node

--- robot/rospackages/src/mcu_control/scripts/CommsNode.py
+++ robot/rospackages/src/mcu_control/scripts/CommsNode.py
@@ -38,6 +38,8 @@ teensy_pins = [ARM_PIN, WHEEL_PIN, SCIENCE_PIN]
 
 ser = serial.Serial('/dev/ttyACM0', 57600) # you sure this is good for the jetson tim?
 
+STOP_BYTE = 0x0A
+
 
 def listen():
     try:
@@ -81,18 +83,42 @@ def send_command(command_name, args, deviceToSendTo):
         if command_name == out_command[deviceToSendTo][0]:
             commandID = out_command[deviceToSendTo][1]
             ser.write(commandID)
-            if args is not None and len(args) != 0:
-                number_of_arguments = 0 # todo figure out number of bytes
-                ser.write(args) # todo: send each arg as a byte, also assume they will be strings so they will need to be casted to int/float first there's definitely more than one line of code to write
+            number_of_arguments = len(args)
+
+            if args is not None and number_of_arguments != 0:
+                arg_bytes = get_arg_bytes(args)
+                for arg_byte in arg_bytes:
+                    ser.write(arg_byte)
+                ser.write(STOP_BYTE)
             return True
     return False
 
 def arm_command_callback(message):
     rospy.loginfo('received: ' + message.data + ' command, sending to arm Teensy')
-    if message.data == 'reset':
-        send_command("reset_angles", [], ARM_SELECTED)
-    elif message.data == 'home':
-        send_command("home_motors", [], ARM_SELECTED)
+    command, args = parse_command(message)
+
+    send_command(command, args, ARM_SELECTED)
+
+def parse_command(message):
+    full_command = message.split(" ")
+    if full_command is not None:
+        command = full_command.pop(0)
+        args = full_command
+        if args is None:
+            args = []
+        return command, args
+    return "", []
+
+def get_arg_bytes(args):
+    arg_bytes = []
+    for arg in args:
+        if re.match(r'^-?\d+(?:\.\d+)$', arg) is None: # Check if float
+            new_bytes = bytearray(struct.pack("f", float(arg)))
+            bytes.append(new_bytes)
+
+    return arg_bytes
+
+
 
 if __name__ == '__main__':
     node_name = 'arm_node'
