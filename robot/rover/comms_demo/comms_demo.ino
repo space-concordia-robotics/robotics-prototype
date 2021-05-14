a9829264cae4e7c0bd16298d618650b921eba2d0
VimFreeman <github@freiman.cc>
2021-05-14 19:04:00 -0400

    [#499] Update demo

--- robot/rover/comms_demo/comms_demo.ino
+++ robot/rover/comms_demo/comms_demo.ino
@@ -15,7 +15,7 @@ void setup()
 {
     //Serial.begin(9600);
     pinMode(LED_BUILTIN, OUTPUT);
-    commandCenter->startSerial(TX_TEENSY_3_6_PIN, RX_TEENSY_3_6_PIN, ENABLE_PIN);
+    commandCenter->startSerial(TX_TEENSY_3_6_PIN, RX_TEENSY_3_6_PIN, ENABLE_PIN, 1); // The 1 assumes that we are currently on the Arm teensy.
 }
 
 void loop()
