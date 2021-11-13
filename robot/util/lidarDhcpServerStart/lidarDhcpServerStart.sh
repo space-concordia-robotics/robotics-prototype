1f61d78fcc16e52984baa26883c71d09e62d5584
William Wells <55707515+RealWilliamWells@users.noreply.github.com>
2021-11-13 14:25:40 -0500

    [#0] Attempt to fix dhcp issues with lidar

--- robot/util/lidarDhcpServerStart/lidarDhcpServerStart.sh
+++ robot/util/lidarDhcpServerStart/lidarDhcpServerStart.sh
@@ -8,4 +8,5 @@ sudo ip addr flush dev $ETHERNET_INTERFACE
 sudo ip addr add 10.5.5.1/24 dev $ETHERNET_INTERFACE
 sudo ip link set $ETHERNET_INTERFACE up
 
+sudo systemctl stop dnsmasq
 sudo dnsmasq -C /dev/null -kd -F 10.5.5.50,10.5.5.100 -i $ETHERNET_INTERFACE --bind-dynamic
