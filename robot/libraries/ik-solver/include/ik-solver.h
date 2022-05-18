8a5ededcafea0c9a866788817652a803ca74238f
William Wells <williamwells2013@hotmail.com>
2022-05-18 11:26:26 -0400

    [#0] Implement function for calculating joint angle angular speeds for moving between two points

--- robot/libraries/ik-solver/include/ik-solver.h
+++ robot/libraries/ik-solver/include/ik-solver.h
@@ -9,4 +9,7 @@ float getJointAngle1For2D(float endPointX, float endPointY, float beta);
 
 float getJointAngle2For2D(float endPointX, float endPointY);
 
+float getJointAngularSpeedBetweenPoints(float startPointX, float startPointY, float endPointX, float endPointY,
+                                        float linearSpeed, float* joint1Speed, float* joint2Speed);
+
 #endif //MCU_CONTROL_IK_SOLVER_H
--- robot/libraries/ik-solver/src/ik-solver.c
+++ robot/libraries/ik-solver/src/ik-solver.c
@@ -7,7 +7,7 @@
 
 #include "ik-solver.h"
 
-// Units are in cm
+// Units are in cm TODO: Double check arm lengths
 const float ARM_LENGTH_1 = 30;
 const float ARM_LENGTH_2 = 40;
 
@@ -32,6 +32,27 @@ float getJointAngle2For2D(float endPointX, float endPointY) {
     float ratio = top / bottom;
 
     return acosf(ratio);
+}
+
+float getJointAngularSpeedBetweenPoints(float startPointX, float startPointY, float endPointX, float endPointY,
+                                        float linearSpeed, float* joint1Speed, float* joint2Speed) {
+    float deltaPosX = endPointX - startPointX;
+    float deltaPosY = endPointY - startPointY;
+
+    float startJoint2Angle = getJointAngle2For2D(startPointX, startPointY);
+    float startJoint1Angle = getJointAngle1For2D(startPointX, startPointY, startJoint2Angle);
+
+    float endJoint2Angle = getJointAngle2For2D(endPointX, endPointY);
+    float endJoint1Angle = getJointAngle1For2D(endPointX, endPointY, endJoint2Angle);
+
+    float deltaJoint2Angle = endJoint2Angle - startJoint2Angle;
+    float deltaJoint1Angle = endJoint1Angle - startJoint1Angle;
+
+    float deltaTime = deltaPosX / (linearSpeed * cos(deltaPosY / deltaPosX));
+
+    *joint2Speed = deltaJoint2Angle / deltaTime;
+
+    *joint1Speed = deltaJoint1Angle / deltaTime;
 }
 
 int main() {
