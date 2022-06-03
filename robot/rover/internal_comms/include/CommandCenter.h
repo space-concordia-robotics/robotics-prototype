de52827cccb8102d4b13824d11a1ce5a6e05b245
Marc Scattolin <marcscattolin@gmail.com>
2022-06-02 20:29:59 -0400

    [#540] undid autoformat of CommandCenter.h

--- robot/rover/internal_comms/include/CommandCenter.h
+++ robot/rover/internal_comms/include/CommandCenter.h
@@ -5,9 +5,8 @@
 // And mostly written by Tim
 //
 
-#include <etl/queue.h>
-
 #include "Arduino.h"
+#include <etl/queue.h>
 
 #define COMMS_BAUDRATE 57600L
 
@@ -17,107 +16,110 @@
 //   #define Serial Serial
 //#endif
 
-namespace internal_comms {
-typedef struct {
-  uint8_t commandID;       // ID of command
-  uint8_t* rawArgs;        // Byte array with the bytes of the arguments
-  uint16_t rawArgsLength;  // Number of bytes in the rawArgs array
-  bool isValid;            // Whether the command is valid
-} Command;
+namespace internal_comms
+{
+   typedef struct {
+       uint8_t commandID; // ID of command
+       uint8_t* rawArgs; // Byte array with the bytes of the arguments
+       uint16_t rawArgsLength; // Number of bytes in the rawArgs array
+       bool isValid; // Whether the command is valid
+   } Command;
 
-typedef struct {
-  uint8_t messageID;
-  uint16_t rawArgsLength;
-  uint8_t* rawArgs;
-} Message;
+    typedef struct {
+        uint8_t messageID;
+        uint16_t rawArgsLength;
+        uint8_t* rawArgs;
+    } Message;
 
-class CommandCenter {
- public:
-  /**
-   * Executes the command with the commandID with the raw arguments
-   * @param commandID The ID of the command to execute
-   * @param rawArgs The raw bytes consisting of the arguments
-   */
-  virtual void executeCommand(const uint8_t commandID, const uint8_t* rawArgs,
-                              const uint8_t rawArgsLength) = 0;
+    class CommandCenter {
+        public:
 
-  /**
-   * Reads serial to next command
-   * and returns a command struct
-   *
-   * @return returns a command struct
-   */
-  Command* processCommand();
+            /**
+            * Executes the command with the commandID with the raw arguments
+            * @param commandID The ID of the command to execute
+            * @param rawArgs The raw bytes consisting of the arguments
+            */
+            virtual void executeCommand(const uint8_t commandID, const uint8_t* rawArgs, const uint8_t rawArgsLength) = 0;
 
-  /**
-   * Queues messages so that they are ready to be sent
-   * allows teensy.
-   * Currently we do not do anything if the queue fills up.
-   * Hopefully we can find a decent queue size that will never fill.
-   * @message reference to message struct
-   */
-  void queueMessage(Message& message);
+            /**
+            * Reads serial to next command
+            * and returns a command struct
+            *
+            * @return returns a command struct
+            */
+            Command* processCommand() ;
 
-  /**
-   * @param messageID command ID of message to send
-   * @param rawArgsLength number of arguments in the message
-   * @param rawArgs contains raw bytes of arguments
-   * @return returns a message struct
-   */
-  Message* createMessage(int messageID, int rawArgsLength, byte* rawArgs);
+            /**
+            * Queues messages so that they are ready to be sent
+            * allows teensy.
+            * Currently we do not do anything if the queue fills up.
+            * Hopefully we can find a decent queue size that will never fill.
+            * @message reference to message struct
+            */
+            void queueMessage(Message& message);
 
-  /**
-   * Holds the messages that are ready to be sent out
-   */
-  etl::queue<Message, 5> messageQueue;
+            /**
+             * @param messageID command ID of message to send
+             * @param rawArgsLength number of arguments in the message
+             * @param rawArgs contains raw bytes of arguments
+             * @return returns a message struct
+             */
+            Message* createMessage(int messageID, int rawArgsLength, byte *rawArgs);
 
-  /**
-   * Starts serial connection with given pins. Enable pin is for 485 flow
-   * control
-   */
-  void startSerial(uint8_t rxPin, uint8_t txPin, uint8_t enablePin,
-                   uint8_t transmitPin);
 
-  /**
-   * Reads data from serial port
-   */
-  void readCommand();
+            /**
+            * Holds the messages that are ready to be sent out 
+            */
+            etl::queue<Message, 5> messageQueue;
 
-  /**
-   * Sends first item of the queue if anything
-   */
-  void sendMessage();
+            /**
+             * Starts serial connection with given pins. Enable pin is for 485 flow control
+             */
+            void startSerial(uint8_t rxPin, uint8_t txPin, uint8_t enablePin, uint8_t transmitPin);
 
-  /**
-   * Enqueues the message and sends top message if enablePin is high
-   */
-  void sendMessage(Message& message);
+            /**
+             * Reads data from serial port
+             */
+            void readCommand();
 
-  /**
-   * Closes serial connection
-   */
-  void endSerial();
+            /**
+             * Sends first item of the queue if anything
+             */
+            void sendMessage();
 
-  /**
-   * Send debug message string
-   */
-  void sendDebug(const char* debugMessage);
+            /**
+             * Enqueues the message and sends top message if enablePin is high
+             */
+            void sendMessage(Message& message);
 
- private:
-  uint8_t enablePin;
+            /**
+             * Closes serial connection
+             */
+            void endSerial();
 
-  /**
-   * Reads the two bytes that make up the argument length and combines
-   * them into an uint16_t
-   */
-  uint8_t readArgSize();
 
-  /**
-   * Waits until there is something to read or for 50ms,
-   * whichever comes first.
-   */
-  uint8_t waitForSerial();
-};
+            /**
+             * Send debug message string
+             */
+            void sendDebug(const char* debugMessage);
 
-}  // namespace internal_comms
-#endif  // INTERNAL_COMMS_COMMANDCENTER_H
+        private:
+            uint8_t enablePin;
+
+
+            /**
+            * Reads the two bytes that make up the argument length and combines
+            * them into an uint16_t
+            */
+            uint16_t readArgSize();
+
+            /**
+             * Waits until there is something to read or for 50ms,
+             * whichever comes first.
+             */
+            uint8_t waitForSerial();
+
+    };
+
+}
+#endif //INTERNAL_COMMS_COMMANDCENTER_H
