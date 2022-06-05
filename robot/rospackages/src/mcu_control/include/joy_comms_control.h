e59f854cf576f73b5171ae454fc4e796cd2c21c5
AymanShe <aymalsh@gmail.com>
2022-06-05 19:08:10 -0400

    [#545] fix typo

--- robot/rospackages/src/mcu_control/include/joy_comms_control.h
+++ robot/rospackages/src/mcu_control/include/joy_comms_control.h
@@ -32,7 +32,7 @@ private:
 
     const std::string share_name = "BUTTON_SHARE";
     const std::string option_name = "BUTTON_OPTION";
-    const std::string ps_button_names[17] = {"BUTTON_CROSS",
+    const std::string xbox_button_names[17] = {"BUTTON_CROSS",
                                 "BUTTON_CIRCLE",
                                 "BUTTON_TRIANGLE",
                                 "BUTTON_SQUARE",
@@ -50,17 +50,19 @@ private:
                                 "BUTTON_DPAD_LEFT",
                                 "BUTTON_DPAD_RIGHT"};
 
-    std::string xbox_button_names[11] = {"BUTTON_CROSS",
+    std::string ps_button_names[13] = {"BUTTON_CROSS",
                                 "BUTTON_CIRCLE",
-                                "BUTTON_SQUARE",
                                 "BUTTON_TRIANGLE",
+                                "BUTTON_SQUARE",
                                 "BUTTON_L1",
                                 "BUTTON_R1",
+                                "BUTTON_L2",
+                                "BUTTON_R2",
                                 share_name,
                                 option_name,
                                 "BUTTON_HOME",
                                 "BUTTON_L3",
-                                "BUTTON_R3",};
+                                "BUTTON_R3"};
 
 
     std::map<std::string, int> button_name_to_id_map;
@@ -68,7 +70,7 @@ private:
 
     const std::string trigger_l2_name = "TRIGGER_L2";
     const std::string trigger_r2_name = "TRIGGER_R2";
-    std::string ps_axis_names[6] = {"JOY_LEFT_X",
+    std::string xbox_axis_names[6] = {"JOY_LEFT_X",
                                 "JOY_LEFT_Y",
                                 trigger_l2_name,
                                 "JOY_RIGHT_X",
@@ -77,14 +79,14 @@ private:
 
 
 
-    std::string xbox_axis_names[8] = {"JOY_LEFT_X",
+    std::string ps_axis_names[8] = {"JOY_LEFT_X",
                                 "JOY_LEFT_Y",
                                 trigger_l2_name,
                                 "JOY_RIGHT_X",
                                 "JOY_RIGHT_Y",
                                 trigger_r2_name,
                                 "DPAD_X",
-                                "DPAD_Y",};
+                                "DPAD_Y"};
 
     std::map<std::string, int> axis_name_to_id_map;
 
