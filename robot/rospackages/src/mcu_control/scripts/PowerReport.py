316fac396b72b1396b0b9394c1c076c9055f9ab4
Marc Scattolin <marcscattolin@gmail.com>
2022-08-16 16:41:27 -0400

    [#568] added message types for power report, so now reports power consumption for each system

--- robot/rospackages/src/mcu_control/scripts/PowerReport.py
+++ robot/rospackages/src/mcu_control/scripts/PowerReport.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+from multiprocessing import current_process
 import sys
 import traceback
 import time
@@ -8,33 +9,43 @@ import re
 
 import rospy
 from std_msgs.msg import String, Float32
-from mcu_control.msg import Voltage, Currents
+from mcu_control.msg import Voltage, Currents, PowerConsumption, PowerReport
 
-watt_hours = []
+wheel_watt_hours = []
+latest_power_report = PowerReport()
 pub = None
 prevTime = None
 
-def currentCallback(data):
-    global watt_hours, pub, prevTime
+def wheelCurrentCallback(data):
+    global wheel_watt_hours, pub, prevTime
     time = rospy.get_rostime().secs + (rospy.get_rostime().nsecs / 1000000000)
     if prevTime is not None:
+        # find power consumed since last datapoint
         delta = time - prevTime
         for i in range(len(data.effort)):
-            watt_hours[i] += data.effort[i] * delta
+            wheel_watt_hours[i] += data.effort[i] * delta
+        # now, sum up all wheels and put in report
+        wheel_power = 0
+        for x in wheel_watt_hours:
+            wheel_power += x
+        latest_power_report.report[0] = PowerConsumption("wheels", wheel_power)
+
+        rospy.loginfo('wheel watt hours: ' + str(wheel_watt_hours))
     else:
         # if this is the first time, initialize the watt_hours array
-        watt_hours = [0 for i in range(len(data.effort))]
+        wheel_watt_hours = [0 for i in range(len(data.effort))]
+        latest_power_report.report = [0]
     prevTime = time
-    pub.publish("energy: " + str(watt_hours))
+    pub.publish(latest_power_report)
 
 
 
 
 def start():
     rospy.init_node('power_report_node', anonymous = False)
-    rospy.Subscriber('wheel_motor_currents', Currents, currentCallback)
+    rospy.Subscriber('wheel_motor_currents', Currents, wheelCurrentCallback)
     global pub
-    pub = rospy.Publisher('power_consumption', String, queue_size = 10)
+    pub = rospy.Publisher('power_consumption', PowerReport, queue_size = 10)
     rospy.spin()
 
 if __name__ == '__main__':
