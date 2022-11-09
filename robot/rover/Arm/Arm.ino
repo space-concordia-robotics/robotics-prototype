3313b9074bae3545ea69ea7d18619e5b4d3ac82a
William Wells <williamwells2013@hotmail.com>
2022-11-09 13:23:44 -0500

    Reimplement Nick's abs encs integration into latest arm code

--- robot/rover/Arm/Arm.ino
+++ robot/rover/Arm/Arm.ino
@@ -5,17 +5,32 @@
 #include "CommandCenter.h"
 #include "include/DcMotor.h"
 #include "include/SerialMotor.h"
+#include "MagneticEncoder.h"
 #include "include/commands/ArmCommandCenter.h"
 
 #define NUM_MOTORS 6
 #define NUM_DC_MOTORS 4
 #define NUM_SMART_SERVOS 2
 
+#define ENCODER_1_ADDRESS      (0x90)
+#define ENCODER_2_ADDRESS      (0xA0)
+#define ENCODER_3_ADDRESS      (0xC0)
+
+#define ENCODER_SAMPLE_RATE    1000
+
 internal_comms::CommandCenter* commandCenter = new ArmCommandCenter();
 
 DcMotor motors[NUM_DC_MOTORS];
 SerialMotor serialMotors[NUM_SMART_SERVOS];
 LSSServoMotor servoController(&Serial5);
+
+Encoder encoder1,encoder2,encoder3;
+Encoder* encoders[] = {&encoder1};
+
+uint32_t encodersLastSample = millis();
+
+void initMagneticEncoder();
+void encoderSample(Encoder& encoder);
 
 void setup() {
 #ifndef DEBUG
@@ -27,6 +42,7 @@ void setup() {
   digitalWrite(LED_BUILTIN, LOW);
 
   pinSetup();
+  initMagneticEncoder();
 
   motors[0] = DcMotor(M1_DIR_PIN, M1_PWM_PIN, 1.0, LOW);
   motors[1] = DcMotor(M2_DIR_PIN, M2_PWM_PIN, 1.0, LOW);
@@ -40,6 +56,29 @@ void setup() {
   commandCenter->startSerial(1, 0, 25, 26);
 
   digitalWrite(LED_BUILTIN, LOW);
+}
+
+void initMagneticEncoder(){
+
+    SPI.begin();
+    pinMode(CS_PIN,OUTPUT);
+
+    encoder1.address = ENCODER_1_ADDRESS;
+    encoder2.address = ENCODER_2_ADDRESS;
+    encoder3.address = ENCODER_3_ADDRESS;
+
+    for(auto& e : encoders) {
+        // Decimation filter to maximum
+        //EncoderWrite16(e.address,REG_MOD1, 0xC000);
+        // Activation Status (Set monitoring flags)
+        //EncoderWrite16(e.address,REG_MOD1, 0x02FF);
+
+    }
+}
+
+void encoderSample(Encoder& encoder) {
+    encoder.status =  EncoderRead16(encoder.address,REG_ANGLE_VAL, &encoder.angle._raw, 1);
+    //updateState(encoder);
 }
 
 /**
@@ -97,11 +136,38 @@ void debug_test() {
   commandCenter->sendMessage(*message);
 }
 
+void sendEncoderData(Encoder& encoder){
+
+    if(encoder.status == OK){
+        encoder.angle._float = (float)(encoder.angle._raw & 0x7FFF) / (powf(2,15)) * 360.f;
+
+        byte data_msg[5] = {encoder.address,0,0,0,0};
+        memcpy(&data_msg[1],encoder.angle._bytes,sizeof(float));
+        auto msg = commandCenter->createMessage(2,sizeof(data_msg),data_msg);
+        commandCenter->sendMessage(*msg);
+    }
+    else{
+        Serial.print(encoder.status);
+        // commandCenter->sendDebug(encoder.status_msg);
+    }
+
+}
+
 void loop() {
   // Read and send messages
   if (Serial.available() > 0) {
     commandCenter->readCommand();
   }
   doMotorChecks();
+
+  if( (millis() - encodersLastSample) > ENCODER_SAMPLE_RATE){
+
+    encodersLastSample = millis();
+    for(auto e : encoders){
+      encoderSample(*e);
+      sendEncoderData(*e);
+    }
+  }
+
   commandCenter->sendMessage();
 }
---
+++ robot/rover/Arm/src/MagneticEncoder.cpp
@@ -0,0 +1,111 @@
+#include "MagneticEncoder.h"
+
+int checkAddress(uint8_t txAddress, uint8_t rxAddress) {
+
+  uint8_t lower = txAddress & 0xF0;
+  if(rxAddress == (lower | (lower >>4))){
+
+    return OK;
+  }
+  return ADDRESS_FAIL;
+}
+//
+//void SPIWrite(byte* buf, int bufSize) {
+//  buf
+//  for (int i = 0; i < bufSize; i++) {
+//    shiftOut(mosiPin, clockPin, MSBFIRST, buf[i]);
+//  }
+//}
+
+int EncoderWrite16(uint8_t address, uint8_t reg, uint16_t val) {
+  SPI.beginTransaction(TLE5012B_SPI_SETTINGS);
+
+  // Form the command from the format specified by the datasheet. The leading 0 signifies write
+  uint16_t command = (0 << 15) | (reg << 4) | 0x1;
+
+  // SPI transaction must start by pulling down the CS line
+  digitalWrite(CS_PIN, LOW);
+
+  // Address check (1 byte)
+  uint8_t receivedAddress = SPI.transfer(address);
+
+  int status = checkAddress(address, receivedAddress);
+
+  if(status != OK)
+    return status;
+
+  // Send the command to the encoder
+  SPI.transfer16(command);
+
+  // Transmit the word, note that we do not care about what is returned
+  SPI.transfer16(0x00);
+
+  // Safety word (2 bytes) contains the CRC (last byte) and some status flags
+  uint16_t safety = SPI.transfer16(0x00);
+
+  // Terminate SPI transaction
+  digitalWrite(CS_PIN,HIGH);
+
+  SPI.endTransaction();
+  // Must swap endianness to verify CRC
+  uint16_t message[2] = {
+          static_cast<uint16_t>((command >> 8) | (command << 8)),
+          static_cast<uint16_t>((val >> 8) | (val << 8))
+  };
+  // Verify CRC, using the command and the data words
+  uint8_t CRC = CRC8((uint8_t* )message,sizeof(message));
+
+  // Compare the CRC byte from the safety word and the computed byte
+  if(CRC != (safety & 0x00FF)){
+    return CRC_FAIL;
+  }
+  return OK;
+}
+
+int EncoderRead16(uint8_t address, uint8_t reg, uint16_t *data, uint8_t size) {
+
+  SPI.beginTransaction(TLE5012B_SPI_SETTINGS);
+
+  // Form the command from the format specified by the datasheet. The leading 1 signifies read.
+  uint16_t command = (1 << 15) | (reg << 4) | (size & 0x0F);
+
+  // SPI transaction must start by pulling down the CS line
+  digitalWrite(CS_PIN, LOW);
+
+  // Address check (1 byte)
+  uint8_t receivedAddress = SPI.transfer(address);
+
+//    Serial.println(receivedAddress,HEX);
+//    Serial.println(address,HEX);
+  if(checkAddress(address, receivedAddress) != OK){
+    return ADDRESS_FAIL;
+  }
+  // Send the command to the encoder
+  SPI.transfer16(command);
+
+  // Receive the desired amount of words. Since it's SPI, but we aren't transmitting anything, use a placeholder value.
+  for(int i = 0 ; i < size ; i++){
+    data[i] = SPI.transfer16(0x00);
+  }
+
+  uint16_t safety = SPI.transfer16(0x00);
+
+  // Terminate SPI transaction
+  digitalWrite(CS_PIN,HIGH);
+
+  SPI.endTransaction();
+
+  // Must swap endianness to verify CRC
+  uint16_t message[2] = {
+          static_cast<uint16_t>((command >> 8) | (command << 8)),
+          static_cast<uint16_t>((data[0] >> 8) | (data[0] << 8))
+  };
+  // Verify CRC, using the command and the data words
+  uint8_t CRC = CRC8((uint8_t*)message,sizeof(message));
+
+  // Compare the CRC byte from the safety word and the computed byte
+  if(CRC != (safety & 0xFF)){
+    return CRC_FAIL;
+  }
+  return OK;
+}
