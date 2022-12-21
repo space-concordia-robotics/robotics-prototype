6d4131f228dfd56642ba303887f31d4e7c018100
William Wells <williamwells2013@hotmail.com>
2022-12-21 14:09:24 -0500

    [#594] Update rover startup to work with ROS2

--- robot/util/rosRoverStart/rosRoverStart.sh
+++ robot/util/rosRoverStart/rosRoverStart.sh
@@ -2,9 +2,8 @@
 HOME="/home/nvidia"
 REPO_HOME="$HOME/Programming/robotics-prototype"
 ETHERNET_CONFIG_STATUS="$HOME/configEthernet/status_done"
-#CATKIN_WS_SETUP="$HOME/catkin_ws/devel/setup.bash" # catkin ws only for isolated prototyping
-OPT_MELODIC_SETUP="/opt/ros/melodic/setup.bash" # ros system default workspace
-ROS_PACKAGES_SETUP="$REPO_HOME/robot/rospackages/devel/setup.bash"
+OPT_FOXY_SETUP="/opt/ros/foxy/setup.bash" # ros2 system default workspace
+ROS_PACKAGES_SETUP="$REPO_HOME/robot/rospackages/install/local_setup.bash"
 ROSLAUNCH_FILE="$REPO_HOME/robot/util/rosRoverStart/rover.launch"
 # wait for connection to configure itself via config-ethernet.service
 while [ `cat $ETHERNET_CONFIG_STATUS` == "0" ]
@@ -21,4 +20,4 @@ source "$REPO_HOME/venv/bin/activate"
 python3 "$REPO_HOME/setup.py" develop
 
 # source primary catkin_ws setup bash script and execute one launch script to rule them all
-source $OPT_MELODIC_SETUP && source $ROS_PACKAGES_SETUP && roslaunch $ROSLAUNCH_FILE
+source OPT_FOXY_SETUP && source $ROS_PACKAGES_SETUP && roslaunch $ROSLAUNCH_FILE
