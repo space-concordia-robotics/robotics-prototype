c2b3a0aea0e3409d9836db90d40d484e1a2fe359
VimFreeman <github@freiman.cc>
2023-01-14 13:37:54 -0500

    Create new setup scripts (untested)

---
+++ scripts/arduino_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo snap install yq --channel=v4/stable
+
+echo "Downloading latest release of Arduino 2.0 IDE..."
+cd /home/$USER/
+curl -s https://api.github.com/repos/arduino/arduino-ide/releases/latest | grep "browser_download_url.*Linux_64bit.zip" | cut -d : -f 2,3 | tr -d \" | wget -i -
+
+echo "Unzipping..."
+unzip -q *Linux_64bit.zip 
+mv arduino-ide_*_Linux_64bit/ arduino-ide
+
+./arduino-ide/arduino-ide &
+
+sleep 6
+
+killall arduino-ide
+
+cat ~/.arduinoIDE/arduino-cli.yaml | yq '.board_manager.additional_urls=["https://www.pjrc.com/teensy/package_teensy_index.json"]' | tee ~/.arduinoIDE/arduino-cli.tmp
+mv ~/.arduinoIDE/arduino-cli.tmp ~/.arduinoIDE/arduino-cli.yaml
+
+echo "Cleaning up..."
+rm arduino-ide_*_Linux*
+
+echo "Done installing Arduino IDE"
+
---
+++ scripts/ros_install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Updating Locale..."
+sudo apt -qq install locales -y
+sudo locale-gen en_CA en_CA.UTF-8
+sudo update-locale LC_ALL=en_CA.UTF-8 LANG=en_CA.UTF-8
+export LANG=en_CA.UTF-8
+
+echo "Adding ROS source..."
+sudo apt -qq install software-properties-common
+sudo add-apt-repository universe
+
+sudo apt -qq install curl
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+echo "Updating sources and upgrading system packages..."
+sudo apt -qq update
+sudo apt -qq upgrade -y
+
+echo "Installing ROS... (will take a while)"
+sudo apt -qq install ros-foxy-desktop
+sudo apt -qq install ros-dev-tools
+
+echo "Done installing ROS"
+
