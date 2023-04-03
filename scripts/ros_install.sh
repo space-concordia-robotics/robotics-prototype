6e3806244c5e0c460067b8e68eb1b9d9a544e8c1
Omar Ziad <omar.ziad1@outlook.com>
2023-04-03 18:43:36 -0400

    [#601] Update script to exit if installation fails

--- scripts/ros_install.sh
+++ scripts/ros_install.sh
@@ -9,9 +9,11 @@ export LANG=en_CA.UTF-8
 echo "Adding ROS source..."
 sudo apt-get -qq install software-properties-common
 sudo apt-get update -y
-sudo add-apt-repository universe
+sudo add-apt-repository universe -y
 
 sudo apt -qq install curl
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 echo "Updating sources and upgrading system packages..."
@@ -22,4 +24,14 @@ echo "Installing ROS... (will take a while)"
 sudo apt -qq install ros-humble-desktop
 sudo apt -qq install ros-dev-tools
 
-echo "Done installing ROS"
+ROS_VERSION=$(rosversion -d)
+
+
+if [ -e /opt/ros/humble/setup.sh ] && [ $? == 0 ]; #$? returns 0 when previous command executes successfully
+then
+        echo "Done installing ROS ${ROS_VERSION}"
+else
+        echo "Install Failed"
+	exit 1
+fi
+
