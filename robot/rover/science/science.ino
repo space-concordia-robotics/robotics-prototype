7e91fb9b74f17794fe4a3de783a00a5d101fc933
Marc Scattolin <marcscattolin@gmail.com>
2023-05-21 21:31:46 -0400

    [#578] recal

--- robot/rover/science/science.ino
+++ robot/rover/science/science.ino
@@ -23,7 +23,7 @@ void updateSystems();
 internal_comms::CommandCenter* commandCenter = new ScienceCommandCenter();
 
 // TODO: set the servo id to a correct one
-Carousel *carousel = new Carousel(0, 570);
+Carousel *carousel = new Carousel(0, 690);
 Updatable* updatables[1] = {carousel};
 unsigned long time = micros();
 
