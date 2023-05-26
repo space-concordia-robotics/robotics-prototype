2c839adb7da697acefe1a0b9534f46173cdaffcc
Marc Scattolin <marcscattolin@gmail.com>
2023-05-25 22:22:03 -0400

    adjust cal

--- robot/rover/science/science.ino
+++ robot/rover/science/science.ino
@@ -23,7 +23,7 @@ void updateSystems();
 internal_comms::CommandCenter* commandCenter = new ScienceCommandCenter();
 
 // TODO: set the servo id to a correct one
-Carousel *carousel = new Carousel(0, 720);
+Carousel *carousel = new Carousel(0, 740);
 Updatable* updatables[1] = {carousel};
 unsigned long time = micros();
 
