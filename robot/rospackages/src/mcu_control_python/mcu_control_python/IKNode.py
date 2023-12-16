b8d4728b5fdc742291c2324c260ca55c92774fff
Marc Scattolin <marcscattolin@gmail.com>
2023-12-16 15:13:47 -0500

    basic cylindrical coords calculation

--- robot/rospackages/src/mcu_control_python/mcu_control_python/IKNode.py
+++ robot/rospackages/src/mcu_control_python/mcu_control_python/IKNode.py
@@ -6,7 +6,7 @@ from std_msgs.msg import Header
 import sys
 from rclpy.node import Node
 from rclpy.clock import Clock
-from decimal import *
+import math
 
 devpath = "/dev/cadmouse"
 
@@ -16,12 +16,19 @@ class IkNode(Node):
     node_name = 'ik_node'
     super().__init__(node_name)
     self.get_logger().info('Initialized "'+node_name+'" node for pub functionality')
-    self.x = Decimal(0)
-    self.y = Decimal(0)
-    self.z = Decimal(0)
-    self.th = Decimal(0)
-    # Set fixed point precision
-    getcontext().prec = 10
+    # Cartesian coordinates of desired location of end effector
+    self.x = 0
+    self.y = 0
+    self.z = 1
+    # Keeps track of spin (think of it as roll angle)
+    # Not touched by ik, sent straight to MCUs
+    self.th = 0
+
+    # Cylindrical coordinates
+    # Starts out with the cylindrical plane in the XZ plane
+    self.u = 0 # Horizontal on plane
+    self.v = 1 # Vertical on plane (same as z)
+    self.phi = 0 # angle
 
     joy_topic = '/cad_mouse_joy'
     self.joy_sub = self.create_subscription(Joy, joy_topic, self.joy_callback, 10)
@@ -31,12 +38,22 @@ class IkNode(Node):
   def joy_callback(self, message: Joy):
     # self.get_logger().info(f"Received from cad mouse {message.axes}")
     x, y, z, pitch, roll, yaw = message.axes
-    self.x += Decimal(x / 150)
-    self.y += Decimal(y / 150)
-    self.z += Decimal(z / 150)
-    self.th += Decimal(yaw / 150) # use rotation axis that is activated by spinning the top
+    self.x += (x / 30000) # left-right of mouse
+    self.y += (y / 30000) # forward-back of mouse
+    self.z += (z / 30000) # vertical axis of mouse
+    self.th += (yaw / 150) # use rotation axis that is activated by spinning the top
+    self.calculate_cylindical()
     self.get_logger().info(f"Current location: {self.x} {self.y} {self.z} roll {self.th}")
+    self.get_logger().info(f"Cylindical coordinates: u {self.u} v {self.v} phi {self.phi}")
     # Stamp {message.header.stamp}
+
+  def calculate_cylindical(self):
+    self.u =  math.sqrt(self.x ** 2 + self.y ** 2)
+    self.v = self.z
+    if self.x == 0:
+      self.phi = 0
+    else:
+      self.phi = math.atan(self.y / self.x)
 
 
 def main(args=None):
