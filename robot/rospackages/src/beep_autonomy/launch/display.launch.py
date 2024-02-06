a1681a3baa495df71ad76ffd7a879eb8265b3cdd
Mathieu <mathieu.krajicek-allard@spaceconcordia.ca>
2024-02-06 16:10:09 -0500

    Added worlds. Updated rover model. Updated navigational parameters. Autonomy almost works now, just seems to need functional GPS

--- robot/rospackages/src/beep_autonomy/launch/display.launch.py
+++ robot/rospackages/src/beep_autonomy/launch/display.launch.py
@@ -2,11 +2,16 @@ import launch
 from launch.substitutions import Command, LaunchConfiguration
 import launch_ros
 import os
+from launch_ros.descriptions import ComposableNode, ParameterFile
+from nav2_common.launch import RewrittenYaml
+from launch.actions import SetEnvironmentVariable
 
 def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='beep_autonomy').find('beep_autonomy')
+    bringup_dir = launch_ros.substitutions.FindPackageShare(package='nav2_bringup').find('nav2_bringup')
     default_model_path = os.path.join(pkg_share, 'src/description/rover_description.urdf')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
+    default_params_file=os.path.join(pkg_share, 'config/nav2_params_no_map.yaml')
     world_path=os.path.join(pkg_share, 'world/my_world.sdf')
     
     robot_state_publisher_node = launch_ros.actions.Node(
@@ -42,15 +47,165 @@ def generate_launch_description():
         arguments=['-entity', 'rover', '-topic', 'robot_description'],
         output='screen'
     )
-    robot_localization_node = launch_ros.actions.Node(
+    odom_node = launch_ros.actions.Node(
          package='robot_localization',
          executable='ekf_node',
-         name='ekf_filter_node',
+         name='ekf_filter_node_odom',
          output='screen',
-         parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+         parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}],
+         remappings=[("odometry/filtered", "odometry/local")]
+    )
+
+    map_node=launch_ros.actions.Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node_map',
+        output='screen',
+        parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}],
+        remappings=[("odometry/filtered", "odometry/global")]
+    )
+    navsat_node=launch_ros.actions.Node(
+        package="robot_localization",
+        executable="navsat_transform_node",
+        name="navsat_transform",
+        output="screen",
+        parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}],
+        remappings=[
+            ("imu/data", "imu/data"),
+            ("gps/fix", "gps/fix"),
+            ("gps/filtered", "gps/filtered"),
+            ("odometry/gps", "odometry/gps"),
+            ("odometry/filtered", "odometry/global"),
+        ]
+    )
+
+    remappings = [('/tf', 'tf'),
+                  ('/tf_static', 'tf_static')]
+    namespace = LaunchConfiguration('namespace')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    autostart = LaunchConfiguration('autostart')
+    log_level = LaunchConfiguration('log_level')
+    use_respawn = LaunchConfiguration('use_respawn')
+    params_file = LaunchConfiguration('params_file')
+    param_substitutions = {
+        'use_sim_time': use_sim_time,
+        'autostart': autostart}
+
+    configured_params = ParameterFile(
+        RewrittenYaml(
+            source_file=params_file,
+            root_key=namespace,
+            param_rewrites=param_substitutions,
+            convert_types=True),
+        allow_substs=True)
+
+
+    lifecycle_nodes = ['controller_server',
+                       'smoother_server',
+                       'planner_server',
+                       'behavior_server',
+                       'bt_navigator',
+                       'waypoint_follower',
+                       'velocity_smoother']
+    controller_node=launch_ros.actions.Node(
+        package='nav2_controller',
+        executable='controller_server',
+        output='screen',
+        respawn=use_respawn,
+        respawn_delay=2.0,
+        parameters=[configured_params],
+        arguments=['--ros-args', '--log-level', log_level],
+        remappings=remappings + [('cmd_vel', 'cmd_vel_nav')]
+    )
+
+    smoother_node=launch_ros.actions.Node(
+        package='nav2_smoother',
+        executable='smoother_server',
+        name='smoother_server',
+        output='screen',
+        respawn=use_respawn,
+        respawn_delay=2.0,
+        parameters=[configured_params],
+        arguments=['--ros-args', '--log-level', log_level],
+        remappings=remappings
+    )
+
+    planner_node=launch_ros.actions.Node(
+        package='nav2_planner',
+        executable='planner_server',
+        name='planner_server',
+        output='screen',
+        respawn=use_respawn,
+        respawn_delay=2.0,
+        parameters=[configured_params],
+        arguments=['--ros-args', '--log-level', log_level],
+        remappings=remappings
+    )
+
+    behaviours_node=launch_ros.actions.Node(
+        package='nav2_behaviors',
+        executable='behavior_server',
+        name='behavior_server',
+        output='screen',
+        respawn=use_respawn,
+        respawn_delay=2.0,
+        parameters=[configured_params],
+        arguments=['--ros-args', '--log-level', log_level],
+        remappings=remappings
+    )
+
+    bt_navigator_node=launch_ros.actions.Node(
+        package='nav2_bt_navigator',
+        executable='bt_navigator',
+        name='bt_navigator',
+        output='screen',
+        respawn=use_respawn,
+        respawn_delay=2.0,
+        parameters=[configured_params],
+        arguments=['--ros-args', '--log-level', log_level],
+        remappings=remappings
+    )
+
+    waypoint_follower_node=launch_ros.actions.Node(
+        package='nav2_waypoint_follower',
+        executable='waypoint_follower',
+        name='waypoint_follower',
+        output='screen',
+        respawn=use_respawn,
+        respawn_delay=2.0,
+        parameters=[configured_params],
+        arguments=['--ros-args', '--log-level', log_level],
+        remappings=remappings
+    )
+
+    velocity_smoother_node=launch_ros.actions.Node(
+        package='nav2_velocity_smoother',
+        executable='velocity_smoother',
+        name='velocity_smoother',
+        output='screen',
+        respawn=use_respawn,
+        respawn_delay=2.0,
+        parameters=[configured_params],
+        arguments=['--ros-args', '--log-level', log_level],
+        remappings=remappings +
+                [('cmd_vel', 'cmd_vel_nav'), ('cmd_vel_smoothed', 'cmd_vel')]
+    )
+
+    lifecycle_manager_node=launch_ros.actions.Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_navigation',
+        output='screen',
+        arguments=['--ros-args', '--log-level', log_level],
+        parameters=[{'use_sim_time': use_sim_time},
+                    {'autostart': autostart},
+                    {'node_names': lifecycle_nodes}]
     )
 
     return launch.LaunchDescription([
+        launch.actions.SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
+        launch.actions.DeclareLaunchArgument(name='namespace',default_value='',
+                                            description='Top-level namespace'),
         launch.actions.DeclareLaunchArgument(name='gui', default_value='False',
                                             description='Flag to enable joint_state_publisher_gui'),
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
@@ -59,11 +214,29 @@ def generate_launch_description():
                                             description='Absolute path to rviz config file'),
         launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
                                             description='Flag to enable use_sim_time'),
+        launch.actions.DeclareLaunchArgument(name='params_file',default_value=default_params_file,
+                                            description='Full path to the ROS2 parameters file to use for all launched nodes'),
+        launch.actions.DeclareLaunchArgument(name='autostart', default_value='true',
+                                            description='Automatically startup the nav2 stack'),
+        launch.actions.DeclareLaunchArgument(name='use_respawn', default_value='False',
+                                            description='Whether to respawn if a node crashes. Applied when composition is disabled.'),
+        launch.actions.DeclareLaunchArgument(name='log_level', default_value='info',
+                                            description='log level'),
         launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path], output='screen'),
         joint_state_publisher_node,
         # joint_state_publisher_gui_node,
         robot_state_publisher_node,
         spawn_entity,
-        robot_localization_node,
-        rviz_node
+        odom_node,
+        map_node,
+        navsat_node,
+        rviz_node,
+        controller_node,
+        smoother_node,
+        planner_node,
+        behaviours_node,
+        bt_navigator_node,
+        waypoint_follower_node,
+        velocity_smoother_node,
+        lifecycle_manager_node
     ])
