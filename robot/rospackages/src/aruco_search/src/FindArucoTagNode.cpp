e49cb05bb78ef90d875423d98d48af7c11cb8271
Omar Ziad <O.Ziad@outlook.com>
2024-05-25 14:22:27 -0400

    Remove aruco_search package for now due to compilation errors

--- robot/rospackages/src/aruco_search/src/FindArucoTagNode.cpp
+++
@@ -1,8 +0,0 @@
-#include "aruco_search/FindArucoTagNode.hpp"
-FindArucoTagNode::FindArucoTagNode(const std::string& xml_tag_name,
-	const std::string& action_name,
-	const BT::NodeConfiguration& conf) : BtActionNode<nav2_msgs::action::Wait>(xml_tag_name, action_name, conf),
-										 initialized_(false)
-{
-	
-}
